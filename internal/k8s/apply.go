7242add7859135c06008a731325e79c33705296f
Dan Miller <dan@dmiller.io>
2018-08-07 16:17:41 -0400

    k8s: simplify Apply to read from stdin instead of file (#6)

--- internal/k8s/apply.go
+++ internal/k8s/apply.go
@@ -1,26 +1,16 @@
 package k8s
 
 import (
+	"bytes"
 	"context"
-	"io/ioutil"
-	"os"
 	"os/exec"
-	"path/filepath"
 )
 
 func Apply(ctx context.Context, rawYAML string) error {
 	// TODO(dmiller) validate that the string is YAML and give a good error
-	dir, err := ioutil.TempDir("", "tilt")
-	if err != nil {
-		return err
-	}
-	defer os.RemoveAll(dir)
+	c := exec.CommandContext(ctx, "kubectl", "apply", "-f", "-")
+	r := bytes.NewReader([]byte(rawYAML))
+	c.Stdin = r
 
-	tmpf := filepath.Join(dir, "tilt.yaml")
-	contents := []byte(rawYAML)
-	if err := ioutil.WriteFile(tmpf, contents, 0666); err != nil {
-		return err
-	}
-	c := exec.CommandContext(ctx, "kubectl", "apply", "-f", tmpf)
 	return c.Run()
 }
