394aa7d19ad73d6ab1027fa6c17d0a8af19e9696
Nick Santos <nicholas.j.santos@gmail.com>
2018-08-08 18:58:57 -0400

    k8s: inject image digest

---
+++ internal/k8s/image.go
@@ -0,0 +1,114 @@
+package k8s
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/docker/distribution/reference"
+	digest "github.com/opencontainers/go-digest"
+	"k8s.io/api/core/v1"
+)
+
+// Returns: the new entity, whether anything was replaced, and an error.
+func InjectImageDigestWithStrings(entity k8sEntity, original string, newDigest string) (k8sEntity, bool, error) {
+	originalRef, err := reference.ParseNamed(original)
+	if err != nil {
+		return k8sEntity{}, false, err
+	}
+
+	d, err := digest.Parse(newDigest)
+	if err != nil {
+		return k8sEntity{}, false, err
+	}
+
+	return InjectImageDigest(entity, originalRef, d)
+}
+
+// Iterate through the fields of a k8s entity and
+// replace a image name with its digest.
+//
+// Returns: the new entity, whether anything was replaced, and an error.
+func InjectImageDigest(entity k8sEntity, originalRef reference.Named, digest digest.Digest) (k8sEntity, bool, error) {
+	newRef, err := reference.WithDigest(originalRef, digest)
+	if err != nil {
+		return k8sEntity{}, false, err
+	}
+
+	containers, err := extractContainers(&entity)
+	if err != nil {
+		return k8sEntity{}, false, err
+	}
+
+	replaced := false
+	for _, container := range containers {
+		existingRef, err := reference.ParseNamed(container.Image)
+		if err != nil {
+			return k8sEntity{}, false, err
+		}
+
+		if existingRef.Name() == originalRef.Name() {
+			container.Image = newRef.String()
+			replaced = true
+		}
+	}
+	return entity, replaced, nil
+}
+
+// Get pointers to all the container specs in this object.
+func extractContainers(obj interface{}) ([]*v1.Container, error) {
+	cType := reflect.TypeOf(v1.Container{})
+	v := reflect.ValueOf(obj)
+	result := make([]*v1.Container, 0)
+
+	// Recursively iterate over the struct fields.
+	var extract func(v reflect.Value) error
+	extract = func(v reflect.Value) error {
+		switch v.Kind() {
+		case reflect.Ptr, reflect.Interface:
+			if v.IsNil() {
+				return nil
+			}
+			return extract(v.Elem())
+
+		case reflect.Struct:
+			if v.Type() == cType {
+				if !v.CanAddr() {
+					return fmt.Errorf("Error addressing: %v", v)
+				}
+				ptr, ok := v.Addr().Interface().(*v1.Container)
+				if !ok {
+					return fmt.Errorf("Error addressing: %v", v)
+				}
+				result = append(result, ptr)
+				return nil
+			}
+
+			for i := 0; i < v.NumField(); i++ {
+				field := v.Field(i)
+				err := extract(field)
+				if err != nil {
+					return err
+				}
+			}
+			return nil
+
+		case reflect.Slice:
+			for i := 0; i < v.Len(); i++ {
+				field := v.Index(i)
+				err := extract(field)
+				if err != nil {
+					return err
+				}
+			}
+			return nil
+
+		}
+		return nil
+	}
+
+	err := extract(v)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
