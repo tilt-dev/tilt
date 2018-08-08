3c4fb73a971b3ea741e1c113865ff5de5130dc90
Nick Santos <nicholas.j.santos@gmail.com>
2018-08-08 17:53:56 -0400

    k8s: parse k8s objects (#18)

---
+++ internal/k8s/serialize_test.go
@@ -0,0 +1,52 @@
+package k8s
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestTracerYAML(t *testing.T) {
+	entities, err := ParseYAMLFromString(TracerYAML)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(entities) != 3 ||
+		entities[0].Kind.Kind != "Deployment" ||
+		entities[1].Kind.Kind != "Service" ||
+		entities[2].Kind.Kind != "Service" {
+		t.Errorf("Unexpected entities: %+v", entities)
+	}
+
+	result, err := SerializeYAML(entities)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !strings.Contains(result, "image: openzipkin/zipkin") {
+		t.Errorf("image name did not appear in serialized yaml: %s", result)
+	}
+	if !strings.Contains(result, "name: tracer-prod") {
+		t.Errorf("service name did not appear in serialized yaml: %s", result)
+	}
+}
+
+func TestSanchoYAML(t *testing.T) {
+	entities, err := ParseYAMLFromString(SanchoYAML)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(entities) != 1 || entities[0].Kind.Kind != "Deployment" {
+		t.Errorf("Unexpected entities: %+v", entities)
+	}
+
+	result, err := SerializeYAML(entities)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !strings.Contains(result, "image: gcr.io/some-project-162817/sancho") {
+		t.Errorf("image name did not appear in serialized yaml: %s", result)
+	}
+}
