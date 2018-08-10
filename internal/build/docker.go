28ef17c30d62930f09742f1e9a75b707ed6cc33c
Maia <maia.mcc@gmail.com>
2018-08-10 16:18:45 -0400

    close tar writer properly

--- internal/build/docker.go
+++ internal/build/docker.go
@@ -7,7 +7,6 @@ import (
 	"flag"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
@@ -16,7 +15,7 @@ import (
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
-	digest "github.com/opencontainers/go-digest"
+	"github.com/opencontainers/go-digest"
 )
 
 type Mount struct {
@@ -61,16 +60,17 @@ func (l *localDockerBuilder) buildBaseWithMounts(ctx context.Context, baseDocker
 		return "", err
 	}
 	// TODO(dmiller): remove this debugging code
-	tar2, err := tarContext(baseDockerfile, mounts)
-	if err != nil {
-		return "", err
-	}
-	buf := new(bytes.Buffer)
-	buf.ReadFrom(tar2)
-	err = ioutil.WriteFile("/tmp/debug.tar", buf.Bytes(), os.FileMode(0777))
-	if err != nil {
-		return "", err
-	}
+	//tar2, err := tarContext(baseDockerfile, mounts)
+	//if err != nil {
+	//	return "", err
+	//}
+	//buf := new(bytes.Buffer)
+	//buf.ReadFrom(tar2)
+	//err = ioutil.WriteFile("/tmp/debug.tar", buf.Bytes(), os.FileMode(0777))
+	//if err != nil {
+	//	return "", err
+	//}
+
 	imageBuildResponse, err := l.dcli.ImageBuild(
 		ctx,
 		tar,
@@ -99,6 +99,14 @@ func (l *localDockerBuilder) buildBaseWithMounts(ctx context.Context, baseDocker
 // and returns that new dockerfile + necessary files in a tar
 func tarContext(df string, mounts []Mount) (*bytes.Reader, error) {
 	buf := new(bytes.Buffer)
+	err := tarToBuf(buf, df, mounts)
+	if err != nil {
+		return nil, err
+	}
+	return bytes.NewReader(buf.Bytes()), nil
+}
+
+func tarToBuf(buf *bytes.Buffer, df string, mounts []Mount) error {
 	tw := tar.NewWriter(buf)
 	defer func() {
 		err := tw.Close()
@@ -118,21 +126,20 @@ func tarContext(df string, mounts []Mount) (*bytes.Reader, error) {
 	}
 	err := tw.WriteHeader(tarHeader)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	_, err = tw.Write([]byte(newdf))
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	for _, m := range mounts {
 		err = tarPath(tw, m.Repo.LocalPath, m.ContainerPath)
 		if err != nil {
-			return nil, err
+			return err
 		}
 	}
-
-	return bytes.NewReader(buf.Bytes()), nil
+	return nil
 }
 
 // tarPath writes the given source path into tarWriter at the given dest (recursively for directories).
