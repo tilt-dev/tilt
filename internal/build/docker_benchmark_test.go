e74cc8944e40a576d17b827bb5c407015006b33c
Maia McCormick <maia.mcc@gmail.com>
2018-08-20 17:32:50 -0400

    build/bench: multiple commands as single step (#105)

--- internal/build/docker_benchmark_test.go
+++ internal/build/docker_benchmark_test.go
@@ -4,6 +4,7 @@ package build
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/windmilleng/tilt/internal/model"
@@ -19,6 +20,34 @@ func BenchmarkBuildTenSteps(b *testing.B) {
 			steps = append(steps, model.ToShellCmd(fmt.Sprintf("echo -n %d > hi", i)))
 		}
 
+		digest, err := f.b.BuildDockerFromScratch(f.ctx, simpleDockerfile, []model.Mount{}, steps, model.Cmd{})
+		if err != nil {
+			b.Fatal(err)
+		}
+
+		expected := []expectedFile{
+			expectedFile{path: "hi", contents: "9"},
+		}
+		f.assertFilesInImage(string(digest), expected)
+	}
+	for i := 0; i < b.N; i++ {
+		run()
+	}
+}
+
+func BenchmarkBuildTenStepsInOne(b *testing.B) {
+	run := func() {
+		f := newDockerBuildFixture(b)
+		defer f.teardown()
+
+		allCmds := make([]string, 10)
+		for i := 0; i < 10; i++ {
+			allCmds[i] = fmt.Sprintf("echo -n %d > hi", i)
+		}
+
+		oneCmd := strings.Join(allCmds, " && ")
+
+		steps := []model.Cmd{model.ToShellCmd(oneCmd)}
 		digest, err := f.b.BuildDockerFromScratch(f.ctx, simpleDockerfile, []model.Mount{}, steps, model.Cmd{})
 		if err != nil {
 			b.Fatal(err)
