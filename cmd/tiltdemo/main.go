1a801a88f2c29df4a0edbf39d1ac91695bcaf57b
Han Yu <han@windmill.engineering>
2018-08-22 16:25:07 -0400

    made awaitInput its own function

--- cmd/tiltdemo/main.go
+++ cmd/tiltdemo/main.go
@@ -11,13 +11,27 @@ func main() {
 	println(0, "Starting tilt...")
 	println(100, "Building [my-app-frontend]")
 	println(500, "Build complete in 3.3423s")
-	bufio.NewReader(os.Stdin).ReadBytes('\n')
-	println(0, "Starting tilt...")
-	println(100, "Building [my-app-frontend]")
+
+	awaitInput('\n')
+
+	println(100, "Building [my-app-backend]")
 	println(500, "Build complete in 0.1232s")
 }
 
 func println(ms int, msg string) {
 	time.Sleep(time.Duration(ms) * time.Millisecond)
 	fmt.Println(msg)
+}
+
+func awaitInput(input byte) {
+	reader := bufio.NewReader(os.Stdin)
+	c, err := reader.ReadByte()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if c == input {
+		return
+	}
 }
