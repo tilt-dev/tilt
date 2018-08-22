3440aed853bc86a26a6b419ffbd6e69b9fe4a140
Han Yu <han@sent.com>
2018-08-22 16:38:16 -0400

    demo: more output after Enter keypress (#150)
    * Nix Cobra
    
    * Print output after user presses Enter
    
    * made awaitInput its own function
    
    * make awaitInput more simple

--- cmd/tiltdemo/main.go
+++ cmd/tiltdemo/main.go
@@ -1,9 +1,38 @@
 package main
 
 import (
-	"github.com/windmilleng/tilt/internal/demo"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"time"
 )
 
 func main() {
-	demo.Execute()
+	println(0, "Starting tilt...")
+	println(100, "Building [my-app-frontend]")
+	println(500, "Build complete in 3.3423s")
+
+	awaitInput()
+
+	println(100, "Building [my-app-backend]")
+	println(500, "Build complete in 0.1232s")
+
+	awaitInput()
+
+	println(100, "Building [my-app-backend2]")
+	println(500, "Build complete in 1.3532s")
+}
+
+func println(ms int, msg string) {
+	time.Sleep(time.Duration(ms) * time.Millisecond)
+	fmt.Println(msg)
+}
+
+func awaitInput() {
+	reader := bufio.NewReader(os.Stdin)
+	_, err := reader.ReadByte()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
