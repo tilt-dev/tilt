2c0b018e61be24e2864aec2dd21d8d4705cdf2da
Han Yu <han@windmill.engineering>
2018-08-23 17:30:04 -0400

    demo: output less ambitious for spec

--- cmd/tiltdemo/main.go
+++ cmd/tiltdemo/main.go
@@ -16,7 +16,7 @@ var cReset = "\u001b[0m"
 
 // Snippets
 var linebreak = "\n"
-var stream = "    │ "
+var stream = "    ╎ "
 var service = "app-frontend"
 
 func main() {
@@ -36,80 +36,75 @@ func main() {
 
 func firstPipeline() {
 	newline()
-	printlnColor(100, "────┤ Pipeline 001 ├────────────────────────────────────────", cBlue)
+	printlnColor(100, "──┤ Pipeline Starting … ├────────────────────────────────────────", cBlue)
 	printlnColor(100, "STEP 1/3 — Building from scratch: [gcr.io/project/app-frontend]", cGreen)
 	println(100, "  → tarring Docker context")
 	println(100, "  → sending to Docker daemon…")
 	printDockerOutput(3)
-	printf(200, "    (Payload — %s5.230s%s)\n", cMagenta, cReset)
+	println(0, "    (Done — 5.230s)")
 
 	newline()
 	printf(100, "%sSTEP 2/3 — Pushing: [%s]%s\n", cGreen, service, cReset)
-	println(200, "    (Done — 0.112s)")
+	println(0, "    (Done — 0.112s)")
 
 	newline()
-	printf(100, "%sSTEP 3/3 — Deploying: [%s]%s", cGreen, service, cReset)
+	printf(100, "%sSTEP 3/3 — Deploying: [%s]%s\n", cGreen, service, cReset)
 	println(100, "  → parsing config YAML")
 	println(100, "  → applying config via kubectl")
 	printK8sOutput()
-	println(200, "    (Done — 2.267s)")
+	println(0, "    (Done — 2.267s)")
 
 	newline()
-	printlnColor(100, "────┤ ︎Pipeline 001 Done ⚡︎├────────────────────────────────", cBlue)
-	println(200, "    │ Overhead — 2.379s")
-	printf(200, "    │  Payload — %s5.230s%s\n", cMagenta, cReset)
-	printf(200, "    │    TOTAL — %s7.609s%s\n", cBlue, cReset)
+	println(0, "  │ Step 1 — 5.230s")
+	println(0, "  │ Step 2 — 0.112s")
+	println(0, "  │ Step 3 — 2.379s")
+	printf(100, "%s──┤ ︎Pipeline Done in %s7.609s%s ⚡︎├────────────────────────────────────%s", cBlue, cGreen, cBlue, cReset)
 }
 
 func secondPipeline() {
 	newline()
-	printlnColor(100, "────┤ Pipeline 002 ├────────────────────────────────────────", cBlue)
+	printlnColor(100, "──┤ Pipeline Starting … ├────────────────────────────────────────", cBlue)
 	printlnColor(100, "STEP 1/2 — Building from existing: [gcr.io/project/app-frontend]", cGreen)
 	println(100, "  → tarring Docker context")
 	println(100, "  → sending to Docker daemon…")
 	printDockerOutput(1)
-	printf(200, "    (Payload — %s5.230s%s)\n", cMagenta, cReset)
+	println(0, "    (Done — 5.230s)")
 
 	newline()
 	printf(100, "%sSTEP 2/2 — Syncing: [%s]%s\n", cGreen, service, cReset)
-	println(200, "    (Done — 0.232s)")
+	println(0, "    (Done — 0.232s)")
 
 	newline()
-	printlnColor(100, "────┤ ︎Pipeline 002 Done ⚡︎├────────────────────────────────", cBlue)
-	println(200, "    │ Overhead — 0.232s")
-	printf(200, "    │  Payload — %s5.230s%s\n", cMagenta, cReset)
-	printf(200, "    │    TOTAL — %s5.642s%s\n", cBlue, cReset)
+	println(0, "  │ Step 1 — 5.230s")
+	println(0, "  │ Step 2 — 0.232s")
+	printf(100, "%s──┤ ︎Pipeline Done in %s5.642s%s ⚡︎├────────────────────────────────────%s", cBlue, cGreen, cBlue, cReset)
+	newline()
+	newline()
 }
 
 func printDockerOutput(factor int) {
-	overhead := cMagenta + "    │ " + cReset
-
-	newline()
 	printf(100*factor, `%sStep 1/6 : FROM iron/go:dev%s`, stream, linebreak)
 	printf(100*factor, `%s  ---> bc624028dcfb%s`, stream, linebreak)
 	printf(100*factor, `%sStep 2/6 : ADD . /%s`, stream, linebreak)
 	printf(100*factor, `%s  ---> Using cache%s`, stream, linebreak)
 	printf(100*factor, `%s  ---> b8fe67c603de%s`, stream, linebreak)
-	printf(200*factor, `%sStep 3/6 : RUN ["sh", "-c", "cd /go/src/github.com/windmilleng/app-frontend; go get ./..."]%s`, overhead, linebreak)
-	printf(100*factor, `%s  ---> Using cache%s`, overhead, linebreak)
-	printf(100*factor, `%s  ---> da097bd0eea4%s`, overhead, linebreak)
-	printf(300*factor, `%sStep 4/6 : RUN ["sh", "-c", "mkdir -p /app"]%s`, overhead, linebreak)
-	printf(100*factor, `%s  ---> Using cache%s`, overhead, linebreak)
-	printf(100*factor, `%s  ---> 5a14cca8a61c%s`, overhead, linebreak)
-	printf(200*factor, `%sStep 5/6 : RUN ["sh", "-c", "cd /go/src/github.com/windmilleng/app-frontend; go build -o server; cp server /app/"]%s`, overhead, linebreak)
-	printf(100*factor, `%s  ---> Using cache%s`, overhead, linebreak)
-	printf(100*factor, `%s  ---> 0008e43da141%s`, overhead, linebreak)
+	printf(200*factor, `%sStep 3/6 : RUN ["sh", "-c", "cd /go/src/github.com/windmilleng/app-frontend; go get ./..."]%s`, stream, linebreak)
+	printf(100*factor, `%s  ---> Using cache%s`, stream, linebreak)
+	printf(100*factor, `%s  ---> da097bd0eea4%s`, stream, linebreak)
+	printf(300*factor, `%sStep 4/6 : RUN ["sh", "-c", "mkdir -p /app"]%s`, stream, linebreak)
+	printf(100*factor, `%s  ---> Using cache%s`, stream, linebreak)
+	printf(100*factor, `%s  ---> 5a14cca8a61c%s`, stream, linebreak)
+	printf(200*factor, `%sStep 5/6 : RUN ["sh", "-c", "cd /go/src/github.com/windmilleng/app-frontend; go build -o server; cp server /app/"]%s`, stream, linebreak)
+	printf(100*factor, `%s  ---> Using cache%s`, stream, linebreak)
+	printf(100*factor, `%s  ---> 0008e43da141%s`, stream, linebreak)
 	printf(200*factor, `%sStep 6/6 : ENTRYPOINT ["sh", "-c", "/app/server"]%s`, stream, linebreak)
 	printf(100*factor, `%s  ---> Using cache%s`, stream, linebreak)
 	printf(100*factor, `%s  ---> 2917b4065035%s`, stream, linebreak)
-	newline()
 }
 
 func printK8sOutput() {
-	newline()
 	printf(100, `%sservice "devel-hanyu-lb-blorgly-be" configured%s`, stream, linebreak)
 	printf(100, `%sdeployment "devel-hanyu-blorgly-be" configured%s`, stream, linebreak)
-	newline()
 }
 
 func newline() {
