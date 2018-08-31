057e53b5f2ef96322db16ffacd0e9e5debe474e2
Han Yu <han@windmill.engineering>
2018-08-31 16:06:04 -0400

    buildkit: super simple test case for print

--- internal/build/buildkit_printer.go
+++ internal/build/buildkit_printer.go
@@ -1,9 +1,24 @@
 package build
 
-import "io"
+import (
+	"io"
+	"strings"
+
+	digest "github.com/opencontainers/go-digest"
+)
 
 type buildkitPrinter struct {
 	output io.Writer
+}
+
+type vertex struct {
+	digest digest.Digest
+	name   string
+}
+
+type vertexLog struct {
+	vertex digest.Digest
+	msg    []byte
 }
 
 func newBuildkitPrinter(output io.Writer) buildkitPrinter {
@@ -12,6 +27,16 @@ func newBuildkitPrinter(output io.Writer) buildkitPrinter {
 	}
 }
 
-func (b *buildkitPrinter) parseAndPrint(vertexes []*vertex, logs []*log) error {
+func (b *buildkitPrinter) parseAndPrint(vertexes []*vertex, logs []*vertexLog) error {
 
+	for _, v := range vertexes {
+		b.output.Write([]byte(strings.Replace(v.name, "/bin/sh -c", "RUN:", 1)))
+	}
+
+	for _, l := range logs {
+		b.output.Write([]byte("\n→ ERROR: "))
+		b.output.Write(l.msg)
+	}
+
+	return nil
 }
--- internal/build/buildkit_printer_test.go
+++ internal/build/buildkit_printer_test.go
@@ -9,12 +9,25 @@ func TestBuildkitPrinter(t *testing.T) {
 	output := &strings.Builder{}
 	p := newBuildkitPrinter(output)
 
+	vertexes := []*vertex{
+		{
+			digest: "sha1234234234234",
+			name:   `/bin/sh -c (>&2 echo "error"; exit 1)`,
+		},
+	}
+	logs := []*vertexLog{
+		{
+			vertex: "sha1234234234234",
+			msg:    []byte("error"),
+		},
+	}
+
 	p.parseAndPrint(vertexes, logs)
 
 	expected := `RUN: (>&2 echo "error"; exit 1)
-	→ ERROR: error`
+→ ERROR: error`
 
 	if output.String() != expected {
-		t.Errorf("Expected %s, got %s", expected, output.String())
+		t.Errorf("Expected %s. Got %s", expected, output.String())
 	}
 }
