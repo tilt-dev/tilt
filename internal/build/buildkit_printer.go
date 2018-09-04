cb7987718bfc6b9b94fa35d8b072dcb47ef7bd40
Han Yu <han@windmill.engineering>
2018-09-04 17:47:48 -0400

    buildkit: fix bug where logs were overwritten

--- internal/build/buildkit_printer.go
+++ internal/build/buildkit_printer.go
@@ -29,6 +29,8 @@ type vertexAndLogs struct {
 	logs   []*vertexLog
 }
 
+var logDigest digest.Digest
+
 func newBuildkitPrinter(output io.Writer) *buildkitPrinter {
 	return &buildkitPrinter{
 		output: output,
@@ -37,19 +39,24 @@ func newBuildkitPrinter(output io.Writer) *buildkitPrinter {
 }
 
 func (b *buildkitPrinter) parse(vertexes []*vertex, logs []*vertexLog) {
+
 	for _, v := range vertexes {
+		if _, ok := b.vMap[v.digest]; ok {
+			continue
+		}
+
 		b.vMap[v.digest] = &vertexAndLogs{
 			vertex: v,
+			logs:   []*vertexLog{},
 		}
 	}
 
 	for _, l := range logs {
-		if val, ok := b.vMap[l.vertex]; ok {
+		if _, ok := b.vMap[l.vertex]; ok {
+			logDigest = l.vertex
 			if len(l.msg) > 0 {
-				b.vMap[l.vertex] = &vertexAndLogs{
-					vertex: val.vertex,
-					logs:   append(val.logs, l),
-				}
+				vl := b.vMap[l.vertex]
+				vl.logs = append(vl.logs, l)
 			}
 		}
 	}
@@ -61,20 +68,18 @@ func (b *buildkitPrinter) print() error {
 		cmdPrefix := "/bin/sh -c "
 		name := strings.TrimPrefix(v.vertex.name, cmdPrefix)
 
-		if !strings.HasPrefix(v.vertex.name, cmdPrefix) {
-			continue
-		}
-
-		if v.vertex.error != "" {
-			for _, l := range v.logs {
-				if len(l.msg) > 0 {
-					msg := fmt.Sprintf("%s  → ERROR: %s\n", buildPrefix, l.msg)
-					_, err := b.output.Write([]byte(msg))
-					if err != nil {
-						return err
-					}
+		for _, l := range v.logs {
+			if len(l.msg) > 0 {
+				msg := fmt.Sprintf("%s  → ERROR: %s\n", buildPrefix, l.msg)
+				_, err := b.output.Write([]byte(msg))
+				if err != nil {
+					return err
 				}
 			}
+		}
+
+		if !strings.HasPrefix(v.vertex.name, cmdPrefix) {
+			continue
 		}
 
 		msg := fmt.Sprintf("%sRUN: %s\n", buildPrefix, name)
