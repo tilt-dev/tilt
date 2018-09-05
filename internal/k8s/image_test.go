1d59f95b881fa32c9a6e1cee2cb907fce31fd17b
Dan Miller <dan@dmiller.io>
2018-09-05 14:10:49 -0400

    k8s: remove command from containers [ch120] (#244)

--- internal/k8s/image_test.go
+++ internal/k8s/image_test.go
@@ -204,6 +204,79 @@ func TestInjectDigestBlorgBackendYAML(t *testing.T) {
 	}
 }
 
+func TestRemoveCommandBlorgBackendYAML(t *testing.T) {
+	entities, err := ParseYAMLFromString(BlorgBackendYAML)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(entities) != 2 {
+		t.Fatalf("Unexpected entities: %+v", entities)
+	}
+
+	entity := entities[1]
+
+	ref, err := reference.ParseNamed("gcr.io/blorg-dev/blorg-backend")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	newEntity, replaced, err := RemoveCommand(entity, ref)
+	if err != nil {
+		t.Fatal(err)
+
+		if !replaced {
+			t.Errorf("Expected replaced: true. Actual: %v", replaced)
+		}
+	}
+
+	result, err := SerializeYAML([]K8sEntity{newEntity})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if strings.Contains(result, "command:") {
+		t.Errorf("image has command stanza: %s", result)
+	}
+}
+
+func TestDontRemoveCommandIfContainerNameDoesntMatch(t *testing.T) {
+	entities, err := ParseYAMLFromString(MultipleContainersYAML)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(entities) != 1 {
+		t.Fatalf("Unexpected entities: %+v", entities)
+	}
+
+	entity := entities[0]
+
+	ref, err := reference.ParseNamed("gcr.io/blorg-dev/backend")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	newEntity, replaced, err := RemoveCommand(entity, ref)
+	if err != nil {
+		t.Fatal(err)
+
+		if replaced {
+			t.Errorf("Expected replaced: false. Actual: %v", replaced)
+		}
+	}
+
+	result, err := SerializeYAML([]K8sEntity{newEntity})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !strings.Contains(result, "command:") {
+		t.Errorf("image does not has command stanza: %s", result)
+	}
+
+}
+
 // Returns: the new entity, whether anything was replaced, and an error.
 func InjectImageDigestWithStrings(entity K8sEntity, original string, newDigest string, policy v1.PullPolicy) (K8sEntity, bool, error) {
 	originalRef, err := reference.ParseNamed(original)
