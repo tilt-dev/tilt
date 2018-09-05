bd64e8678688dbe196d2a697166d3d1901ace9c5
Dan Miller <dmiller@windmill.engineering>
2018-09-05 12:31:51 -0400

    build: buildkit printer streams output [ch100]

--- internal/build/buildkit_printer_test.go
+++ internal/build/buildkit_printer_test.go
@@ -1,9 +1,13 @@
 package build
 
 import (
+	"fmt"
+	"io/ioutil"
 	"strings"
 	"testing"
 )
+
+const writeGoldenMaster = false
 
 func TestBuildkitPrinter(t *testing.T) {
 	output := &strings.Builder{}
@@ -11,14 +15,40 @@ func TestBuildkitPrinter(t *testing.T) {
 
 	vertexes := []*vertex{
 		{
-			digest: "sha1234234234234",
-			name:   `/bin/sh -c (>&2 echo "error"; exit 1)`,
+			digest: "sha8234234546454",
+			name:   "/bin/sh -c make",
 			error:  "",
 		},
 		{
-			digest: "sha8234234546454",
-			name:   "/bin/sh -c echo hi",
+			digest:  "sha8234234546454",
+			name:    "/bin/sh -c make",
+			error:   "",
+			started: true,
+		},
+		{
+			digest:    "sha8234234546454",
+			name:      "/bin/sh -c make",
+			error:     "",
+			started:   true,
+			completed: true,
+		},
+		{
+			digest: "sha1234234234234",
+			name:   `/bin/sh -c (>&2 echo "hi")`,
 			error:  "",
+		},
+		{
+			digest:  "sha1234234234234",
+			name:    `/bin/sh -c (>&2 echo "hi")`,
+			error:   "",
+			started: true,
+		},
+		{
+			digest:    "sha1234234234234",
+			name:      `/bin/sh -c (>&2 echo "hi")`,
+			error:     "context canceled",
+			started:   true,
+			completed: true,
 		},
 		{
 			digest: "sha82342xxxx454",
@@ -26,15 +56,23 @@ func TestBuildkitPrinter(t *testing.T) {
 			error:  "",
 		},
 		{
-			digest: "sha1234234234234",
-			name:   `/bin/sh -c (>&2 echo "error"; exit 1)`,
-			error:  "",
+			digest:  "sha82342xxxx454",
+			name:    "docker-image://docker.io/blah",
+			error:   "",
+			started: true,
+		},
+		{
+			digest:    "sha1234234234234",
+			name:      `/bin/sh -c (>&2 echo "hi")`,
+			error:     "",
+			started:   true,
+			completed: true,
 		},
 	}
 	logs := []*vertexLog{
 		{
 			vertex: "sha1234234234234",
-			msg:    []byte("error"),
+			msg:    []byte("hi"),
 		},
 		{
 			vertex: "sha8234234546454",
@@ -42,19 +80,25 @@ func TestBuildkitPrinter(t *testing.T) {
 		},
 	}
 
-	p.parse(vertexes, logs)
-
-	expected := `    ╎ RUN: (>&2 echo "error"; exit 1)
-    ╎   → ERROR: error
-    ╎ RUN: echo hi
-`
-
-	err := p.print()
+	err := p.parseAndPrint(vertexes, logs)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if output.String() != expected {
+	d1 := []byte(output.String())
+	gmPath := fmt.Sprintf("testdata/%s_master", t.Name())
+	if writeGoldenMaster {
+		err := ioutil.WriteFile(gmPath, d1, 0644)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+	expected, err := ioutil.ReadFile(gmPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if output.String() != string(expected) {
 		t.Errorf("EXPECTED:\n%s\nGOT:\n%s\n", expected, output.String())
 	}
 }
