8a28f55887f759ebb73e483d0034faee0418725a
Dan Miller <dmiller@windmill.engineering>
2018-09-07 16:48:34 -0400

    tiltfile: swap order arguments to add()

--- internal/tiltfile/skylarkTypes.go
+++ internal/tiltfile/skylarkTypes.go
@@ -97,12 +97,12 @@ func runDockerImageCmd(thread *skylark.Thread, fn *skylark.Builtin, args skylark
 }
 
 func addMount(thread *skylark.Thread, fn *skylark.Builtin, args skylark.Tuple, kwargs []skylark.Tuple) (skylark.Value, error) {
-	var mountPoint string
 	var gitRepo gitRepo
+	var mountPoint string
 	if len(fn.Receiver().(*dockerImage).cmds) > 0 {
 		return nil, errors.New("add mount before run command")
 	}
-	err := skylark.UnpackArgs(fn.Name(), args, kwargs, "mount_point", &mountPoint, "local_git_repo", &gitRepo)
+	err := skylark.UnpackArgs(fn.Name(), args, kwargs, "local_git_repo", &gitRepo, "mount_point", &mountPoint)
 	if err != nil {
 		return nil, err
 	}
