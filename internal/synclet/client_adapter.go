6ad425a48b62662540daa0dcaafdfd30f52dbf99
Matt Landis <matt@windmill.engineering>
2018-09-19 15:08:31 -0400

    tilt: stream synclet logs back to client

--- internal/synclet/client_adapter.go
+++ internal/synclet/client_adapter.go
@@ -2,6 +2,11 @@ package synclet
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"io"
+
+	"github.com/windmilleng/tilt/internal/logger"
 
 	"github.com/docker/distribution/reference"
 	"github.com/windmilleng/tilt/internal/k8s"
@@ -28,6 +33,26 @@ func NewGRPCClient(conn *grpc.ClientConn) *SyncletCli {
 	return &SyncletCli{del: proto.NewSyncletClient(conn), conn: conn}
 }
 
+func protoLogLevelToLevel(protoLevel proto.LogLevel) (logger.Level, error) {
+	var level logger.Level
+	switch protoLevel {
+	case proto.LogLevel_INFO:
+		level = logger.InfoLvl
+	case proto.LogLevel_VERBOSE:
+		level = logger.VerboseLvl
+	case proto.LogLevel_DEBUG:
+		level = logger.DebugLvl
+	default:
+		return logger.InfoLvl, fmt.Errorf("unknown log level '%v'", protoLevel)
+	}
+
+	return level, nil
+}
+
+func newProtoContext(ctx context.Context) *proto.Context {
+	return &proto.Context{LogColorsEnabled: logger.Get(ctx).SupportsColor()}
+}
+
 func (s *SyncletCli) UpdateContainer(
 	ctx context.Context,
 	containerId k8s.ContainerID,
@@ -41,18 +66,41 @@ func (s *SyncletCli) UpdateContainer(
 		protoCmds = append(protoCmds, &proto.Cmd{Argv: cmd.Argv})
 	}
 
-	_, err := s.del.UpdateContainer(ctx, &proto.UpdateContainerRequest{
+	stream, err := s.del.UpdateContainer(ctx, &proto.UpdateContainerRequest{
+		Context:       newProtoContext(ctx),
 		ContainerId:   containerId.String(),
 		TarArchive:    tarArchive,
 		FilesToDelete: filesToDelete,
 		Commands:      protoCmds,
 	})
 
+	if err != nil {
+		return err
+	}
+
+	for {
+		reply, err := stream.Recv()
+
+		if err == io.EOF {
+			return nil
+		} else if err != nil {
+			return err
+		}
+
+		level, err := protoLogLevelToLevel(reply.LogMessage.Level)
+		if err != nil {
+			return err
+		}
+
+		logger.Get(ctx).Write(level, string(reply.LogMessage.Message))
+	}
+
 	return err
 }
 
 func (s *SyncletCli) ContainerIDForPod(ctx context.Context, podID k8s.PodID, imageID reference.NamedTagged) (k8s.ContainerID, error) {
-	reply, err := s.del.GetContainerIdForPod(ctx, &proto.GetContainerIdForPodRequest{
+	stream, err := s.del.GetContainerIdForPod(ctx, &proto.GetContainerIdForPodRequest{
+		Context: newProtoContext(ctx),
 		PodId:   podID.String(),
 		ImageId: imageID.String(),
 	})
@@ -60,7 +108,27 @@ func (s *SyncletCli) ContainerIDForPod(ctx context.Context, podID k8s.PodID, ima
 		return "", err
 	}
 
-	return k8s.ContainerID(reply.ContainerId), nil
+	for {
+		reply, err := stream.Recv()
+
+		if err == io.EOF {
+			return k8s.ContainerID(""), errors.New("internal error: GetContainerIdForPod reached eof without returning either an error or a container id")
+		} else if err != nil {
+			return k8s.ContainerID(""), err
+		}
+
+		switch x := reply.Content.(type) {
+		case *proto.GetContainerIdForPodReply_ContainerId:
+			return k8s.ContainerID(x.ContainerId), nil
+		case *proto.GetContainerIdForPodReply_Message:
+			level, err := protoLogLevelToLevel(x.Message.Level)
+			if err != nil {
+				return k8s.ContainerID(""), err
+			}
+
+			logger.Get(ctx).Write(level, string(x.Message.Message))
+		}
+	}
 }
 
 func (s *SyncletCli) Close() error {
--- internal/synclet/server_adapter.go
+++ internal/synclet/server_adapter.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/docker/distribution/reference"
 	"github.com/windmilleng/tilt/internal/k8s"
+	"github.com/windmilleng/tilt/internal/logger"
+	"github.com/windmilleng/tilt/internal/output"
 
 	"github.com/windmilleng/tilt/internal/model"
 	"github.com/windmilleng/tilt/internal/synclet/proto"
@@ -21,30 +23,84 @@ func NewGRPCServer(del *Synclet) *GRPCServer {
 
 var _ proto.SyncletServer = &GRPCServer{}
 
-func (s *GRPCServer) GetContainerIdForPod(ctx context.Context, req *proto.GetContainerIdForPodRequest) (*proto.GetContainerIdForPodReply, error) {
+func logLevelToProto(level logger.Level) (proto.LogLevel, error) {
+	var protoLevel proto.LogLevel
+	switch level {
+	case logger.InfoLvl:
+		protoLevel = proto.LogLevel_INFO
+	case logger.VerboseLvl:
+		protoLevel = proto.LogLevel_VERBOSE
+	case logger.DebugLvl:
+		protoLevel = proto.LogLevel_DEBUG
+	default:
+		return proto.LogLevel_INFO, fmt.Errorf("unknown log level '%v'", level)
+	}
+
+	return protoLevel, nil
+}
+
+func makeContext(ctx context.Context, pCtx *proto.Context, f func(m *proto.LogMessage) error) (context.Context, error) {
+	writeLog := func(level logger.Level, bytes []byte) error {
+		protoLevel, err := logLevelToProto(level)
+		if err != nil {
+			return err
+		}
+
+		logMessage := &proto.LogMessage{Level: protoLevel, Message: bytes}
+		return f(logMessage)
+	}
+
+	l := logger.NewFuncLogger(pCtx.LogColorsEnabled, writeLog)
+
+	// TODO(matt) making a new outputter here is hacky - since outputter is stateful, someone might make
+	// rely on state persisting across service boundaries
+	ret := output.WithOutputter(logger.WithLogger(ctx, l), output.NewOutputter(l))
+	return ret, nil
+}
+
+func (s *GRPCServer) GetContainerIdForPod(req *proto.GetContainerIdForPodRequest, server proto.Synclet_GetContainerIdForPodServer) error {
+
+	send := func(m *proto.LogMessage) error {
+		return server.Send(&proto.GetContainerIdForPodReply{Content: &proto.GetContainerIdForPodReply_Message{Message: m}})
+	}
+
+	ctx, err := makeContext(server.Context(), req.Context, send)
+	if err != nil {
+		return err
+	}
+
 	name, err := reference.ParseNamed(req.ImageId)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	ref, ok := name.(reference.NamedTagged)
 	if !ok {
-		return nil, fmt.Errorf("Expected a tagged ref: %s", req.ImageId)
+		return fmt.Errorf("Expected a tagged ref: %s", req.ImageId)
 	}
 
 	containerID, err := s.del.ContainerIDForPod(ctx, k8s.PodID(req.PodId), ref)
-
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	return &proto.GetContainerIdForPodReply{ContainerId: string(containerID)}, nil
+	return server.Send(&proto.GetContainerIdForPodReply{Content: &proto.GetContainerIdForPodReply_ContainerId{ContainerId: string(containerID)}})
 }
 
-func (s *GRPCServer) UpdateContainer(ctx context.Context, req *proto.UpdateContainerRequest) (*proto.UpdateContainerReply, error) {
+func (s *GRPCServer) UpdateContainer(req *proto.UpdateContainerRequest, server proto.Synclet_UpdateContainerServer) error {
 	var commands []model.Cmd
 	for _, cmd := range req.Commands {
 		commands = append(commands, model.Cmd{Argv: cmd.Argv})
 	}
-	return &proto.UpdateContainerReply{}, s.del.UpdateContainer(ctx, k8s.ContainerID(req.ContainerId), req.TarArchive, req.FilesToDelete, commands)
+
+	send := func(m *proto.LogMessage) error {
+		return server.Send(&proto.UpdateContainerReply{LogMessage: m})
+	}
+
+	ctx, err := makeContext(server.Context(), req.Context, send)
+	if err != nil {
+		return err
+	}
+
+	return s.del.UpdateContainer(ctx, k8s.ContainerID(req.ContainerId), req.TarArchive, req.FilesToDelete, commands)
 }
