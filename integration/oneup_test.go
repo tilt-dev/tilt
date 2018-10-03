c9bc063eeeaa8e489f3d0d3bc3440d34c2c0b9aa
Nick Santos <nicholas.j.santos@gmail.com>
2018-10-03 15:55:09 -0400

    circleci: spin up a test k8s cluster and run tests against it

--- integration/oneup_test.go
+++ integration/oneup_test.go
@@ -17,13 +17,13 @@ func TestOneUp(t *testing.T) {
 	// ForwardPort will fail if all the pods are not ready.
 	// TODO(nick): We should make port-forwarding a primitive in the
 	// Tiltfile since this seems generally useful, then get rid of this code.
-	ctx, cancel := context.WithTimeout(f.ctx, 20*time.Second)
+	ctx, cancel := context.WithTimeout(f.ctx, time.Minute)
 	defer cancel()
 	f.WaitForAllPodsReady(ctx, "app=oneup")
 
 	f.ForwardPort("deployment/oneup", "31234:8000")
 
-	ctx, cancel = context.WithTimeout(f.ctx, 20*time.Second)
+	ctx, cancel = context.WithTimeout(f.ctx, time.Minute)
 	defer cancel()
 	f.CurlUntil(ctx, "http://localhost:31234", "🍄 One-Up! 🍄")
 }
