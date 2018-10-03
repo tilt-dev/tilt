0fae7f0fb25f60bf4e4c284224b40041fe485845
Matt Landis <landism@users.noreply.github.com>
2018-10-03 18:02:57 -0400

    tilt: add hud package (#462)

---
+++ internal/hud/renderer.go
@@ -0,0 +1,11 @@
+package hud
+
+import "github.com/windmilleng/tilt/internal/hud/view"
+
+type Renderer struct {
+}
+
+func (r Renderer) Render(v view.View) error {
+	// TODO: draw v
+	return nil
+}
---
+++ internal/hud/view/view.go
@@ -0,0 +1,29 @@
+package view
+
+import "time"
+
+type Resource struct {
+	Name                    string
+	DirectoryWatched        string
+	LatestFileChanges       []string
+	TimeSinceLastFileChange time.Duration
+	Status                  ResourceStatus
+
+	// e.g., "CrashLoopBackOff", "No Pod found", "Build error"
+	StatusDesc string
+}
+
+type ResourceStatus int
+
+const (
+	// something is wrong and requires investigation, e.g. the build failed or the pod is crashlooping
+	ResourceStatusBroken ResourceStatus = iota
+	// tilt has observed changes since the last deploy, and is in the process of rebuilding and deploying
+	ResourceStatusStale
+	// the latest code is currently running
+	ResourceStatusFresh
+)
+
+type View struct {
+	Resources []Resource
+}
