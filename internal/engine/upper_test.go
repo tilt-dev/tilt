c774532cf8ee64ca4947b9bb4cb39853d84447fa
Maia McCormick <maia.mcc@gmail.com>
2018-10-18 10:54:39 -0400

    engine: don't image build if config file changes but doesn't result in manifest change (#561)
    * engine: don't image build if config file changes but doesn't result in manifest change
    
    * fix lint

--- internal/engine/upper_test.go
+++ internal/engine/upper_test.go
@@ -547,10 +547,7 @@ func TestNoOpChangeToDockerfile(t *testing.T) {
 	// incremental build.
 	call = <-f.b.calls
 	assert.Equal(t, "foobar", string(call.manifest.Name))
-	assert.ElementsMatch(t, []string{
-		f.JoinPath("Dockerfile"),
-		f.JoinPath("random_file.go"),
-	}, call.state.FilesChanged())
+	assert.ElementsMatch(t, []string{f.JoinPath("random_file.go")}, call.state.FilesChanged())
 
 	f.WaitUntil("all builds complete", func(es store.EngineState) bool {
 		return es.CurrentlyBuilding == ""
@@ -560,7 +557,7 @@ func TestNoOpChangeToDockerfile(t *testing.T) {
 	assert.Nil(t, err)
 	f.assertAllBuildsConsumed()
 
-	assert.Contains(t, f.LogLines(), "Manifest foobar hasn't changed, not rebuilding")
+	assert.Contains(t, strings.Join(f.LogLines(), "\n"), "manifest foobar hasn't changed")
 }
 
 func TestRebuildDockerfileFailed(t *testing.T) {
