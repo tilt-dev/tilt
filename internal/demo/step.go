ca4dd0abdde2c187050791ae3b3f6948b2c1f8b2
Nick Santos <nicholas.j.santos@gmail.com>
2018-10-18 14:57:08 -0400

    demo: add a branch flag to help development (#571)

--- internal/demo/step.go
+++ internal/demo/step.go
@@ -7,7 +7,9 @@ const Pause = 3 * time.Second
 type Step struct {
 	Narration       string
 	Command         string
+	ChangeBranch    bool
 	CreateManifests bool
+	Pause           time.Duration
 }
 
 var steps = []Step{
@@ -17,6 +19,10 @@ var steps = []Step{
 	Step{
 		Narration: "\t📚  git clone https://github.com/windmilleng/tiltdemo",
 		Command:   "git clone https://github.com/windmilleng/tiltdemo $(pwd)",
+	},
+	Step{
+		Narration:    "\t🌲  Changing branch",
+		ChangeBranch: true,
 	},
 	Step{
 		Narration:       "\t🎂  Building and deploying demo server",
