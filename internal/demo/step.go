239d41932cf819fe52aba26a57043a2efd16fcd2
Nick Santos <nicholas.j.santos@gmail.com>
2018-10-18 16:36:13 -0400

    demo: flesh out the demo script (#577)

--- internal/demo/step.go
+++ internal/demo/step.go
@@ -5,11 +5,13 @@ import "time"
 const Pause = 3 * time.Second
 
 type Step struct {
-	Narration       string
-	Command         string
-	ChangeBranch    bool
-	CreateManifests bool
-	Pause           time.Duration
+	Narration         string
+	Command           string
+	ChangeBranch      bool
+	CreateManifests   bool
+	WaitForHealthy    bool
+	WaitForBuildError bool
+	WaitForPodRestart bool
 }
 
 var steps = []Step{
@@ -27,8 +29,44 @@ var steps = []Step{
 	Step{
 		Narration:       "\t🎂  Building and deploying demo server",
 		CreateManifests: true,
+		WaitForHealthy:  true,
 	},
 	Step{
 		Narration: "\t🎉  Deployment success!",
+	},
+	Step{
+		Narration:         "\t⚒️  Introducing build error on demoserver1",
+		Command:           "sed -i -e 's!// \\(.*// tilt:BUILD_ERROR\\)!\\1!' cmd/demoserver1/main.go",
+		WaitForBuildError: true,
+	},
+	Step{
+		Narration: "\t😱  Oh no!",
+	},
+	Step{
+		Narration:      "\t🚑  Fixing build error",
+		Command:        "sed -i -e 's!^\\(.*// tilt:BUILD_ERROR\\)!// \\1!' cmd/demoserver1/main.go",
+		WaitForHealthy: true,
+	},
+	Step{
+		Narration: "\t😌  Whew! Back to normal...",
+	},
+	Step{
+		Narration:         "\t⚒️  Introducing panic on demoserver1",
+		Command:           "sed -i -e 's!// \\(.*// tilt:STARTUP_PANIC\\)!\\1!' cmd/demoserver1/main.go",
+		WaitForPodRestart: true,
+	},
+	Step{
+		Narration: "\t😱  Oh no!",
+	},
+	Step{
+		Narration:      "\t🚒  Fixing panic",
+		Command:        "sed -i -e 's!^\\(.*// tilt:STARTUP_PANIC\\)!// \\1!' cmd/demoserver1/main.go",
+		WaitForHealthy: true,
+	},
+	Step{
+		Narration: "\t😌  Whew! Back to normal...",
+	},
+	Step{
+		Narration: "\t😎  Demo Complete!",
 	},
 }
