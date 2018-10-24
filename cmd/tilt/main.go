06e2186e5225b40283cc37a2593eda49fcd620ba
Nick Santos <nicholas.j.santos@gmail.com>
2018-10-24 15:36:12 -0400

    cmd: make tilt binary compatible with goreleaser

--- cmd/tilt/main.go
+++ cmd/tilt/main.go
@@ -4,6 +4,15 @@ import (
 	"github.com/windmilleng/tilt/internal/cli"
 )
 
+// Magic variables set by goreleaser
+var version string
+var commit string
+var date string
+
 func main() {
+	cli.SetBuildInfo(cli.BuildInfo{
+		Version: version,
+		Date:    date,
+	})
 	cli.Execute()
 }
