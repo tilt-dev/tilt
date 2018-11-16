0b91e1ce6de8aea02b965558e7818c20d77031bd
Nick Santos <nicholas.j.santos@gmail.com>
2018-11-16 17:47:50 -0500

    tilt: disable glog to avoid problems where TTY is garbage (#722)

--- cmd/tilt/main.go
+++ cmd/tilt/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"flag"
+	"log"
+
 	"github.com/windmilleng/tilt/internal/cli"
 )
 
@@ -10,9 +13,27 @@ var commit string
 var date string
 
 func main() {
+	disableGlog()
+
 	cli.SetBuildInfo(cli.BuildInfo{
 		Version: version,
 		Date:    date,
 	})
 	cli.Execute()
+}
+
+// HACK(nick): The Kubernetes libs we use sometimes use glog to log things to
+// os.Stderr. There are no API hooks to configure this. And printing to Stderr
+// scrambles the HUD tty.
+//
+// Fortunately, glog does initialize itself from flags, so we can backdoor
+// configure it by setting our own flags. Don't do this at home! This works
+// OK because we use Cobra for flags.
+func disableGlog() {
+	err := flag.CommandLine.Parse([]string{
+		"--stderrthreshold", "FATAL",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
 }
