465979d4d003032fff5675169dfef42970c6fdc6
Matt Landis <matt@windmill.engineering>
2018-11-28 17:37:26 -0500

    tilt: full redraw on shift-R

--- internal/hud/hud.go
+++ internal/hud/hud.go
@@ -7,9 +7,8 @@ import (
 	"time"
 
 	"github.com/pkg/browser"
-	"github.com/windmilleng/tcell"
-
 	"github.com/pkg/errors"
+	"github.com/windmilleng/tcell"
 
 	"github.com/windmilleng/tilt/internal/hud/view"
 	"github.com/windmilleng/tilt/internal/store"
@@ -144,6 +143,8 @@ func (h *Hud) handleScreenEvent(ctx context.Context, dispatch func(action store.
 				h.Close()
 				dispatch(NewExitAction(nil))
 				return true
+			case r == 'R':
+				h.r.screen.Sync()
 			}
 		case tcell.KeyUp:
 			h.activeScroller().Up()
