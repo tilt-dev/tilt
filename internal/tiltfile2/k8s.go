cdde16696166412cdc5454cf9845425850d1d3cc
Dan Bentley <gitspam@danbentley.com>
2018-12-04 09:54:46 -0500

    wip

--- internal/tiltfile2/k8s.go
+++ internal/tiltfile2/k8s.go
@@ -1,7 +1,12 @@
 package tiltfile2
 
 import (
+	"fmt"
+
+	"github.com/google/skylark"
+
 	"github.com/windmilleng/tilt/internal/k8s"
+	"github.com/windmilleng/tilt/internal/model"
 )
 
 // k8sResource
@@ -10,7 +15,158 @@ type k8sResource struct {
 	k8s      []k8s.K8sEntity
 	imageRef string
 
-	// FIXME(dbentley): port forwards
+	portForwards []portForward
 
 	expandedFrom string // this resource was not declared but expanded from another resource
+}
+
+func (s *tiltfileState) k8sResource(thread *skylark.Thread, fn *skylark.Builtin, args skylark.Tuple, kwargs []skylark.Tuple) (skylark.Value, error) {
+	var name string
+	var yamlValue skylark.Value
+	var portForwardsVal skylark.Value
+
+	if err := skylark.UnpackArgs(fn.Name(), args, kwargs,
+		"name", &name,
+		"yaml?", &yamlValue,
+		"port_forwards?", &portForwardsVal,
+	); err != nil {
+		return nil, err
+	}
+
+	if name == "" {
+		return nil, fmt.Errorf("k8s_resource: name must not be empty")
+	}
+	if s.k8sByName[name] != nil {
+		return nil, fmt.Errorf("resource named %q has already been defined", name)
+	}
+
+	yamlText := ""
+	if b, ok := yamlValue.(*blob); ok {
+		yamlText = b.String()
+	} else {
+
+		yamlPath, err := s.localPathFromSkylarkValue(yamlValue)
+		if err != nil {
+			return nil, err
+		}
+
+		bs, err := s.readFile(yamlPath)
+		if err != nil {
+			return nil, err
+		}
+		yamlText = string(bs)
+	}
+
+	entities, err := k8s.ParseYAMLFromString(yamlText)
+	if err != nil {
+		return nil, err
+	}
+
+	portForwards, err := s.convertPortForwards(name, portForwardsVal)
+	if err != nil {
+		return nil, err
+	}
+
+	r := &k8sResource{
+		name: name,
+		k8s:  entities,
+
+		portForwards: portForwards,
+	}
+	s.k8s = append(s.k8s, r)
+	s.k8sByName[name] = r
+	return skylark.None, nil
+}
+
+func (s *tiltfileState) convertPortForwards(name string, val skylark.Value) ([]portForward, error) {
+	if val == nil {
+		return nil, nil
+	}
+	switch val := val.(type) {
+	case skylark.Int:
+		pf, err := intToPortForward(val)
+		if err != nil {
+			return nil, err
+		}
+		return []portForward{pf}, nil
+	case portForward:
+		return []portForward{val}, nil
+	case skylark.Sequence:
+		var result []portForward
+		it := val.Iterate()
+		defer it.Done()
+		var i skylark.Value
+		for it.Next(&i) {
+			switch i := i.(type) {
+			case skylark.Int:
+				pf, err := intToPortForward(i)
+				if err != nil {
+					return nil, err
+				}
+				result = append(result, pf)
+			case portForward:
+				result = append(result, i)
+			default:
+				return nil, fmt.Errorf("k8s_resource %q: port_forwards arg %v includes element %v which must be an int or a port_forward; is a %T", name, val, i, i)
+			}
+		}
+		return result, nil
+	default:
+		return nil, fmt.Errorf("k8s_resource %q: port_forwards must be an int, a port_forward, or a sequence of those; is a %T", name, val)
+	}
+}
+
+func (s *tiltfileState) portForward(thread *skylark.Thread, fn *skylark.Builtin, args skylark.Tuple, kwargs []skylark.Tuple) (skylark.Value, error) {
+	var local int
+	var container int
+
+	if err := skylark.UnpackArgs(fn.Name(), args, kwargs, "local", &local, "container?", &container); err != nil {
+		return nil, err
+	}
+
+	return portForward{local: local, container: container}, nil
+}
+
+type portForward struct {
+	local     int
+	container int
+}
+
+var _ skylark.Value = portForward{}
+
+func (f portForward) String() string {
+	return fmt.Sprintf("port_forward(%d, %d)", f.local, f.container)
+}
+
+func (f portForward) Type() string {
+	return "port_forward"
+}
+
+func (f portForward) Freeze() {}
+
+func (f portForward) Truth() skylark.Bool {
+	return f.local != 0 && f.container != 0
+}
+
+func (f portForward) Hash() (uint32, error) {
+	return 0, fmt.Errorf("unhashable type: port_forward")
+}
+
+func intToPortForward(i skylark.Int) (portForward, error) {
+	n, ok := i.Int64()
+	if !ok {
+		return portForward{}, fmt.Errorf("portForward value %v is not representable as an int64", i)
+	}
+	if n < 0 || n > 65535 {
+		return portForward{}, fmt.Errorf("portForward value %v is not in the range for a port [0-65535]", n)
+	}
+	return portForward{local: int(n)}, nil
+}
+
+func (s *tiltfileState) portForwardsToDomain(r *k8sResource) []model.PortForward {
+	var result []model.PortForward
+	for _, pf := range r.portForwards {
+		result = append(result, model.PortForward{LocalPort: pf.local, ContainerPort: pf.container})
+	}
+	return result
 }
--- internal/tiltfile2/tiltfile_test.go
+++ internal/tiltfile2/tiltfile_test.go
@@ -3,8 +3,14 @@ package tiltfile2
 import (
 	"bytes"
 	"context"
+	"fmt"
+	"os"
+	"reflect"
+	"sort"
 	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 
 	"github.com/windmilleng/tilt/internal/k8s"
 	"github.com/windmilleng/tilt/internal/k8s/testyaml"
@@ -13,23 +19,19 @@ import (
 	"github.com/windmilleng/tilt/internal/testutils/tempdir"
 )
 
-func TestSimple(t *testing.T) {
+func TestNoTiltfile(t *testing.T) {
 	f := newFixture(t)
 	defer f.tearDown()
 
-	f.file("foo/Dockerfile", "FROM golang:1.10")
-	f.yaml("foo.yaml", deployment("foo", image("gcr.io/foo")))
+	f.loadErrString("no such file")
+}
 
-	f.file("Tiltfile", `
-docker_build('gcr.io/foo', 'foo/Dockerfile')
-k8s_resource('foo', 'foo.yaml')
-`)
+func TestEmpty(t *testing.T) {
+	f := newFixture(t)
+	defer f.tearDown()
 
+	f.file("Tiltfile", "")
 	f.load()
-
-	f.assertManifest("foo",
-		image("gcr.io/foo"),
-		deployment("foo"))
 }
 
 func TestMissingDockerfile(t *testing.T) {
@@ -37,11 +39,180 @@ func TestMissingDockerfile(t *testing.T) {
 	defer f.tearDown()
 
 	f.file("Tiltfile", `
-docker_build('gcr.io/foo', 'foo/Dockerfile')
+docker_build('gcr.io/foo', 'foo')
 k8s_resource('foo', 'foo.yaml')
 `)
 
-	f.loadErrString("Reading path foo/Dockerfile", "no such file or directory")
+	f.loadErrString("foo/Dockerfile", "no such file or directory")
+}
+
+func TestSimple(t *testing.T) {
+	f := newFixture(t)
+	defer f.tearDown()
+
+	f.setupFoo()
+
+	f.file("Tiltfile", `
+docker_build('gcr.io/foo', 'foo')
+k8s_resource('foo', 'foo.yaml')
+`)
+
+	f.load()
+
+	f.assertManifest("foo",
+		db(image("gcr.io/foo")),
+		deployment("foo"))
+	f.assertConfigFiles("Tiltfile", "foo/Dockerfile", "foo.yaml")
+}
+
+func TestFastBuildSimple(t *testing.T) {
+	f := newFixture(t)
+	defer f.tearDown()
+
+	f.setupFoo()
+	f.file("Tiltfile", `
+repo = local_git_repo('.')
+fast_build('gcr.io/foo', 'foo/Dockerfile') \
+  .add(repo.path('foo'), 'src/') \
+  .run("echo hi")
+k8s_resource('foo', 'foo.yaml')
+`)
+	f.load()
+	f.assertManifest("foo",
+		fb(image("gcr.io/foo"), add("foo", "src/"), run("echo hi")),
+		deployment("foo"),
+	)
+	f.assertConfigFiles("Tiltfile", "foo/Dockerfile", "foo.yaml")
+}
+
+func TestFastBuildValidates(t *testing.T) {
+	f := newFixture(t)
+	defer f.tearDown()
+
+	f.setupFoo()
+	f.file("foo/Dockerfile", `
+from golang:1.10
+ADD . .`)
+	f.file("Tiltfile", `
+repo = local_git_repo('.')
+fast_build('gcr.io/foo', 'foo/Dockerfile') \
+  .add(repo.path('foo'), 'src/') \
+  .run("echo hi")
+k8s_resource('foo', 'foo.yaml')
+`)
+	f.loadErrString("base Dockerfile contains an ADD/COPY")
+}
+
+func TestFastBuildRunBeforeAdd(t *testing.T) {
+	f := newFixture(t)
+	defer f.tearDown()
+
+	f.setupFoo()
+	f.file("Tiltfile", `
+repo = local_git_repo('.')
+fast_build('gcr.io/foo', 'foo/Dockerfile') \
+  .run("echo hi") \
+  .add(repo.path('foo'), 'src/')
+k8s_resource('foo', 'foo.yaml')
+`)
+	f.loadErrString("fast_build(\"gcr.io/foo\").add() called after .run()")
+}
+
+func TestVerifiesGitRepo(t *testing.T) {
+	f := newFixture(t)
+	defer f.tearDown()
+	f.file("Tiltfile", "local_git_repo('.')")
+	f.loadErrString("isn't a valid git repo")
+}
+
+func TestLocal(t *testing.T) {
+	f := newFixture(t)
+	defer f.tearDown()
+
+	f.setupFoo()
+
+	f.file("Tiltfile", `
+docker_build('gcr.io/foo', 'foo')
+yaml = local('cat foo.yaml')
+k8s_resource('foo', yaml)
+`)
+
+	f.load()
+
+	f.assertManifest("foo",
+		db(image("gcr.io/foo")),
+		deployment("foo"))
+}
+
+func TestReadFile(t *testing.T) {
+	f := newFixture(t)
+	defer f.tearDown()
+
+	f.setupFoo()
+
+	f.file("Tiltfile", `
+docker_build('gcr.io/foo', 'foo')
+yaml = read_file('foo.yaml')
+k8s_resource('foo', yaml)
+`)
+
+	f.load()
+
+	f.assertManifest("foo",
+		db(image("gcr.io/foo")),
+		deployment("foo"))
+	f.assertConfigFiles("Tiltfile", "foo/Dockerfile", "foo.yaml")
+}
+
+func TestKustomize(t *testing.T) {
+	f := newFixture(t)
+	defer f.tearDown()
+
+	f.setupFoo()
+	f.file("kustomization.yaml", kustomizeFileText)
+	f.file("configMap.yaml", kustomizeConfigMapText)
+	f.file("deployment.yaml", kustomizeDeploymentText)
+	f.file("service.yaml", kustomizeServiceText)
+	f.file("Tiltfile", `
+docker_build("gcr.io/foo", "foo")
+k8s_resource('foo', kustomize("."))
+`)
+	f.load()
+	f.assertManifest("foo", deployment("the-deployment"), numEntities(3))
+	f.assertConfigFiles("Tiltfile", "foo/Dockerfile", "configMap.yaml", "deployment.yaml", "kustomization.yaml", "service.yaml")
+}
+
+type portForwardCase struct {
+	name     string
+	expr     string
+	expected []model.PortForward
+}
+
+func TestPortForward(t *testing.T) {
+	portForwardCases := []portForwardCase{
+		{"value_local", "8000", []model.PortForward{{LocalPort: 8000}}},
+		{"value_both", "port_forward(8001, 443)", []model.PortForward{{LocalPort: 8001, ContainerPort: 443}}},
+		{"list", "[8000, port_forward(8001, 443)]", []model.PortForward{{LocalPort: 8000}, {LocalPort: 8001, ContainerPort: 443}}},
+	}
+
+	for _, c := range portForwardCases {
+		t.Run(c.name, func(t *testing.T) {
+			f := newFixture(t)
+			defer f.tearDown()
+			f.setupFoo()
+			s := `
+docker_build('gcr.io/foo', 'foo')
+k8s_resource('foo', 'foo.yaml', port_forwards=EXPR)
+`
+			s = strings.Replace(s, "EXPR", c.expr, -1)
+			f.file("Tiltfile", s)
+			f.load()
+			f.assertManifest("foo",
+				c.expected,
+				db(image("gcr.io/foo")),
+				deployment("foo"))
+		})
+	}
 }
 
 type fixture struct {
@@ -74,6 +245,10 @@ func (f *fixture) file(path string, contents string) {
 }
 
 type k8sOpts interface{}
+
+func (f *fixture) dockerfile(path string) {
+	f.file(path, "FROM golang:1.10")
+}
 
 func (f *fixture) yaml(path string, entities ...k8sOpts) {
 	var entityObjs []k8s.K8sEntity
@@ -129,6 +304,13 @@ func (f *fixture) loadErrString(msgs ...string) {
 	}
 }
 
+func (f *fixture) gitInit(path string) {
+	if err := os.Mkdir(f.tmp.JoinPath(".git"), os.FileMode(0777)); err != nil {
+		f.t.Fatal(err)
+	}
+}
+
+// assert functions and helpers
 func (f *fixture) assertManifest(name string, opts ...interface{}) model.Manifest {
 	if len(f.manifests) == 0 {
 		f.t.Fatalf("no more manifests; trying to find %q", name)
@@ -139,16 +321,83 @@ func (f *fixture) assertManifest(name string, opts ...interface{}) model.Manifes
 
 	for _, opt := range opts {
 		switch opt := opt.(type) {
-		case imageHelper:
-			if m.DockerRef().Name() != opt.ref {
-				f.t.Fatalf("manifest %v image ref: %q; expected %q", m.Name, m.DockerRef().Name(), opt.ref)
+		case dbHelper:
+			if m.DockerRef().Name() != opt.image.ref {
+				f.t.Fatalf("manifest %v image ref: %q; expected %q", m.Name, m.DockerRef().Name(), opt.image.ref)
+			}
+		case fbHelper:
+			if m.DockerRef().Name() != opt.image.ref {
+				f.t.Fatalf("manifest %v image ref: %q; expected %q", m.Name, m.DockerRef().Name(), opt.image.ref)
+			}
+
+			for _, matcher := range opt.matchers {
+				mounts := m.Mounts
+				steps := m.Steps
+				switch matcher := matcher.(type) {
+				case addHelper:
+					mount := mounts[0]
+					mounts = mounts[1:]
+					if mount.LocalPath != f.tmp.JoinPath(matcher.src) {
+						f.t.Fatalf("manifest %v mount %+v src: %q; expected %q", m.Name, mount, mount.LocalPath, f.tmp.JoinPath(matcher.src))
+					}
+				case runHelper:
+					step := steps[0]
+					steps = steps[1:]
+					assert.Equal(f.t, model.ToShellCmd(matcher.cmd), step.Cmd)
+				default:
+					f.t.Fatalf("unknown fbHelper matcher: %T %v", matcher, matcher)
+				}
 			}
 		case deployHelper:
-
+			found := false
+			for _, e := range f.entities(m) {
+				if e.Kind.Kind == "Deployment" && f.k8sName(e) == opt.name {
+					found = true
+					break
+				}
+			}
+			if !found {
+				f.t.Fatalf("deployment %v not found in yaml %q", opt.name, m.K8sYAML())
+			}
+		case numEntitiesHelper:
+			if opt.num != len(f.entities(m)) {
+				f.t.Fatalf("manifest %v has %v entities in %v; expected %v", m.Name, len(f.entities(m)), m.K8sYAML(), opt.num)
+			}
+		case []model.PortForward:
+			assert.Equal(f.t, opt, m.PortForwards())
+		default:
+			f.t.Fatalf("unexpected arg to assertManifest: %T %v", opt, opt)
 		}
 	}
 	return m
+}
 
+func (f *fixture) assertConfigFiles(filenames ...string) {
+	var expected []string
+	for _, filename := range filenames {
+		expected = append(expected, f.tmp.JoinPath(filename))
+	}
+	sort.Strings(expected)
+	sort.Strings(f.configFiles)
+	assert.Equal(f.t, expected, f.configFiles)
+}
+
+func (f *fixture) entities(m model.Manifest) []k8s.K8sEntity {
+	yamlText := m.K8sYAML()
+	es, err := k8s.ParseYAMLFromString(yamlText)
+	if err != nil {
+		f.t.Fatal(err)
+	}
+	return es
+}
+
+func (f *fixture) k8sName(e k8s.K8sEntity) string {
+	// Every k8s object we care about has is a pointer to a struct with a field ObjectMeta that has a field "Name" that's a string.
+	name := reflect.ValueOf(e.Obj).Elem().FieldByName("ObjectMeta").FieldByName("Name")
+	if !name.IsValid() {
+		return ""
+	}
+	return name.String()
 }
 
 type deployHelper struct {
@@ -162,9 +411,19 @@ func deployment(name string, opts ...interface{}) deployHelper {
 		switch opt := opt.(type) {
 		case imageHelper:
 			r.image = opt.ref
+		default:
+			panic(fmt.Errorf("unexpected arg to deployment: %T %v", opt, opt))
 		}
 	}
 	return r
+}
+
+type numEntitiesHelper struct {
+	num int
+}
+
+func numEntities(num int) numEntitiesHelper {
+	return numEntitiesHelper{num}
 }
 
 type imageHelper struct {
@@ -173,4 +432,48 @@ type imageHelper struct {
 
 func image(ref string) imageHelper {
 	return imageHelper{ref: ref}
+}
+
+// match a docker_build
+type dbHelper struct {
+	image imageHelper
+}
+
+func db(img imageHelper) dbHelper {
+	return dbHelper{img}
+}
+
+type fbHelper struct {
+	image    imageHelper
+	matchers []interface{}
+}
+
+func fb(img imageHelper, opts ...interface{}) fbHelper {
+	return fbHelper{img, opts}
+}
+
+type addHelper struct {
+	src  string
+	dest string
+}
+
+func add(src string, dest string) addHelper {
+	return addHelper{src, dest}
+}
+
+type runHelper struct {
+	cmd string
+}
+
+func run(cmd string) runHelper {
+	return runHelper{cmd}
+}
+
+// useful scenarios to setup
+
+// foo just has one image and one yaml
+func (f *fixture) setupFoo() {
+	f.dockerfile("foo/Dockerfile")
+	f.yaml("foo.yaml", deployment("foo", image("gcr.io/foo")))
+	f.gitInit("")
 }
