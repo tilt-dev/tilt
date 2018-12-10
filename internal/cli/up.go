5872909bd9f75e2bc342b4a3475de624a7cb4c83
Maia McCormick <maia.mcc@gmail.com>
2018-12-10 10:50:37 -0500

    logger: funcLogger should observe log levels (#822)

--- internal/cli/up.go
+++ internal/cli/up.go
@@ -134,7 +134,9 @@ func provideLogActions() store.LogActionsFlag {
 func NewLogActionLogger(ctx context.Context, dispatch func(action store.Action)) logger.Logger {
 	l := logger.Get(ctx)
 	return logger.NewFuncLogger(l.SupportsColor(), l.Level(), func(level logger.Level, b []byte) error {
-		dispatch(engine.LogAction{Log: b})
+		if l.Level() >= level {
+			dispatch(engine.LogAction{Log: b})
+		}
 		return nil
 	})
 }
