69705a11002c10ece2d5dc1cbe88175a67567924
Nick Santos <nicholas.j.santos@gmail.com>
2018-12-10 15:13:47 -0500

    demo: move log setup to engine, use it in demo (#824)

---
+++ internal/engine/logger.go
@@ -0,0 +1,18 @@
+package engine
+
+import (
+	"context"
+
+	"github.com/windmilleng/tilt/internal/logger"
+	"github.com/windmilleng/tilt/internal/store"
+)
+
+func NewLogActionLogger(ctx context.Context, dispatch func(action store.Action)) logger.Logger {
+	l := logger.Get(ctx)
+	return logger.NewFuncLogger(l.SupportsColor(), l.Level(), func(level logger.Level, b []byte) error {
+		if l.Level() >= level {
+			dispatch(LogAction{Log: b})
+		}
+		return nil
+	})
+}
