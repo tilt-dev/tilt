4cfd38e21acd9a9fe7f9696f0e510e536c226c44
Nick Santos <nicholas.j.santos@gmail.com>
2018-12-12 12:07:43 -0500

    docker: some versions of docker reject requests for buildkit with error if it's not supported :sob: (#836)

---
+++ internal/docker/client_test.go
@@ -0,0 +1,34 @@
+package docker
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/docker/docker/api/types"
+	"github.com/stretchr/testify/assert"
+)
+
+type buildkitTestCase struct {
+	v        types.Version
+	expected bool
+}
+
+func TestSupportsBuildkit(t *testing.T) {
+	cases := []buildkitTestCase{
+		{types.Version{APIVersion: "1.37", Experimental: true}, false},
+		{types.Version{APIVersion: "1.37", Experimental: false}, false},
+		{types.Version{APIVersion: "1.38", Experimental: true}, true},
+		{types.Version{APIVersion: "1.38", Experimental: false}, false},
+		{types.Version{APIVersion: "1.39", Experimental: true}, true},
+		{types.Version{APIVersion: "1.39", Experimental: false}, true},
+		{types.Version{APIVersion: "1.40", Experimental: true}, true},
+		{types.Version{APIVersion: "1.40", Experimental: false}, true},
+		{types.Version{APIVersion: "garbage", Experimental: false}, false},
+	}
+
+	for i, c := range cases {
+		t.Run(fmt.Sprintf("Case%d", i), func(t *testing.T) {
+			assert.Equal(t, c.expected, SupportsBuildkit(c.v))
+		})
+	}
+}
