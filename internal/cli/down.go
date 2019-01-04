12d910c0d380344f9ed5d4a3a7f5ac3fb272baec
Matt Landis <matt@windmill.engineering>
2019-01-04 15:28:31 -0500

    oops

--- internal/cli/down.go
+++ internal/cli/down.go
@@ -63,6 +63,7 @@ func (c downCmd) run(ctx context.Context, args []string) error {
 			// TODO(maia): when we support up-ing from multiple docker-compose files, we'll
 			// need to support down-ing as well. For now, we `down` the first one we find.
 			dcConfigPath = m.DCInfo().ConfigPath
+			continue
 		}
 	}
 
