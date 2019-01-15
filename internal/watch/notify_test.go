822bcff4a0580f7f83ba40181d8b2668c686043f
Dan Miller <dmiller@windmill.engineering>
2019-01-15 11:23:48 -0500

    watch: add watcher that traverses up directory structure recursively

--- internal/watch/notify_test.go
+++ internal/watch/notify_test.go
@@ -309,6 +309,25 @@ func TestWatchBothDirAndFile(t *testing.T) {
 	f.assertEvents(fileB)
 }
 
+func TestWatchNonexistantDirectory(t *testing.T) {
+	f := newNotifyFixture(t)
+	defer f.tearDown()
+
+	root := f.JoinPath("root")
+	err := os.Mkdir(root, 0777)
+	if err != nil {
+		t.Fatal(err)
+	}
+	parent := f.JoinPath("root", "parent")
+	file := f.JoinPath("root", "parent", "a")
+
+	f.watch(file)
+	f.fsync()
+	f.events = nil
+	f.WriteFile(file, "hello")
+	f.assertEvents(parent, file)
+}
+
 type notifyFixture struct {
 	*tempdir.TempDirFixture
 	notify  Notify
