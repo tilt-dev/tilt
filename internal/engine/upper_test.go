f07a7e3953d738db7312e68cd82fd7f950430301
Dan Miller <dmiller@windmill.engineering>
2019-02-04 18:57:12 -0500

    engine: print Tiltfile errors in main log

--- internal/engine/upper_test.go
+++ internal/engine/upper_test.go
@@ -1945,7 +1945,7 @@ func TestDockerComposeBuildCompletedDoesntSetStatusIfNotSuccessful(t *testing.T)
 func TestEmptyTiltfile(t *testing.T) {
 	f := newTestFixture(t)
 	f.WriteFile("Tiltfile", "")
-	go f.upper.Start(context.Background(), []string{}, false, model.TriggerAuto, "Tiltfile", true)
+	go f.upper.Start(f.ctx, []string{}, false, model.TriggerAuto, "Tiltfile", true)
 	f.WaitUntil("build is set", func(st store.EngineState) bool {
 		return !st.LastTiltfileBuild.Empty()
 	})
