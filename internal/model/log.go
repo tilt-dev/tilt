820e113b1600cab1a6fab3af4f6da42e89173d5d
Matt Landis <matt@windmill.engineering>
2019-02-11 13:17:21 -0500

    add comment

--- internal/model/log.go
+++ internal/model/log.go
@@ -21,6 +21,9 @@ func (l Log) Empty() bool {
 	return len(l.content) == 0
 }
 
+// Returns a new instance of `Log` with content equal to `b` appended to the end of `l`
+// Performs truncation off the start of the log (at a newline) to ensure the resulting log is not
+// longer than `maxLogLengthInBytes`. (which maybe means a pedant would say this isn't strictly an `append`?)
 func AppendLog(l Log, b []byte) Log {
 	content := append(l.content, b...)
 	if len(content) > maxLogLengthInBytes {
