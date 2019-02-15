e586d8a60b2579eac1b8d30c9fa24e35edb1f7b1
Nick Santos <nicholas.j.santos@gmail.com>
2019-02-15 17:45:49 -0500

    dockerfile: add a helper function for injecting an image digest into a dockerfile (#1167)

---
+++ internal/dockerfile/inject_test.go
@@ -0,0 +1,53 @@
+package dockerfile
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/windmilleng/tilt/internal/container"
+)
+
+func TestInjectUntagged(t *testing.T) {
+	df := Dockerfile(`
+FROM gcr.io/windmill/foo
+ADD . .
+`)
+	ref := container.MustParseNamedTagged("gcr.io/windmill/foo:deadbeef")
+	newDf, modified, err := InjectImageDigest(df, ref)
+	if assert.NoError(t, err) {
+		assert.True(t, modified)
+		assert.Equal(t, `
+FROM gcr.io/windmill/foo:deadbeef
+ADD . .
+`, string(newDf))
+	}
+}
+
+func TestInjectTagged(t *testing.T) {
+	df := Dockerfile(`
+FROM gcr.io/windmill/foo:v1
+ADD . .
+`)
+	ref := container.MustParseNamedTagged("gcr.io/windmill/foo:deadbeef")
+	newDf, modified, err := InjectImageDigest(df, ref)
+	if assert.NoError(t, err) {
+		assert.True(t, modified)
+		assert.Equal(t, `
+FROM gcr.io/windmill/foo:deadbeef
+ADD . .
+`, string(newDf))
+	}
+}
+
+func TestInjectNoMatch(t *testing.T) {
+	df := Dockerfile(`
+FROM gcr.io/windmill/bar:v1
+ADD . .
+`)
+	ref := container.MustParseNamedTagged("gcr.io/windmill/foo:deadbeef")
+	newDf, modified, err := InjectImageDigest(df, ref)
+	if assert.NoError(t, err) {
+		assert.False(t, modified)
+		assert.Equal(t, df, newDf)
+	}
+}
