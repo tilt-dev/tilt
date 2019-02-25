9840223d0126499afa76ef02c35143ffa812eca5
Matt Landis <landism@users.noreply.github.com>
2019-02-25 16:02:22 -0500

    engine: missed changes from #1202 (#1208)

--- internal/engine/store_testutil.go
+++ internal/engine/store_testutil.go
@@ -10,23 +10,24 @@ import (
 )
 
 // for use by tests, to wait until an action of the specified type comes out of the given chan
-func waitForAction(t testing.TB, typ reflect.Type, actions <-chan store.Action) store.Action {
-	timeout := time.After(300 * time.Millisecond)
-	var seenActions []store.Action
-	for {
-		select {
-		case <-timeout:
-			t.Fatalf("timed out waiting for action of type %s. Saw the following actions while waiting: %+v",
-				typ.Name(),
-				seenActions)
-		case a := <-actions:
+// at some point we might want it to return the index it was found at, and then take an index,
+// so that we can start searching from the next index
+func waitForAction(t testing.TB, typ reflect.Type, getActions func() []store.Action) store.Action {
+	start := time.Now()
+	timeout := 300 * time.Millisecond
+
+	for time.Since(start) < timeout {
+		actions := getActions()
+		for _, a := range actions {
 			if reflect.TypeOf(a) == typ {
 				return a
 			} else if la, ok := a.(LogAction); ok {
 				fmt.Println(string(la.Log))
-			} else {
-				seenActions = append(seenActions, a)
 			}
 		}
 	}
+	t.Fatalf("timed out waiting for action of type %s. Saw the following actions while waiting: %+v",
+		typ.Name(),
+		getActions())
+	return nil
 }
