10457160d0ef8cd8a12afc1a3d6d13a2daffcb75
Dan Miller <dmiller@windmill.engineering>
2019-03-01 12:10:05 -0500

    remove debug logs

--- integration/fixture_test.go
+++ integration/fixture_test.go
@@ -7,7 +7,6 @@ import (
 	"go/build"
 	"io"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
 	"os/exec"
@@ -221,10 +220,7 @@ func (f *fixture) AllPodsReady(ctx context.Context, selector string) (bool, stri
 	cmd := exec.Command("kubectl", "get", "pods",
 		namespaceFlag, "--selector="+selector, "-o=template",
 		"--template", "{{range .items}}{{.metadata.name}} {{.status.phase}}{{println}}{{end}}")
-	log.Printf("CMD: %+v", cmd.Args)
 	out, err := cmd.CombinedOutput()
-	log.Printf("OUT: %s", out)
-	log.Printf("ERR: %v", err)
 	if err != nil {
 		f.t.Fatal(err)
 	}
