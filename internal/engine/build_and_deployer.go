50e6cc248113cc78fe7ea9c0fa30ef78bef5e00b
Dan Miller <dmiller@windmill.engineering>
2019-03-08 13:27:47 -0500

    engine: always show unexpected fallback message
    This is very useful when debugging issues and helps a user understand
    what Tilt is doing.

--- internal/engine/build_and_deployer.go
+++ internal/engine/build_and_deployer.go
@@ -54,7 +54,7 @@ func (composite *CompositeBuildAndDeployer) BuildAndDeploy(ctx context.Context, 
 			logger.Get(ctx).Debugf("(expected error) falling back to next build and deploy method "+
 				"after error: %v", err)
 		} else {
-			logger.Get(ctx).Verbosef("falling back to next build and deploy method "+
+			logger.Get(ctx).Infof("falling back to next build and deploy method "+
 				"after unexpected error: %v", err)
 		}
 
