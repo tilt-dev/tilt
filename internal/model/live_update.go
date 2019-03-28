e234d7a9c731671def655910a1f6bda0c33d8d5d
Matt Landis <matt@windmill.engineering>
2019-03-28 10:51:05 -0400

    adjust comments, add tests

--- internal/model/live_update.go
+++ internal/model/live_update.go
@@ -1,10 +1,38 @@
 package model
 
+import "github.com/pkg/errors"
+
+// Specifies how to update a running container.
+// 1. If there are Sync steps in `Steps`, files will be synced as specified.
+// 2. Any time we sync one or more files, all Run and RestartContainer steps will be evaluated.
 type LiveUpdate struct {
 	Steps []LiveUpdateStep
 
 	// When files matching any of these globs change, we should fall back to a full rebuild.
 	FullRebuildTriggers []string
+}
+
+func NewLiveUpdate(steps []LiveUpdateStep, fullRebuildTriggers []string) (LiveUpdate, error) {
+	seenRunStep := false
+	for i, step := range steps {
+		switch step.(type) {
+		case LiveUpdateWorkDirStep:
+			if i != 0 {
+				return LiveUpdate{}, errors.New("workdir is only valid as the first step")
+			}
+		case LiveUpdateSyncStep:
+			if seenRunStep {
+				return LiveUpdate{}, errors.New("all sync steps must precede all run steps")
+			}
+		case LiveUpdateRunStep:
+			seenRunStep = true
+		case LiveUpdateRestartContainerStep:
+			if i != len(steps)-1 {
+				return LiveUpdate{}, errors.New("restart container is only valid as the last step")
+			}
+		}
+	}
+	return LiveUpdate{steps, fullRebuildTriggers}, nil
 }
 
 type LiveUpdateStep interface {
@@ -16,9 +44,7 @@ type LiveUpdateWorkDirStep string
 
 func (l LiveUpdateWorkDirStep) liveUpdateStep() {}
 
-// Specifies that when there are changes to the local path `Source`, we:
-// 1. Sync those changes to the remote path `Dest`
-// 2. Trigger any applicable `Run` steps
+// Specifies that changes to local path `Source` should be synced to container path `Dest`
 type LiveUpdateSyncStep struct {
 	Source, Dest string
 }
@@ -26,7 +52,8 @@ type LiveUpdateSyncStep struct {
 func (l LiveUpdateSyncStep) liveUpdateStep() {}
 
 // Specifies that `Command` should be executed when any files in `Sync` steps have changed
-// If `Trigger` is non-empty, will only be executed when those changes match the glob specified in `Trigger`.
+// If `Trigger` is non-empty, `Command` will only be executed when the local paths of changed files covered by
+// at least one `Sync` match the glob in `Trigger`.
 type LiveUpdateRunStep struct {
 	Command Cmd
 	Trigger string
@@ -37,4 +64,4 @@ func (l LiveUpdateRunStep) liveUpdateStep() {}
 // Specifies that any files matched by `Sync` steps should also cause the container to be restarted.
 type LiveUpdateRestartContainerStep struct{}
 
-func (l LiveUpdateRestartContainerStep) LiveUpdateStep() {}
+func (l LiveUpdateRestartContainerStep) liveUpdateStep() {}
