45c908ddf3beaa8494d957af8ce9569ba1dcd36b
Dan Miller <dmiller@windmill.engineering>
2019-03-29 15:19:05 -0400

    web: remove prototype web code (#1392)

--- web/src/index.js
+++ web/src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
-import LogApp from './LogApp';
-import K8sApp from './K8sApp';
 import HUD from './HUD';
 import NoMatch from './NoMatch';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
@@ -12,15 +9,7 @@ let Main = () => {
   return (<Router>
     <div>
       <Switch>
-          <Route exact path="/" component={App} />
-        {/*
-          * New HUD work. HUD will eventually be promoted to the top-level component
-          * and we'll delete App & friends
-          */}
         <Route path="/hud" component={HUD} />
-        <Route exact path="/log" component={LogApp} />
-        <Route exact path="/r/:name/log" component={LogApp} />
-        <Route exact path="/r/:name/k8s" component={K8sApp} />
         <Route component={NoMatch} />
       </Switch>
     </div>
