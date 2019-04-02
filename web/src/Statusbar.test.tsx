e7147e2658f19a5c6cf60c45d731dfe5b4b60fb7
Nick Santos <nicholas.j.santos@gmail.com>
2019-04-02 17:39:54 -0400

    web: fix up error display

--- web/src/Statusbar.test.tsx
+++ web/src/Statusbar.test.tsx
@@ -2,6 +2,8 @@ import React from "react"
 import ReactDOM from "react-dom"
 import renderer from "react-test-renderer"
 import Statusbar, { StatusItem } from "./Statusbar"
+import { mount } from "enzyme"
+import { oneResourceView, twoResourceView } from "./testdata.test"
 
 describe("StatusBar", () => {
   it("renders without crashing", () => {
@@ -11,11 +13,54 @@ describe("StatusBar", () => {
 
     expect(tree).toMatchSnapshot()
   })
+
+  it("renders two items both errors", () => {
+    let items = twoResourceView().Resources.map(res => new StatusItem(res))
+    let statusbar = mount(<Statusbar items={items} toggleSidebar={null} />)
+    expect(statusbar.find(".Statusbar-panel--error").html()).toContain(
+      "2 Errors"
+    )
+  })
+
+  it("renders two items both errors snapshot", () => {
+    let items = twoResourceView().Resources.map(res => new StatusItem(res))
+    const tree = renderer
+      .create(<Statusbar items={items} toggleSidebar={null} />)
+      .toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it("renders two items all ok snapshot", () => {
+    let view = twoResourceView()
+    view.Resources.forEach(res => {
+      res.BuildHistory[0].Error = ""
+    })
+
+    let items = view.Resources.map(res => new StatusItem(res))
+    const tree = renderer
+      .create(<Statusbar items={items} toggleSidebar={null} />)
+      .toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it("renders two items all ok", () => {
+    let view = twoResourceView()
+    view.Resources.forEach(res => {
+      res.BuildHistory[0].Error = ""
+    })
+    let items = view.Resources.map(res => new StatusItem(res))
+    let statusbar = mount(<Statusbar items={items} toggleSidebar={null} />)
+    expect(statusbar.find(".Statusbar-panel--error").html()).toContain(
+      "0 Errors"
+    )
+  })
 })
 
 describe("StatusItem", () => {
   it("can be constructed with no build history", () => {
     let si = new StatusItem({})
-    expect(si.error).toBe("")
+    expect(si.hasError).toBe(false)
   })
 })
---
+++ web/src/testdata.test.tsx
@@ -0,0 +1,138 @@
+import { RouteComponentProps } from "react-router-dom"
+import { UnregisterCallback, Href } from "history"
+
+// NOTE(dmiller) 4-02-19 this function is currently unused but I'm going to keep it around.
+// I have a feeling that it will come in handy later.
+function getMockRouterProps<P>(data: P) {
+  var location = {
+    hash: "",
+    key: "",
+    pathname: "",
+    search: "",
+    state: {},
+  }
+
+  var props: RouteComponentProps<P> = {
+    match: {
+      isExact: true,
+      params: data,
+      path: "",
+      url: "",
+    },
+    location: location,
+    history: {
+      length: 2,
+      action: "POP",
+      location: location,
+      push: () => {},
+      replace: () => {},
+      go: num => {},
+      goBack: () => {},
+      goForward: () => {},
+      block: t => {
+        var temp: UnregisterCallback = () => {}
+        return temp
+      },
+      createHref: t => {
+        var temp: Href = ""
+        return temp
+      },
+      listen: t => {
+        var temp: UnregisterCallback = () => {}
+        return temp
+      },
+    },
+    staticContext: {},
+  }
+
+  return props
+}
+
+function oneResourceView() {
+  const ts = Date.now().toLocaleString()
+  const resource = {
+    Name: "vigoda",
+    DirectoriesWatched: ["foo", "bar"],
+    LastDeployTime: ts,
+    BuildHistory: [
+      {
+        Edits: ["main.go", "cli.go"],
+        Error: "the build failed!",
+        FinishTime: ts,
+        StartTime: ts,
+      },
+    ],
+    PendingBuildEdits: ["main.go", "cli.go", "vigoda.go"],
+    PendingBuildSince: ts,
+    CurrentBuild: {
+      Edits: ["main.go"],
+      StartTime: ts,
+    },
+    PodName: "vigoda-pod",
+    PodCreationTime: ts,
+    PodStatus: "Running",
+    PodRestarts: 1,
+    Endpoints: ["1.2.3.4:8080"],
+    PodLog: "1\n2\n3\n4\nabe vigoda is now dead\n5\n6\n7\n8\n",
+  }
+  return { Resources: [resource] }
+}
+
+function twoResourceView() {
+  const ts = Date.now().toLocaleString()
+  const vigoda = {
+    Name: "vigoda",
+    DirectoriesWatched: ["foo", "bar"],
+    LastDeployTime: ts,
+    BuildHistory: [
+      {
+        Edits: ["main.go", "cli.go"],
+        Error: "the build failed!",
+        FinishTime: ts,
+        StartTime: ts,
+      },
+    ],
+    PendingBuildEdits: ["main.go", "cli.go", "vigoda.go"],
+    PendingBuildSince: ts,
+    CurrentBuild: {
+      Edits: ["main.go"],
+      StartTime: ts,
+    },
+    PodName: "vigoda-pod",
+    PodCreationTime: ts,
+    PodStatus: "Running",
+    PodRestarts: 1,
+    Endpoints: ["1.2.3.4:8080"],
+    PodLog: "1\n2\n3\n4\nabe vigoda is now dead\n5\n6\n7\n8\n",
+  }
+
+  const snack = {
+    Name: "snack",
+    DirectoriesWatched: ["foo", "bar"],
+    LastDeployTime: ts,
+    BuildHistory: [
+      {
+        Edits: ["main.go", "cli.go"],
+        Error: "the build failed!",
+        FinishTime: ts,
+        StartTime: ts,
+      },
+    ],
+    PendingBuildEdits: ["main.go", "cli.go", "snack.go"],
+    PendingBuildSince: ts,
+    CurrentBuild: {
+      Edits: ["main.go"],
+      StartTime: ts,
+    },
+    PodName: "snack-pod",
+    PodCreationTime: ts,
+    PodStatus: "Running",
+    PodRestarts: 1,
+    Endpoints: ["1.2.3.4:8080"],
+    PodLog: "1\n2\n3\n4\nsnacks are great\n5\n6\n7\n8\n",
+  }
+  return { Resources: [vigoda, snack] }
+}
+
+it("loads ok", () => {})
+export { oneResourceView, twoResourceView, getMockRouterProps }
