bff8a509395aa5e5f7b3b42310bd205e252b0efb
Nick Santos <nicholas.j.santos@gmail.com>
2019-04-15 12:55:24 -0400

    sail: add websocket fan-out (#1493)

--- internal/sail/server/room.go
+++ internal/sail/server/room.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"io"
 	"log"
 
 	"github.com/google/uuid"
@@ -15,6 +16,11 @@ type Room struct {
 	// Immutable data
 	id     RoomID
 	source SourceConn
+	addFan chan AddFanAction
+	fanOut chan FanOutAction
+
+	// Mutable data, only read/written in the action loop.
+	fans []FanConn
 }
 
 // A websocket that we only read messages from
@@ -25,11 +31,54 @@ type SourceConn interface {
 
 var _ SourceConn = &websocket.Conn{}
 
+// A websocket that we only write messages to
+type FanConn interface {
+	WriteMessage(int, []byte) error
+	NextReader() (int, io.Reader, error)
+	Close() error
+}
+
+var _ FanConn = &websocket.Conn{}
+
+type AddFanAction struct {
+	fan FanConn
+}
+
+type FanOutAction struct {
+	messageType int
+	data        []byte
+}
+
 func NewRoom(conn SourceConn) *Room {
 	return &Room{
 		id:     RoomID(uuid.New().String()),
 		source: conn,
+		addFan: make(chan AddFanAction, 0),
+		fanOut: make(chan FanOutAction, 0),
 	}
+}
+
+// Add a fan that consumes messages from the source.
+// Calling AddFan() after Close() will error.
+func (r *Room) AddFan(ctx context.Context, conn FanConn) {
+	r.addFan <- AddFanAction{fan: conn}
+
+	go func() {
+		for ctx.Err() != nil {
+			// We need to read from the connection so that the websocket
+			// library handles control messages, but we can otherwise discard them.
+			if _, _, err := conn.NextReader(); err != nil {
+				// TODO(nick): Remove this fan from the room.
+				log.Printf("streamFan: %v", err)
+				return
+			}
+		}
+	}()
+}
+
+// Only close the room when we know AddFan can't be called.
+func (r *Room) Close() {
+	close(r.addFan)
 }
 
 // Receive messages from the source websocket and put them through the state loop.
@@ -41,21 +90,35 @@ func (r *Room) ConsumeSource(ctx context.Context) error {
 		defer cancel()
 
 		for ctx.Err() == nil {
-			_, data, err := r.source.ReadMessage()
-			if err != nil {
+			messageType, data, err := r.source.ReadMessage()
+			if err != nil && context.Canceled != err {
 				log.Printf("ConsumeSource: %v", err)
 				return
 			}
 
-			log.Printf("Received new message: %s", string(data))
+			r.fanOut <- FanOutAction{messageType: messageType, data: data}
 		}
 	}()
 
 	for {
 		select {
 		case <-ctx.Done():
+
+			for _, fan := range r.fans {
+				_ = fan.Close()
+			}
 			_ = r.source.Close()
+
 			return ctx.Err()
+		case action := <-r.addFan:
+			r.fans = append(r.fans, action.fan)
+		case action := <-r.fanOut:
+			for _, fan := range r.fans {
+				err := fan.WriteMessage(action.messageType, action.data)
+				if err != nil {
+					log.Printf("Room Fan-out: %v", err)
+				}
+			}
 		}
 	}
 }
