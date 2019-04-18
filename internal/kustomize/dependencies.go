41efed611f792ccbffbe23aa3468f844700351f2
Dan Miller <dmiller@windmill.engineering>
2019-04-18 16:24:56 -0400

    kustomize: support alternative file names (#1505)
    Mostly taken from the [kustomize source code](https://github.com/kubernetes-sigs/kustomize/blob/ee68a9c450bc884b0d657fb7e3d62eb1ac59d14f/pkg/target/kusttarget.go#L97) itself.

--- internal/kustomize/dependencies.go
+++ internal/kustomize/dependencies.go
@@ -1,11 +1,18 @@
 package kustomize
 
 import (
+	"fmt"
 	"io/ioutil"
 	"path/filepath"
 
 	yaml "gopkg.in/yaml.v2"
 )
+
+var kustomizationFileNames = []string{
+	"kustomization.yaml",
+	"kustomization.yml",
+	"Kustomization",
+}
 
 // kustomization is the content of a kustomization.yaml file.
 type kustomization struct {
@@ -25,11 +32,38 @@ type configMapGenerator struct {
 	Files []string `yaml:"files"`
 }
 
+// Mostly taken from the [kustomize source code](https://github.com/kubernetes-sigs/kustomize/blob/ee68a9c450bc884b0d657fb7e3d62eb1ac59d14f/pkg/target/kusttarget.go#L97) itself.
+func loadKustFile(dir string) ([]byte, string, error) {
+	var content []byte
+	var path string
+	match := 0
+	for _, kf := range kustomizationFileNames {
+		p := filepath.Join(dir, kf)
+		c, err := ioutil.ReadFile(p)
+		if err == nil {
+			path = p
+			match += 1
+			content = c
+		}
+	}
+
+	switch match {
+	case 0:
+		return nil, "", fmt.Errorf(
+			"unable to find one of %v in directory '%s'",
+			kustomizationFileNames, dir)
+	case 1:
+		return content, path, nil
+	default:
+		return nil, "", fmt.Errorf(
+			"Found multiple kustomization files under: %s\n", dir)
+	}
+}
+
 func dependenciesForKustomization(dir string) ([]string, error) {
 	var deps []string
 
-	path := filepath.Join(dir, "kustomization.yaml")
-	buf, err := ioutil.ReadFile(path)
+	buf, path, err := loadKustFile(dir)
 	if err != nil {
 		return nil, err
 	}
