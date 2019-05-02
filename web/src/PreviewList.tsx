54b44a8bce0871f9b2d31181edce405d8a914608
Han Yu <han@windmill.engineering>
2019-05-02 16:18:04 -0400

    web: put “ALL” preview links in grid

--- web/src/PreviewList.tsx
+++ web/src/PreviewList.tsx
@@ -1,4 +1,5 @@
 import React, { PureComponent } from "react"
+import { ReactComponent as LogoWordmarkSvg } from "./assets/svg/logo-wordmark-gray.svg"
 import PathBuilder from "./PathBuilder"
 import { Link } from "react-router-dom"
 import "./PreviewList.scss"
@@ -11,19 +12,40 @@ type PreviewListProps = {
 class PreviewList extends PureComponent<PreviewListProps> {
   render() {
     let pb = this.props.pathBuilder
-    if (this.props.resourcesWithEndpoints.length === 0) {
-      return <p>No resources found with endpoints!</p>
-    }
-    let links = this.props.resourcesWithEndpoints.map(r => (
-      <li key={r}>
-        <Link to={pb.path(`/r/${r}/preview`)}>{r}</Link>
-      </li>
-    ))
+
+    let endpointsEmptyEl = (
+      <section className="Pane-empty-message">
+        <LogoWordmarkSvg />
+        <h2>No Endpoints Found</h2>
+        <p>
+          If you'd like to preview your resources in the browser, <br />
+          the <a href="https://docs.tilt.dev/tutorial.html">
+            Tilt Tutorial
+          </a>{" "}
+          has more on setting up port forwarding.
+        </p>
+      </section>
+    )
+
+    let endpointsEl = (
+      <ul>
+        {this.props.resourcesWithEndpoints.map(r => (
+          <li key={r}>
+            <Link to={pb.path(`/r/${r}/preview`)} className="PreviewList-link">
+              {r}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    )
+
+    let noEndpoints = this.props.resourcesWithEndpoints.length === 0
 
     return (
-      <section className="PreviewList">
-        <p>The following resources have preview endpoints:</p>
-        <ul>{links}</ul>
+      <section
+        className={`PreviewList ${noEndpoints ? "PreviewList--empty" : ""}`}
+      >
+        {noEndpoints ? endpointsEmptyEl : endpointsEl}
       </section>
     )
   }
