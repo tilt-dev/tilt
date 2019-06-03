0e46b9ccb9f2baf9c28b537c7ac2ef67f4ada30a
Nick Santos <nicholas.j.santos@gmail.com>
2019-06-03 08:30:21 -0700

    k8s: better error reporting when tilt doesn't have permission to read nodes (#1704)

---
+++ internal/k8s/runtime_test.go
@@ -0,0 +1,32 @@
+package k8s
+
+import (
+	"bytes"
+	"context"
+	"testing"
+
+	"github.com/windmilleng/tilt/internal/container"
+	"github.com/windmilleng/tilt/internal/logger"
+
+	"github.com/stretchr/testify/assert"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes/fake"
+	ktesting "k8s.io/client-go/testing"
+)
+
+func TestRuntimeForbidden(t *testing.T) {
+	cs := &fake.Clientset{}
+	cs.AddReactor("*", "*", func(action ktesting.Action) (handled bool, ret runtime.Object, err error) {
+		return true, nil, newForbiddenError()
+	})
+
+	core := cs.CoreV1()
+	runtimeAsync := newRuntimeAsync(core)
+
+	out := &bytes.Buffer{}
+	l := logger.NewLogger(logger.InfoLvl, out)
+	ctx := logger.WithLogger(context.Background(), l)
+	runtime := runtimeAsync.Runtime(ctx)
+	assert.Equal(t, container.RuntimeUnknown, runtime)
+	assert.Contains(t, out.String(), "Tilt could not read your node configuration")
+}
