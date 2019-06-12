737ba0c3310a64c68b788e52f9bc548e6640f555
Han Yu <han@sent.com>
2019-06-12 12:28:30 -0400

    web: show button to trigger update [ch2697] (#1738)

---
+++ web/src/SidebarTriggerButton.test.tsx
@@ -0,0 +1,33 @@
+import React from "react"
+import { mount } from "enzyme"
+import SidebarTriggerButton from "./SidebarTriggerButton"
+import { TriggerMode } from "./types"
+
+describe("SidebarTriggerButtoon", () => {
+  beforeEach(() => {
+    fetchMock.resetMocks()
+  })
+
+  it("POSTs to endpoint when clicked", () => {
+    fetchMock.mockResponse(JSON.stringify({}))
+
+    const root = mount(
+      <SidebarTriggerButton
+        isSelected={true}
+        resourceName="doggos"
+        triggerMode={TriggerMode.TriggerModeManual}
+      />
+    )
+
+    let element = root.find(".SidebarTriggerButton")
+    expect(element).toHaveLength(1)
+    element.simulate("click")
+
+    expect(fetchMock.mock.calls.length).toEqual(1)
+    expect(fetchMock.mock.calls[0][0]).toEqual("//localhost/api/trigger")
+    expect(fetchMock.mock.calls[0][1].method).toEqual("post")
+    expect(fetchMock.mock.calls[0][1].body).toEqual(
+      JSON.stringify({ manifest_names: ["doggos"] })
+    )
+  })
+})
