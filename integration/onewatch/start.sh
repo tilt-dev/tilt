b37477032c49cf99abdc5e06f40c50416a0ae7b7
Matt Landis <landism@users.noreply.github.com>
2019-07-01 17:00:32 -0400

    start.sh: fix quoting, kill underlying process on exit (#1784)

--- integration/onewatch/start.sh
+++ integration/onewatch/start.sh
@@ -11,14 +11,28 @@
 #   To restart the container:
 #   ./restart.sh
 
+set -euo pipefail
+
+process_id=""
+
+trap quit TERM INT
+
+quit() {
+  if [ -n "$process_id" ]; then
+    kill $process_id
+  fi
+}
+
 while true; do
     rm -f restart.txt
-    
-    $* &
+
+    "$@" &
     process_id=$!
     echo "$process_id" > process.txt
+    set +e
     wait $process_id
     EXIT_CODE=$?
+    set -e
     if [ ! -f restart.txt ]; then
         exit $EXIT_CODE
     fi
