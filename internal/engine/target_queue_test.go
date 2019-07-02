598a05a1ae3a9cd87edbf293e5a39a4d99351f34
Matt Landis <matt@windmill.engineering>
2019-07-02 18:18:43 -0400

    engine: add TargetQueue tests

---
+++ internal/engine/target_queue_test.go
@@ -0,0 +1,194 @@
+package engine
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/windmilleng/tilt/internal/container"
+	"github.com/windmilleng/tilt/internal/model"
+	"github.com/windmilleng/tilt/internal/store"
+)
+
+func TestTargetQueue_Simple(t *testing.T) {
+	f := newTargetQueueFixture(t)
+
+	t1 := model.NewImageTarget(container.MustParseSelector("vigoda"))
+	s1 := store.BuildState{}
+
+	targets := []model.ImageTarget{t1}
+	buildStateSet := store.BuildStateSet{
+		t1.ID(): s1,
+	}
+
+	f.run(targets, buildStateSet)
+
+	expectedCalls := map[model.TargetID]fakeBuildHandlerCall{
+		t1.ID(): newFakeBuildHandlerCall(t1, s1, 1, []store.BuildResult{}),
+	}
+	assert.Equal(t, expectedCalls, f.handler.calls)
+}
+
+func TestTargetQueue_DepsBuilt(t *testing.T) {
+	f := newTargetQueueFixture(t)
+
+	fooTarget := model.NewImageTarget(container.MustParseSelector("foo"))
+	s1 := store.BuildState{LastResult: store.BuildResult{TargetID: fooTarget.ID(), Image: container.MustParseNamedTagged("foo:1234")}}
+	barTarget := model.NewImageTarget(container.MustParseSelector("bar")).WithDependencyIDs([]model.TargetID{fooTarget.ID()})
+	s2 := store.BuildState{}
+
+	targets := []model.ImageTarget{fooTarget, barTarget}
+	buildStateSet := store.BuildStateSet{
+		fooTarget.ID(): s1,
+		barTarget.ID(): s2,
+	}
+
+	f.run(targets, buildStateSet)
+
+	barCall := newFakeBuildHandlerCall(barTarget, s2, 1, []store.BuildResult{{TargetID: fooTarget.ID(), Image: s1.LastResult.Image}})
+
+	// foo has a valid last result, so only bar gets rebuilt
+	expectedCalls := map[model.TargetID]fakeBuildHandlerCall{
+		barTarget.ID(): barCall,
+	}
+	assert.Equal(t, expectedCalls, f.handler.calls)
+}
+
+func TestTargetQueue_DepsUnbuilt(t *testing.T) {
+	f := newTargetQueueFixture(t)
+
+	fooTarget := model.NewImageTarget(container.MustParseSelector("foo"))
+	s1 := store.BuildState{}
+	barTarget := model.NewImageTarget(container.MustParseSelector("bar")).WithDependencyIDs([]model.TargetID{fooTarget.ID()})
+	var s2 = store.BuildState{LastResult: store.BuildResult{
+		TargetID: barTarget.ID(),
+		Image:    container.MustParseNamedTagged("bar:54321"),
+	}}
+	targets := []model.ImageTarget{fooTarget, barTarget}
+	buildStateSet := store.BuildStateSet{
+		fooTarget.ID(): s1,
+		barTarget.ID(): s2,
+	}
+
+	f.run(targets, buildStateSet)
+
+	fooCall := newFakeBuildHandlerCall(fooTarget, s1, 1, []store.BuildResult{})
+	// bar's dep is dirty, so bar should not get its old state
+	barCall := newFakeBuildHandlerCall(barTarget, store.BuildState{}, 2, []store.BuildResult{fooCall.result})
+
+	expectedCalls := map[model.TargetID]fakeBuildHandlerCall{
+		fooTarget.ID(): fooCall,
+		barTarget.ID(): barCall,
+	}
+	assert.Equal(t, expectedCalls, f.handler.calls)
+}
+
+func TestTargetQueue_IncrementalBuild(t *testing.T) {
+	f := newTargetQueueFixture(t)
+
+	fooTarget := model.NewImageTarget(container.MustParseSelector("foo"))
+	s1 := store.BuildState{
+		LastResult: store.BuildResult{
+			TargetID: fooTarget.ID(),
+			Image:    container.MustParseNamedTagged("foo:1234"),
+		},
+		FilesChangedSet: map[string]bool{"hello.txt": true},
+	}
+
+	targets := []model.ImageTarget{fooTarget}
+	buildStateSet := store.BuildStateSet{fooTarget.ID(): s1}
+
+	f.run(targets, buildStateSet)
+
+	fooCall := newFakeBuildHandlerCall(fooTarget, s1, 1, []store.BuildResult{})
+
+	expectedCalls := map[model.TargetID]fakeBuildHandlerCall{
+		fooTarget.ID(): fooCall,
+	}
+	assert.Equal(t, expectedCalls, f.handler.calls)
+}
+
+func TestTargetQueue_CachedBuild(t *testing.T) {
+	f := newTargetQueueFixture(t)
+
+	fooTarget := model.NewImageTarget(container.MustParseSelector("foo"))
+	s1 := store.BuildState{
+		LastResult: store.BuildResult{
+			TargetID: fooTarget.ID(),
+			Image:    container.MustParseNamedTagged("foo:1234"),
+		},
+	}
+
+	targets := []model.ImageTarget{fooTarget}
+	buildStateSet := store.BuildStateSet{fooTarget.ID(): s1}
+
+	f.run(targets, buildStateSet)
+
+	// last result is still valid, so handler doesn't get called at all
+	expectedCalls := map[model.TargetID]fakeBuildHandlerCall{}
+	assert.Equal(t, expectedCalls, f.handler.calls)
+}
+
+func newFakeBuildHandlerCall(target model.ImageTarget, state store.BuildState, num int, depResults []store.BuildResult) fakeBuildHandlerCall {
+	return fakeBuildHandlerCall{
+		target: target,
+		state:  state,
+		result: store.BuildResult{
+			TargetID: target.ID(),
+			Image:    container.MustParseNamedTagged(fmt.Sprintf("%s:%d", target.ConfigurationRef.String(), num)),
+		},
+		depResults: depResults,
+	}
+}
+
+type fakeBuildHandlerCall struct {
+	target     model.TargetSpec
+	state      store.BuildState
+	depResults []store.BuildResult
+	result     store.BuildResult
+}
+
+type fakeBuildHandler struct {
+	buildNum int
+	calls    map[model.TargetID]fakeBuildHandlerCall
+}
+
+func newFakeBuildHandler() *fakeBuildHandler {
+	return &fakeBuildHandler{
+		calls: make(map[model.TargetID]fakeBuildHandlerCall),
+	}
+}
+
+func (fbh *fakeBuildHandler) handle(target model.TargetSpec, state store.BuildState, depResults []store.BuildResult) (store.BuildResult, error) {
+	iTarget := target.(model.ImageTarget)
+	fbh.buildNum++
+	namedTagged := container.MustParseNamedTagged(fmt.Sprintf("%s:%d", iTarget.ConfigurationRef, fbh.buildNum))
+	result := store.NewImageBuildResult(target.ID(), namedTagged)
+	fbh.calls[target.ID()] = fakeBuildHandlerCall{target, state, depResults, result}
+	return result, nil
+}
+
+type targetQueueFixture struct {
+	t       *testing.T
+	handler *fakeBuildHandler
+}
+
+func newTargetQueueFixture(t *testing.T) *targetQueueFixture {
+	return &targetQueueFixture{
+		t:       t,
+		handler: newFakeBuildHandler(),
+	}
+}
+
+func (f *targetQueueFixture) run(targets []model.ImageTarget, buildStateSet store.BuildStateSet) {
+	tq, err := NewImageTargetQueue(targets, buildStateSet)
+	if err != nil {
+		f.t.Fatal(err)
+	}
+
+	err = tq.RunBuilds(f.handler.handle)
+	if err != nil {
+		f.t.Fatal(err)
+	}
+}
