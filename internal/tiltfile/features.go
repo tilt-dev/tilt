056b8179353f196fbd55c6df3f83fa4b05eafa74
Dan Miller <dmiller@windmill.engineering>
2019-07-15 11:25:53 -0400

    tiltfile: unknown feature flag checks are warnings
    I realized that this will be annoying now that we are deleting our first
    feature flag. Once someone started tilt on a project that checks an old
    feature flag it would crash. This changes unknown feature flags to be
    warnings in the Tiltfile instead of errors.

--- internal/tiltfile/features.go
+++ internal/tiltfile/features.go
@@ -1,6 +1,8 @@
 package tiltfile
 
 import (
+	"fmt"
+
 	"go.starlark.net/starlark"
 )
 
@@ -11,6 +13,10 @@ func (s *tiltfileState) enableFeature(thread *starlark.Thread, fn *starlark.Buil
 		return nil, err
 	}
 
+	if _, ok := s.f.GetAllFlags()[flag]; !ok {
+		s.warnings = append(s.warnings, fmt.Sprintf("Unknown feature flag used in check: %s", flag))
+		return starlark.None, nil
+	}
 	err = s.f.Enable(flag)
 	if err != nil {
 		return nil, err
