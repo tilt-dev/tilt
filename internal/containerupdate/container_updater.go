f710e499cf63c0057041e397a063704ab8cbd0bf
Maia <maia@windmill.engineering>
2019-07-23 16:09:27 -0400

    rm ExplodingContainerUpdater

--- internal/containerupdate/container_updater.go
+++ internal/containerupdate/container_updater.go
@@ -4,10 +4,6 @@ import (
 	"context"
 	"io"
 
-	"github.com/windmilleng/tilt/internal/container"
-	"github.com/windmilleng/tilt/internal/docker"
-	"github.com/windmilleng/tilt/internal/k8s"
-	"github.com/windmilleng/tilt/internal/mode"
 	"github.com/windmilleng/tilt/internal/model"
 	"github.com/windmilleng/tilt/internal/store"
 )
@@ -15,24 +11,4 @@ import (
 type ContainerUpdater interface {
 	UpdateContainer(ctx context.Context, deployInfo store.DeployInfo,
 		archiveToCopy io.Reader, filesToDelete []string, cmds []model.Cmd, hotReload bool) error
-}
-
-func ProvideContainerUpdater(kCli k8s.Client, dCli docker.Client, sm SyncletManager, env k8s.Env, updMode mode.UpdateMode, runtime container.Runtime) ContainerUpdater {
-	if updMode == mode.UpdateModeImage || updMode == mode.UpdateModeNaive {
-		return NewExplodingContainerUpdater()
-	}
-
-	if updMode == mode.UpdateModeKubectlExec {
-		return NewExecUpdater(kCli)
-	}
-
-	if updMode == mode.UpdateModeContainer || (runtime == container.RuntimeDocker && env.IsLocalCluster()) {
-		return NewDockerContainerUpdater(dCli)
-	}
-
-	if updMode == mode.UpdateModeSynclet || runtime == container.RuntimeDocker {
-		return NewSyncletUpdater(sm)
-	}
-
-	return NewExecUpdater(kCli)
 }
