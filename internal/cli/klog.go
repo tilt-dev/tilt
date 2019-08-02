21e97b8d6537f278d1fa0971cc0b754f089f5348
Matt Landis <landism@users.noreply.github.com>
2019-08-02 10:24:34 -0400

    klog: hide 'resourceVersion too old' warnings (#1955)

--- internal/cli/klog.go
+++ internal/cli/klog.go
@@ -1,15 +1,50 @@
 package cli
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"strings"
 
 	"k8s.io/klog"
 )
 
 var klogLevel = 0
+
+// everything on google indicates this warning is useless and should be ignored
+func isResourceVersionTooOldMessage(s string) bool {
+	return strings.Contains(s, "reflector.go") && strings.Contains(s, "The resourceVersion for the provided watch is too old.")
+}
+
+// unfortunately, k8s logs spurious and annoying messages via glog warnings, and there's no good way to control that
+// via command line flags, so we're just gonna apply string filtering!
+// `filteredWriter` creates a new `io.Writer` that passes through to `w` only those lines for which
+// 	 `filterFunc` returns false
+func filteredWriter(w io.Writer, filterFunc func(s string) bool) io.Writer {
+	r, fw := io.Pipe()
+	go func() {
+		scanner := bufio.NewScanner(r)
+		scanner.Split(bufio.ScanLines)
+		for {
+			if !scanner.Scan() {
+				break
+			}
+
+			s := scanner.Text()
+			if !filterFunc(s) {
+				_, err := w.Write([]byte(s + "\n"))
+				if err != nil {
+					_ = r.CloseWithError(err)
+				}
+			}
+		}
+	}()
+
+	return fw
+}
 
 // HACK(nick): The Kubernetes libs we use sometimes use klog to log things to
 // os.Stderr. There are no API hooks to configure this. And printing to Stderr
@@ -17,15 +52,17 @@ var klogLevel = 0
 //
 // Fortunately, klog does initialize itself from flags, so we can backdoor
 // configure it by setting our own flags. Don't do this at home!
-func initKlog() {
+func initKlog(w io.Writer) {
 	var tmpFlagSet = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
 	klog.InitFlags(tmpFlagSet)
 
+	if klogLevel == 0 {
+		w = filteredWriter(w, isResourceVersionTooOldMessage)
+	}
+	klog.SetOutput(w)
+
 	flags := []string{
 		"--stderrthreshold", "FATAL",
-		// k8s' reflector.go currently has only spurious warnings
-		// by default, set it to v0 so that they are not shown
-		fmt.Sprintf("--vmodule=reflector=%d", klogLevel),
 	}
 
 	if klogLevel > 0 {
