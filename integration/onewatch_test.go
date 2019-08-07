f4a43018577e8f9cfa29630af7663d0738fee437
Nick Santos <nicholas.j.santos@gmail.com>
2019-08-07 15:27:46 -0400

    integration: add tests for crash rebuilds (#2003)

--- integration/onewatch_test.go
+++ integration/onewatch_test.go
@@ -17,17 +17,9 @@ func TestOneWatch(t *testing.T) {
 
 	f.TiltWatch()
 
-	// ForwardPort will fail if all the pods are not ready.
-	//
-	// For the purposes of this integration tests, we want the test to fail if the
-	// Pod is re-created (rather than getting updated in-place).  We deliberately
-	// don't use Tilt-managed port-forwarding because it would auto-connect to the
-	// new pod.
 	ctx, cancel := context.WithTimeout(f.ctx, time.Minute)
 	defer cancel()
 	oneUpPods := f.WaitForAllPodsReady(ctx, "app=onewatch")
-
-	f.ForwardPort("deployment/onewatch", "31234:8000")
 
 	ctx, cancel = context.WithTimeout(f.ctx, time.Minute)
 	defer cancel()
@@ -51,4 +43,19 @@ func TestOneWatch(t *testing.T) {
 	// Assert that the pods were changed in-place, and not that we
 	// created new pods.
 	assert.Equal(t, oneUpPods, twoUpPods)
+
+	if len(twoUpPods) != 1 {
+		t.Fatalf("Expected one pod, actual: %v", twoUpPods)
+	}
+
+	// Delete the pod and make sure it got replaced with something that prints the
+	// same thing (crash rebuild).
+	f.runCommandSilently("kubectl", "delete", "pod", twoUpPods[0], namespaceFlag)
+
+	ctx, cancel = context.WithTimeout(f.ctx, time.Minute)
+	defer cancel()
+	f.CurlUntil(ctx, "http://localhost:31234", "🍄 Two-Up! 🍄")
+
+	newTwoUpPods := f.WaitForAllPodsReady(ctx, "app=onewatch")
+	assert.NotEqual(t, twoUpPods, newTwoUpPods)
 }
