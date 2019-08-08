06a7227ef12138cd12edcddce01ce9668dfc6baf
Maia McCormick <maia@windmill.engineering>
2019-08-08 17:36:42 -0400

    engine: don't run empty LiveUpdateInfos thru buildAndDeploy (eliminates log spam when LU-ing only one of multiple images (#2018)

--- internal/engine/live_update_build_and_deployer.go
+++ internal/engine/live_update_build_and_deployer.go
@@ -64,14 +64,14 @@ func (lubad *LiveUpdateBuildAndDeployer) BuildAndDeploy(ctx context.Context, st 
 	}
 
 	containerUpdater := lubad.containerUpdaterForSpecs(specs)
-	liveUpdInfos := make([]liveUpdInfo, len(liveUpdateStateSet))
+	liveUpdInfos := make([]liveUpdInfo, 0, len(liveUpdateStateSet))
 
 	if len(liveUpdateStateSet) == 0 {
 		return nil, RedirectToNextBuilderInfof("no targets for LiveUpdate found")
 	}
 
 	unclaimedFiles := allChangedFiles(liveUpdateStateSet)
-	for i, luStateTree := range liveUpdateStateSet {
+	for _, luStateTree := range liveUpdateStateSet {
 		luInfo, err := liveUpdateInfoForStateTree(luStateTree)
 		if err != nil {
 			return store.BuildResultSet{}, err
@@ -82,7 +82,7 @@ func (lubad *LiveUpdateBuildAndDeployer) BuildAndDeploy(ctx context.Context, st 
 		}
 
 		if !luInfo.Empty() {
-			liveUpdInfos[i] = luInfo
+			liveUpdInfos = append(liveUpdInfos, luInfo)
 		}
 	}
 
