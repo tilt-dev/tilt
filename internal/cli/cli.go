e51c6de4e48919cd97a2c4cf23f80aff215cd51d
Nick Santos <nicholas.j.santos@gmail.com>
2019-08-09 14:39:36 -0400

    Revert "klog: hide 'resourceVersion too old' warnings (#1955)" (#2029)
    This reverts commit 21e97b8d6537f278d1fa0971cc0b754f089f5348.

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -77,11 +77,10 @@ type tiltCmd interface {
 
 func preCommand(ctx context.Context, a *analytics.TiltAnalytics) (context.Context, func() error) {
 	cleanup := func() error { return nil }
+	initKlog()
 	l := logger.NewLogger(logLevel(verbose, debug), os.Stdout)
 	ctx = logger.WithLogger(ctx, l)
 	ctx = analytics.WithAnalytics(ctx, a)
-
-	initKlog(l.Writer(logger.InfoLvl))
 
 	if trace {
 		backend, err := tracer.StringToTracerBackend(traceType)
