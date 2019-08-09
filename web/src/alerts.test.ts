a26aba465ea2354bc6bf1ea9d282983a0f1be84a
Maria Vivanco <vivancomar@gmail.com>
2019-08-09 11:02:20 -0400

    fixed resourceInfo logic for alerts tests

--- web/src/alerts.test.ts
+++ web/src/alerts.test.ts
@@ -12,9 +12,10 @@ import { Resource, K8sResourceInfo, TriggerMode } from "./types"
 
 describe("getResourceAlerts", () => {
   it("K8Resource: shows that a pod status of error is an alert", () => {
-    let r: Resource = emptyK8Resource()
-    r.ResourceInfo.PodStatus = "Error"
-    r.ResourceInfo.PodStatusMessage = "I'm a pod in Error"
+    let r: Resource = k8sResource()
+    let rInfo = <K8sResourceInfo>r.ResourceInfo
+    rInfo.PodStatus = "Error"
+    rInfo.PodStatusMessage = "I'm a pod in Error"
 
     let actual = getResourceAlerts(r)
     let expectedAlerts: Array<Alert> = [
@@ -30,8 +31,9 @@ describe("getResourceAlerts", () => {
   })
 
   it("K8s Resource: should show pod restart alert ", () => {
-    let r: Resource = emptyK8Resource()
-    r.ResourceInfo.PodRestarts = 1
+    let r: Resource = k8sResource()
+    let rInfo = <K8sResourceInfo>r.ResourceInfo
+    rInfo.PodRestarts = 1
     let actual = getResourceAlerts(r)
     let expectedAlerts: Array<Alert> = [
       {
@@ -46,7 +48,7 @@ describe("getResourceAlerts", () => {
   })
 
   it("K8s Resource should show the first build alert", () => {
-    let r: Resource = emptyK8Resource()
+    let r: Resource = k8sResource()
     r.BuildHistory = [
       {
         Log: "Build error log",
@@ -70,11 +72,8 @@ describe("getResourceAlerts", () => {
   })
 
   it("K8s Resource: should show a crash rebuild alert  using the first build info", () => {
-    let r: Resource = emptyK8Resource()
+    let r: Resource = k8sResource()
     r.CrashLog = "Hello I am a crash log"
-    if (r.ResourceInfo.type !== "DCResourceInfo") {
-      r.ResourceInfo.PodCreationTime = "10:00AM"
-    }
     r.BuildHistory = [
       {
         Log: "Hello I am a log",
@@ -87,6 +86,9 @@ describe("getResourceAlerts", () => {
         Error: null,
       },
     ]
+    let rInfo = <K8sResourceInfo>r.ResourceInfo
+    rInfo.PodCreationTime = "10:00AM"
+
     let actual = getResourceAlerts(r)
     let expectedAlerts: Array<Alert> = [
       {
@@ -101,7 +103,7 @@ describe("getResourceAlerts", () => {
   })
 
   it("K8s Resource: should show a warning alert using the first build history ", () => {
-    let r: Resource = emptyK8Resource()
+    let r: Resource = k8sResource()
     r.BuildHistory = [
       {
         Log: "Hello I'm a log",
@@ -128,10 +130,12 @@ describe("getResourceAlerts", () => {
   })
 
   it("K8s Resource: should show a pod restart alert and a build failed alert", () => {
-    let r: Resource = emptyK8Resource()
-    r.ResourceInfo.PodRestarts = 1 // triggers pod restart alert
+    let r: Resource = k8sResource()
+    let rInfo = <K8sResourceInfo>r.ResourceInfo
+    rInfo.PodRestarts = 1 // triggers pod restart alert
+    rInfo.PodCreationTime = "10:00AM"
+
     r.CrashLog = "I'm a pod that crashed"
-    r.ResourceInfo.PodCreationTime = "10:00AM"
     r.BuildHistory = [
       // triggers build failed alert
       {
@@ -163,7 +167,7 @@ describe("getResourceAlerts", () => {
   })
 
   it("K8s Resource: should show 3 alerts: 1 crash rebuild alert, 1 build failed alert, 1 warning alert ", () => {
-    let r: Resource = emptyK8Resource()
+    let r: Resource = k8sResource()
     r.CrashLog = "Hello I am a crash log"
     r.BuildHistory = [
       {
@@ -201,8 +205,9 @@ describe("getResourceAlerts", () => {
   })
 
   it("K8s Resource: should show number of alerts a resource has", () => {
-    let r: Resource = emptyK8Resource()
-    r.ResourceInfo.PodRestarts = 1
+    let r: Resource = k8sResource()
+    let rInfo = <K8sResourceInfo>r.ResourceInfo
+    rInfo.PodRestarts = 1
     r.Alerts = getResourceAlerts(r)
     let actualNum = numberOfAlerts(r)
     let expectedNum = 1
@@ -360,7 +365,7 @@ it("renders a build error for both a K8s resource and DC resource ", () => {
       Warnings: ["This warning shouldn't show up", "Or this one"],
     },
   ]
-  let k8sresource: Resource = emptyK8Resource()
+  let k8sresource: Resource = k8sResource()
   k8sresource.BuildHistory = [
     {
       Log: "Hi this (k8s) resource failed too :)",
@@ -396,7 +401,7 @@ it("renders a build error for both a K8s resource and DC resource ", () => {
   expect(actual).toEqual(expectedAlerts)
 })
 
-function emptyK8Resource(): Resource {
+function k8sResource(): Resource {
   return {
     Name: "snack",
     CombinedLog: "",
@@ -479,7 +484,6 @@ function dcResource(): Resource {
     },
     RuntimeStatus: "ok",
     IsTiltfile: false,
-    ShowBuildStatus: true,
     CombinedLog:
       "\n\u001b[34m──┤ Building: \u001b[0mvigoda\u001b[34m ├──────────────────────────────────────────────\u001b[0m\n\u001b[34mSTEP 1/1 — \u001b[0mBuilding Dockerfile: [docker.io/library/vigoda_for_dc]\nBuilding Dockerfile:\n  FROM golang:1.10\n  \n  ADD . /go/src/github.com/windmilleng/servantes/vigoda\n  RUN go install github.com/windmilleng/servantes/vigoda\n  \n  ENV TEMPLATE_DIR /go/src/github.com/windmilleng/servantes/vigoda/web/templates\n  \n  ENTRYPOINT /go/bin/vigoda\n\n\n\u001b[34m  │ \u001b[0mTarring context…\n\u001b[34m  │ \u001b[0mBuilding image\n    ╎ copy /context /\n    ╎ copy /context / done | 224ms\n    ╎ [1/3] FROM docker.io/library/golang:1.10\n    ╎ [2/3] ADD . /go/src/github.com/windmilleng/servantes/vigoda\n    ╎ [3/3] RUN go install github.com/windmilleng/servantes/vigoda\n    ╎ [3/3] done | 1.836s\n    ╎ exporting to image\n\nCreating servantes_vigoda_1 ... \r\n\u001b[1A\u001b[2K\rCreating servantes_vigoda_1 ... \u001b[32mdone\u001b[0m\r\u001b[1B\u001b[34m  │ \u001b[0mStep 1 - 2.735s\n\u001b[34m  │ \u001b[0mDone in: 5.145s \n\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:43:38.877497931Z 2019/08/07 15:43:38 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:43:40.876938430Z 2019/08/07 15:43:40 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:43:42.879584079Z 2019/08/07 15:43:42 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:43:44.878642925Z 2019/08/07 15:43:44 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:43:46.879662719Z 2019/08/07 15:43:46 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:43:48.882274621Z 2019/08/07 15:43:48 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:43:50.861075847Z 2019/08/07 15:43:50 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:43:52.861272944Z 2019/08/07 15:43:52 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:43:54.861524440Z 2019/08/07 15:43:54 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:43:56.861696610Z 2019/08/07 15:43:56 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:43:58.862126678Z 2019/08/07 15:43:58 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:44:00.862754783Z 2019/08/07 15:44:00 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:44:02.863098595Z 2019/08/07 15:44:02 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:44:04.863405587Z 2019/08/07 15:44:04 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:44:06.863710630Z 2019/08/07 15:44:06 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:44:08.864700524Z 2019/08/07 15:44:08 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:44:10.865846654Z 2019/08/07 15:44:10 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:44:12.866805855Z 2019/08/07 15:44:12 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:44:14.866990429Z 2019/08/07 15:44:14 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:44:16.868635304Z 2019/08/07 15:44:16 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:44:18.871037376Z 2019/08/07 15:44:18 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:44:20.850004669Z 2019/08/07 15:44:20 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:44:22.850199360Z 2019/08/07 15:44:22 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:44:24.850466551Z 2019/08/07 15:44:24 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:44:26.850814196Z 2019/08/07 15:44:26 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:44:28.851385278Z 2019/08/07 15:44:28 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:44:30.856848209Z 2019/08/07 15:44:30 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:44:32.852509949Z 2019/08/07 15:44:32 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:44:34.853034950Z 2019/08/07 15:44:34 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:44:36.853769552Z 2019/08/07 15:44:36 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:44:38.855119725Z 2019/08/07 15:44:38 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:44:40.855541030Z 2019/08/07 15:44:40 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:44:42.856937115Z 2019/08/07 15:44:42 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:44:44.857522514Z 2019/08/07 15:44:44 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:44:46.858652465Z 2019/08/07 15:44:46 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:44:48.859603605Z 2019/08/07 15:44:48 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:44:50.838684337Z 2019/08/07 15:44:50 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:44:52.839122921Z 2019/08/07 15:44:52 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:44:54.839763392Z 2019/08/07 15:44:54 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:44:56.840349509Z 2019/08/07 15:44:56 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:44:58.840822171Z 2019/08/07 15:44:58 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:45:00.840846852Z 2019/08/07 15:45:00 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:45:02.841764655Z 2019/08/07 15:45:02 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:45:04.842121588Z 2019/08/07 15:45:04 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:45:06.842686609Z 2019/08/07 15:45:06 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:45:08.843235140Z 2019/08/07 15:45:08 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:45:10.843617268Z 2019/08/07 15:45:10 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:45:12.844101030Z 2019/08/07 15:45:12 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:45:14.844335731Z 2019/08/07 15:45:14 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:45:16.845612367Z 2019/08/07 15:45:16 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:45:18.846818405Z 2019/08/07 15:45:18 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:45:20.825827247Z 2019/08/07 15:45:20 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:45:22.826246643Z 2019/08/07 15:45:22 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:45:24.826573026Z 2019/08/07 15:45:24 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:45:26.826906606Z 2019/08/07 15:45:26 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:45:28.827086696Z 2019/08/07 15:45:28 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:45:30.828016224Z 2019/08/07 15:45:30 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:45:32.830856549Z 2019/08/07 15:45:32 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:45:34.831068046Z 2019/08/07 15:45:34 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:45:36.831767838Z 2019/08/07 15:45:36 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:45:38.832336622Z 2019/08/07 15:45:38 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:45:40.833605370Z 2019/08/07 15:45:40 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:45:42.834266733Z 2019/08/07 15:45:42 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:45:44.835463644Z 2019/08/07 15:45:44 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:45:46.836028837Z 2019/08/07 15:45:46 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:45:48.836319511Z 2019/08/07 15:45:48 Server status: All good\n\u001b[36mvigoda_1       |\u001b[0m 2019-08-07T15:45:50.815110337Z 2019/08/07 15:45:50 Server status: All good\n",
     CrashLog: "",
