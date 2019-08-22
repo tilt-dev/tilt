3944d662286c4ff7fdc8d5a4ba9a74ec203d7f42
Nick Santos <nicholas.j.santos@gmail.com>
2019-08-22 19:22:41 -0400

    web: make the snapshot modal prettier (#2100)

--- web/src/ShareSnapshotModal.test.tsx
+++ web/src/ShareSnapshotModal.test.tsx
@@ -1,19 +1,48 @@
 import React from "react"
+import ReactDOM from "react-dom"
+import ReactModal from "react-modal"
 import renderer from "react-test-renderer"
 import ShareSnapshotModal from "./ShareSnapshotModal"
 
 const fakeSendsnapshot = () => {}
 const fakeHandleCloseModal = () => {}
+let originalCreatePortal = ReactDOM.createPortal
 
 describe("ShareSnapshotModal", () => {
+  beforeEach(() => {
+    ReactModal.setAppElement(document.body)
+    let mock: any = (node: any) => node
+    ReactDOM.createPortal = mock
+  })
+
+  afterEach(() => {
+    ReactDOM.createPortal = originalCreatePortal
+  })
+
   it("renders with modal open", () => {
     const tree = renderer
       .create(
         <ShareSnapshotModal
           handleSendSnapshot={fakeSendsnapshot}
           handleClose={fakeHandleCloseModal}
-          show={true}
+          isOpen={true}
           snapshotUrl="http://test.com"
+          registerTokenUrl="http://registertoken.com"
+        />
+      )
+      .toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it("renders without snapshotUrl", () => {
+    const tree = renderer
+      .create(
+        <ShareSnapshotModal
+          handleSendSnapshot={fakeSendsnapshot}
+          handleClose={fakeHandleCloseModal}
+          isOpen={true}
+          snapshotUrl=""
           registerTokenUrl="http://registertoken.com"
         />
       )
@@ -28,7 +57,7 @@ describe("ShareSnapshotModal", () => {
         <ShareSnapshotModal
           handleSendSnapshot={fakeSendsnapshot}
           handleClose={fakeHandleCloseModal}
-          show={false}
+          isOpen={false}
           snapshotUrl="http://test.com"
           registerTokenUrl="http://registertoken.com"
         />
