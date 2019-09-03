d0b413acf47df2854eb10cee252a541eb3acfe75
Matt Landis <matt@windmill.engineering>
2019-09-03 14:07:09 -0400

    tilt: pass tilt callback to register_token

--- web/src/ShareSnapshotModal.tsx
+++ web/src/ShareSnapshotModal.tsx
@@ -43,6 +43,9 @@ export default class ShareSnapshotModal extends PureComponent<props> {
   }
 
   tiltCloudCopy() {
+    let callbackURL = `${window.location.protocol}//${
+      window.location.host
+    }/api/refresh_tilt_cloud_whoami`
     if (!this.props.tiltCloudUsername) {
       return (
         <div>
@@ -59,6 +62,7 @@ export default class ShareSnapshotModal extends PureComponent<props> {
                 type="hidden"
                 value={cookies.get("Tilt-Token")}
               />
+              <input name="tilt_callback" type="hidden" value={callbackURL} />
             </form>
             to associate this copy of Tilt with your TiltCloud account.
           </div>
