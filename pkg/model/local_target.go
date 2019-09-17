95522d1952e016ea603f297915c2441de0cb9f52
Maia McCormick <maia@windmill.engineering>
2019-09-17 12:11:06 -0400

    model: LocalTarget [ch3471] (#2201)
    * model: LocalTarget [ch3471]
    
    * typo whoops
    
    * code review

---
+++ pkg/model/local_target.go
@@ -0,0 +1,66 @@
+package model
+
+import (
+	"fmt"
+
+	"github.com/windmilleng/tilt/internal/sliceutils"
+)
+
+type LocalTarget struct {
+	Cmd  Cmd
+	deps []string // a list of ABSOLUTE file paths that are dependencies of this target
+
+	dockerignores []Dockerignore
+	repos         []LocalGitRepo
+}
+
+var _ TargetSpec = LocalTarget{}
+
+func NewLocalTarget(cmd Cmd, deps []string) LocalTarget {
+	return LocalTarget{Cmd: cmd, deps: deps}
+}
+
+func (lt LocalTarget) WithRepos(repos []LocalGitRepo) LocalTarget {
+	lt.repos = append(append([]LocalGitRepo{}, lt.repos...), repos...)
+	return lt
+}
+
+func (lt LocalTarget) WithDockerignores(dockerignores []Dockerignore) LocalTarget {
+	lt.dockerignores = append(append([]Dockerignore{}, lt.dockerignores...), dockerignores...)
+	return lt
+}
+
+func (lt LocalTarget) ID() TargetID {
+	return TargetID{
+		Name: TargetName(lt.Cmd.String()),
+		Type: TargetTypeLocal,
+	}
+}
+
+func (lt LocalTarget) DependencyIDs() []TargetID {
+	return nil
+}
+
+func (lt LocalTarget) Validate() error {
+	if lt.Cmd.Empty() {
+		return fmt.Errorf("[Validate] LocalTarget missing command")
+	}
+	return nil
+}
+
+// Implements: engine.WatchableManifest
+func (lt LocalTarget) Dependencies() []string {
+	return sliceutils.DedupedAndSorted(lt.deps)
+}
+
+func (lt LocalTarget) LocalRepos() []LocalGitRepo {
+	return lt.repos
+}
+
+func (lt LocalTarget) Dockerignores() []Dockerignore {
+	return lt.dockerignores
+}
+
+func (lt LocalTarget) IgnoredLocalDirectories() []string {
+	return nil
+}
