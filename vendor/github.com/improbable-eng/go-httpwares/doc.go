eba3ccba04a84e8e9868f3da7204a0224af76084
Nick Santos <nicholas.j.santos@gmail.com>
2019-09-19 11:05:05 -0400

    vendor: update to kubernetes 1.16

--- vendor/github.com/improbable-eng/go-httpwares/doc.go
+++
@@ -1,8 +0,0 @@
-// Copyright 2017 Michal Witkowski. All Rights Reserved.
-// See LICENSE for licensing terms.
-
-/*
-`httpwares` is a collection of middlewares (server-side) and tripperwares (client-side) for vanilla Golang HTTP libs
-
-*/
-package httpwares
