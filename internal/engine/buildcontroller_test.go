b938024fc9c621ebe730f0a69fbcb19f77767679
Nick Santos <nicholas.j.santos@gmail.com>
2019-09-19 17:06:58 -0400

    engine: respect the native docker-compose order. Fixes part of issue #2210

--- internal/engine/buildcontroller_test.go
+++ internal/engine/buildcontroller_test.go
@@ -655,3 +655,32 @@ func TestBuildControllerUnresourcedYAMLFirst(t *testing.T) {
 	}
 	assert.Equal(t, expectedBuildOrder, observedBuildOrder)
 }
+
+func TestBuildControllerRespectDockerComposeOrder(t *testing.T) {
+	f := newTestFixture(t)
+	defer f.TearDown()
+
+	sancho := NewSanchoFastBuildDCManifest(f)
+	redis := manifestbuilder.New(f, "redis").WithDockerCompose().Build()
+	donQuixote := manifestbuilder.New(f, "don-quixote").WithDockerCompose().Build()
+	manifests := []model.Manifest{redis, sancho, donQuixote}
+	f.Start(manifests, true)
+
+	var observedBuildOrder []string
+	for i := 0; i < len(manifests); i++ {
+		call := f.nextCall()
+		observedBuildOrder = append(observedBuildOrder, call.dc().Name.String())
+	}
+
+	// If these were Kubernetes resources, we would try to deploy don-quixote
+	// before sancho, because it doesn't have an image build.
+	//
+	// But this would be wrong, because Docker Compose has stricter ordering requirements, see:
+	// https://docs.docker.com/compose/startup-order/
+	expectedBuildOrder := []string{
+		"redis",
+		"sancho",
+		"don-quixote",
+	}
+	assert.Equal(t, expectedBuildOrder, observedBuildOrder)
+}
