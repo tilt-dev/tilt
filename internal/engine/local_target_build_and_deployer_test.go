946df231e7c5e015a89ed5e5798396985e72174f
Maia <maia@windmill.engineering>
2019-09-24 14:54:22 -0400

    go imports

--- internal/engine/local_target_build_and_deployer_test.go
+++ internal/engine/local_target_build_and_deployer_test.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
 	"github.com/windmilleng/tilt/internal/testutils/tempdir"
 
 	"github.com/windmilleng/tilt/internal/store"
