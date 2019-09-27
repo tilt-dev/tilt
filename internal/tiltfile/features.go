6f78648a50a62946a5e1912ac6149d4e39f69a6c
Matt Landis <landism@users.noreply.github.com>
2019-09-27 15:35:18 -0400

    tiltfile: add disable_snapshots (#2270)

--- internal/tiltfile/features.go
+++ internal/tiltfile/features.go
@@ -41,3 +41,20 @@ func (s *tiltfileState) disableFeature(thread *starlark.Thread, fn *starlark.Bui
 
 	return starlark.None, nil
 }
+
+func (s *tiltfileState) disableSnapshots(thread *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	err := s.unpackArgs(fn.Name(), args, kwargs)
+	if err != nil {
+		return nil, err
+	}
+
+	err = s.features.Set(feature.Snapshots, false)
+	if err != nil {
+		if _, ok := err.(feature.ObsoleteError); !ok {
+			return nil, err
+		}
+		s.warnings = append(s.warnings, err.Error())
+	}
+
+	return starlark.None, nil
+}
