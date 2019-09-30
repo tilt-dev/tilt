64a01a01e2c786035de20c15d85c5ecf4305fe04
Rudi Chiarito <rudi@ctrl-labs.com>
2019-09-30 14:08:58 -0400

    Let SYNCLET_IMAGE variable override Docker image

--- internal/synclet/sidecar/sidecar.go
+++ internal/synclet/sidecar/sidecar.go
@@ -2,6 +2,7 @@ package sidecar
 
 import (
 	"fmt"
+	"os"
 
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
@@ -14,9 +15,18 @@ func syncletPrivileged() *bool {
 	return &val
 }
 
+func getImageName() string {
+	v := os.Getenv(SyncletImageEnvVar)
+	if v == "" {
+		return SyncletImageName
+	}
+	return v
+}
+
 // When we deploy Tilt for development, we override this with LDFLAGS
 var SyncletTag = "v20190904"
 
+const SyncletImageEnvVar = "SYNCLET_IMAGE"
 const SyncletImageName = "gcr.io/windmill-public-containers/tilt-synclet"
 const SyncletContainerName = "tilt-synclet"
 
@@ -24,7 +34,7 @@ var SyncletImageRef = container.MustParseNamed(SyncletImageName)
 
 var SyncletContainer = v1.Container{
 	Name:            SyncletContainerName,
-	Image:           fmt.Sprintf("%s:%s", SyncletImageName, SyncletTag),
+	Image:           fmt.Sprintf("%s:%s", getImageName(), SyncletTag),
 	ImagePullPolicy: v1.PullIfNotPresent,
 	Resources:       v1.ResourceRequirements{Requests: v1.ResourceList{v1.ResourceCPU: resource.MustParse("0Mi")}},
 	VolumeMounts: []v1.VolumeMount{
