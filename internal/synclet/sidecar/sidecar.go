tilt-dev/tilt

commit 74f58533d75e7ca5111b1ff47f5079f25dfc63bd

Let TILT_SYNCLET_IMAGE variable override Docker image (#2277)
* Let SYNCLET_IMAGE variable override Docker image

* Rename variable

internal/synclet/sidecar/sidecar.go
@@ -2,6 +2,7 @@ package sidecar
 
 import (
 	"fmt"
+	"os"
 
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
@@ -14,9 +15,18 @@ func syncletPrivileged() *bool {
 	return &val
 }
 
+func getImageName() string {
+	v := os.Getenv(SyncletImageEnvVar)
+	if v == "" {
+		return SyncletImageName
+	}
+	return v
+}
+
 // When we deploy Tilt for development, we override this with LDFLAGS
 var SyncletTag = "v20190904"
 
+const SyncletImageEnvVar = "TILT_SYNCLET_IMAGE"
 const SyncletImageName = "gcr.io/windmill-public-containers/tilt-synclet"
 const SyncletContainerName = "tilt-synclet"
 
@@ -24,7 +34,7 @@ var SyncletImageRef = container.MustParseNamed(SyncletImageName)
 
 var SyncletContainer = v1.Container{
 	Name:            SyncletContainerName,
-	Image:           fmt.Sprintf("%s:%s", SyncletImageName, SyncletTag),
+	Image:           fmt.Sprintf("%s:%s", getImageName(), SyncletTag),
 	ImagePullPolicy: v1.PullIfNotPresent,
 	Resources:       v1.ResourceRequirements{Requests: v1.ResourceList{v1.ResourceCPU: resource.MustParse("0Mi")}},
 	VolumeMounts: []v1.VolumeMount{

commit 36f04931eed5d6549ec7c2876c51e813a7b43f3a

synclet: fix a global init bug introduced by https://github.com/windmilleng/tilt/pull/2277

internal/synclet/sidecar/sidecar.go
@@ -15,10 +15,12 @@ func syncletPrivileged() *bool {
 	return &val
 }
 
+const defaultSyncletImageName = "gcr.io/windmill-public-containers/tilt-synclet"
+
 func getImageName() string {
 	v := os.Getenv(SyncletImageEnvVar)
 	if v == "" {
-		return SyncletImageName
+		return defaultSyncletImageName
 	}
 	return v
 }
@@ -27,7 +29,10 @@ func getImageName() string {
 var SyncletTag = "v20190904"
 
 const SyncletImageEnvVar = "TILT_SYNCLET_IMAGE"
-const SyncletImageName = "gcr.io/windmill-public-containers/tilt-synclet"
+
+// TODO(nick): Revamp this to not rely on global init
+var SyncletImageName = getImageName()
+
 const SyncletContainerName = "tilt-synclet"
 
 var SyncletImageRef = container.MustParseNamed(SyncletImageName)