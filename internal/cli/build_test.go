f3ce8bdf7f30f6f9685da13e3582114c22afa70b
Dan Miller <dmiller@windmill.engineering>
2019-10-04 16:19:19 -0400

    fix vet

--- internal/cli/build_test.go
+++ internal/cli/build_test.go
@@ -14,7 +14,7 @@ func TestProvideDevWebVersion(t *testing.T) {
 
 func TestProvideProdWebVersion(t *testing.T) {
 	expected := fmt.Sprintf("v%s", devVersion)
-	actual := provideWebVersion(model.TiltBuild{"0.10.13", "", false})
+	actual := provideWebVersion(model.TiltBuild{Version: "0.10.13", Date: "", Dev: false})
 
 	assert.Equal(t, expected, string(actual))
 }
