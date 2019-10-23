9c70dd5d3ff3adfff4c8bee10d1380df3fc75ef1
Dan Miller <dmiller@windmill.engineering>
2019-10-23 17:06:19 -0400

    run toast proto

--- web/src/viewServiceClientPb.ts
+++ web/src/viewServiceClientPb.ts
@@ -6,50 +6,55 @@
 
 // GENERATED CODE -- DO NOT EDIT!
 
-import * as grpcWeb from "grpc-web"
 
-import { GetViewRequest, View } from "./view_pb"
+import * as grpcWeb from 'grpc-web';
+
+import * as google_api_annotations_pb from '../../google/api/annotations_pb';
+
+import {
+  GetViewRequest,
+  View} from './view_pb';
 
 export class ViewServiceClient {
-  client_: grpcWeb.AbstractClientBase
-  hostname_: string
-  credentials_: null | { [index: string]: string }
-  options_: null | { [index: string]: string }
+  client_: grpcWeb.AbstractClientBase;
+  hostname_: string;
+  credentials_: null | { [index: string]: string; };
+  options_: null | { [index: string]: string; };
 
-  constructor(
-    hostname: string,
-    credentials?: null | { [index: string]: string },
-    options?: null | { [index: string]: string }
-  ) {
-    if (!options) options = {}
-    if (!credentials) credentials = {}
-    options["format"] = "text"
+  constructor (hostname: string,
+               credentials?: null | { [index: string]: string; },
+               options?: null | { [index: string]: string; }) {
+    if (!options) options = {};
+    if (!credentials) credentials = {};
+    options['format'] = 'text';
 
-    this.client_ = new grpcWeb.GrpcWebClientBase(options)
-    this.hostname_ = hostname
-    this.credentials_ = credentials
-    this.options_ = options
+    this.client_ = new grpcWeb.GrpcWebClientBase(options);
+    this.hostname_ = hostname;
+    this.credentials_ = credentials;
+    this.options_ = options;
   }
 
   methodInfoGetView = new grpcWeb.AbstractClientBase.MethodInfo(
     View,
     (request: GetViewRequest) => {
-      return request.serializeBinary()
+      return request.serializeBinary();
     },
     View.deserializeBinary
-  )
+  );
 
   getView(
     request: GetViewRequest,
     metadata: grpcWeb.Metadata | null,
-    callback: (err: grpcWeb.Error, response: View) => void
-  ) {
+    callback: (err: grpcWeb.Error,
+               response: View) => void) {
     return this.client_.rpcCall(
-      this.hostname_ + "/webview.ViewService/GetView",
+      this.hostname_ +
+        '/webview.ViewService/GetView',
       request,
       metadata || {},
       this.methodInfoGetView,
-      callback
-    )
+      callback);
   }
+
 }
+
