e37418287a689fcd88686b3b20ba73f787008eae
Matt Landis <matt@windmill.engineering>
2019-11-01 15:41:03 -0400

    Revert "pod_watch: only emit events from pods whose specs we have deployed since startup (#2439)"
    This reverts commit 58c787b18e26f31b0d42fc2900b25ee1f43c0268.

--- internal/testutils/podbuilder/podbuilder.go
+++ internal/testutils/podbuilder/podbuilder.go
@@ -70,20 +70,16 @@ type PodBuilder struct {
 	imageRefs map[int]string
 	cIDs      map[int]string
 	cReady    map[int]bool
-
-	setPodTemplateSpecHash bool
-	podTemplateSpecHash    k8s.PodTemplateSpecHash
 }
 
 func New(t testing.TB, manifest model.Manifest) PodBuilder {
 	return PodBuilder{
-		t:                      t,
-		manifest:               manifest,
-		imageRefs:              make(map[int]string),
-		cIDs:                   make(map[int]string),
-		cReady:                 make(map[int]bool),
-		extraPodLabels:         make(map[string]string),
-		setPodTemplateSpecHash: true,
+		t:              t,
+		manifest:       manifest,
+		imageRefs:      make(map[int]string),
+		cIDs:           make(map[int]string),
+		cReady:         make(map[int]bool),
+		extraPodLabels: make(map[string]string),
 	}
 }
 
@@ -94,16 +90,6 @@ func (b PodBuilder) WithPodLabel(key, val string) PodBuilder {
 
 func (b PodBuilder) ManifestName() model.ManifestName {
 	return b.manifest.Name
-}
-
-func (b PodBuilder) WithTemplateSpecHash(s k8s.PodTemplateSpecHash) PodBuilder {
-	b.podTemplateSpecHash = s
-	return b
-}
-
-func (b PodBuilder) WithNoTemplateSpecHash() PodBuilder {
-	b.setPodTemplateSpecHash = false
-	return b
 }
 
 func (b PodBuilder) RestartCount() int {
@@ -260,19 +246,6 @@ func (b PodBuilder) buildLabels(tSpec *v1.PodTemplateSpec) map[string]string {
 	for k, v := range b.extraPodLabels {
 		labels[k] = v
 	}
-
-	if b.setPodTemplateSpecHash {
-		podTemplateSpecHash := b.podTemplateSpecHash
-		if podTemplateSpecHash == "" {
-			var err error
-			podTemplateSpecHash, err = k8s.HashPodTemplateSpec(tSpec)
-			if err != nil {
-				panic(fmt.Sprintf("error computing pod template spec hash: %v", err))
-			}
-		}
-		labels[k8s.TiltPodTemplateHashLabel] = string(podTemplateSpecHash)
-	}
-
 	return labels
 }
 
