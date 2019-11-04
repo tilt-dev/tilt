3dcb8a8bc67d04ef84bb9e049125bf6012aff31b
Nick Santos <nicholas.j.santos@gmail.com>
2019-11-04 17:56:30 -0500

    integration: add more logging to onewatchexec

--- integration/onewatch_exec_test.go
+++ integration/onewatch_exec_test.go
@@ -4,6 +4,7 @@ package integration
 
 import (
 	"context"
+	"fmt"
 	"testing"
 	"time"
 
@@ -16,14 +17,18 @@ func TestWatchExec(t *testing.T) {
 
 	f.TiltWatchExec()
 
+	fmt.Printf("Wait for pods ready\n")
 	ctx, cancel := context.WithTimeout(f.ctx, time.Minute)
 	defer cancel()
 	oneUpPods := f.WaitForAllPodsReady(ctx, "app=onewatchexec")
 
+	fmt.Printf("Pods ready: %s\n", oneUpPods)
+	fmt.Printf("Wait for server available on 31234\n")
 	ctx, cancel = context.WithTimeout(f.ctx, time.Minute)
 	defer cancel()
 	f.CurlUntil(ctx, "http://localhost:31234", "🍄 One-Up! 🍄")
 
+	fmt.Printf("Replace source.txt contents and wait for change to take effect")
 	f.ReplaceContents("source.txt", "One-Up", "Two-Up")
 
 	ctx, cancel = context.WithTimeout(f.ctx, time.Minute)
@@ -31,8 +36,8 @@ func TestWatchExec(t *testing.T) {
 	f.CurlUntil(ctx, "http://localhost:31234", "🍄 Two-Up! 🍄")
 
 	twoUpPods := f.WaitForAllPodsReady(ctx, "app=onewatchexec")
+	fmt.Printf("New pods ready: %s\n", twoUpPods)
 	// Assert that the pods were changed in-place, and not that we
 	// created new pods.
 	assert.Equal(t, oneUpPods, twoUpPods)
-
 }
