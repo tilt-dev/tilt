b41fa090b235fa4431796029bcb2d0733f86b435
Maia <maia@windmill.engineering>
2019-11-05 12:10:39 -0500

    test for button disabled

--- web/src/SidebarTriggerButton.test.tsx
+++ web/src/SidebarTriggerButton.test.tsx
@@ -34,4 +34,26 @@ describe("SidebarTriggerButton", () => {
       JSON.stringify({ manifest_names: ["doggos"] })
     )
   })
+
+  it("disables button when resource is queued", () => {
+    fetchMock.mockResponse(JSON.stringify({}))
+
+    const root = mount(
+      <SidebarTriggerButton
+        isSelected={true}
+        resourceName="doggos"
+        triggerMode={TriggerMode.TriggerModeManual}
+        hasBuilt={true}
+        isBuilding={false}
+        hasPendingChanges={false}
+        isQueued={true}
+      />
+    )
+
+    let element = root.find(".SidebarTriggerButton")
+    expect(element).toHaveLength(1)
+    element.simulate("click")
+
+    expect(fetchMock.mock.calls.length).toEqual(0)
+  })
 })
