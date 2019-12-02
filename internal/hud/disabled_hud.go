0328de1414104e5d6f414f4809f36b7607cf384e
Maia <maia@windmill.engineering>
2019-12-02 17:01:58 -0500

    upper test

--- internal/hud/disabled_hud.go
+++ internal/hud/disabled_hud.go
@@ -13,38 +13,36 @@ import (
 var _ HeadsUpDisplay = &DisabledHud{}
 
 type DisabledHud struct {
-	mu               sync.RWMutex
+	mu sync.RWMutex
 
-	processedLogByteCount int
+	ProcessedLogByteCount int
 }
-
 
 func NewDisabledHud() HeadsUpDisplay {
 	return &DisabledHud{}
 }
-
 
 func (h *DisabledHud) Run(ctx context.Context, dispatch func(action store.Action), refreshRate time.Duration) error {
 	return nil
 }
 
 func (h *DisabledHud) OnChange(ctx context.Context, st store.RStore) {
-		state := st.RLockState()
-		log := state.Log
-		st.RUnlockState()
+	state := st.RLockState()
+	log := state.Log
+	st.RUnlockState()
 
-		h.mu.Lock()
-		defer h.mu.Unlock()
+	h.mu.Lock()
+	defer h.mu.Unlock()
 
-		h.printLatestLogs(ctx, log)
+	h.printLatestLogs(ctx, log)
 }
 
 // printLatestLogs prints the new logs to stdout.
 func (h *DisabledHud) printLatestLogs(ctx context.Context, log model.Log) {
 	logLen := log.Len()
-	if h.processedLogByteCount < logLen {
-		fmt.Print(log.String()[h.processedLogByteCount:])
+	if h.ProcessedLogByteCount < logLen {
+		fmt.Print(log.String()[h.ProcessedLogByteCount:])
 	}
 
-	h.processedLogByteCount = logLen
+	h.ProcessedLogByteCount = logLen
 }
