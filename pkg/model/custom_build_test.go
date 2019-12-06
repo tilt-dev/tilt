247b3df892bb7253176a293130b348f9dd9bb5df
Maia <maia@windmill.engineering>
2019-12-06 17:28:11 -0500

    fix model tests

--- pkg/model/custom_build_test.go
+++ pkg/model/custom_build_test.go
@@ -8,35 +8,6 @@ import (
 	"github.com/windmilleng/tilt/internal/container"
 )
 
-func TestAnyFastBuildInfo(t *testing.T) {
-	fb := FastBuild{
-		BaseDockerfile: "FROM alpine",
-		Entrypoint:     Cmd{[]string{"echo", "hi"}},
-	}
-	cb := CustomBuild{
-		Command: "true",
-		Deps:    []string{"foo", "bar"},
-		Fast:    fb,
-	}
-	it := ImageTarget{
-		BuildDetails: cb,
-	}
-	bi := it.AnyFastBuildInfo()
-	assert.Equal(t, fb, bi)
-
-	it = ImageTarget{
-		BuildDetails: fb,
-	}
-	bi = it.AnyFastBuildInfo()
-	assert.Equal(t, fb, bi)
-
-	it = ImageTarget{
-		BuildDetails: DockerBuild{},
-	}
-	bi = it.AnyFastBuildInfo()
-	assert.True(t, bi.Empty())
-}
-
 func TestEmptyLiveUpdate(t *testing.T) {
 	lu, err := NewLiveUpdate(nil, "/base/dir")
 	if err != nil {
