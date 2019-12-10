07de3cdfa50dae4354fba9d2ee3cf90737488ef5
Nick Santos <nicholas.j.santos@gmail.com>
2019-12-10 17:10:40 -0500

    web: use structured logs to build a data model (#2649)

---
+++ web/src/logs.ts
@@ -0,0 +1,45 @@
+// Helper functions for dealing with logs
+
+import { LogLine } from "./types"
+
+export function logLinesFromString(
+  log: string,
+  manifestName?: string
+): LogLine[] {
+  let lines = log.split("\n")
+  return lines.map(text => {
+    return {
+      text: text,
+      manifestName: manifestName ?? "",
+    }
+  })
+}
+
+export function logLinesToString(
+  lines: LogLine[],
+  showManifestPrefix: boolean
+): string {
+  return lines
+    .map(line => {
+      let text = line.text + "\n"
+      if (showManifestPrefix) {
+        text = sourcePrefix(line.manifestName) + text
+      }
+      return text
+    })
+    .join("")
+}
+
+export function sourcePrefix(n: string) {
+  if (n == "" || n == "(Tiltfile)") {
+    return ""
+  }
+  let max = 12
+  let spaces = ""
+  if (n.length > max) {
+    n = n.substring(0, max - 1) + "…"
+  } else {
+    spaces = " ".repeat(max - n.length)
+  }
+  return n + spaces + "┊ "
+}
