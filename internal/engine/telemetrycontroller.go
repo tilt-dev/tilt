56a8f032e619e35c86720dac29d9397c1384f3ba
Dan Bentley <dan@windmill.engineering>
2019-12-11 10:38:50 -0500

    wdyt?

--- internal/engine/telemetrycontroller.go
+++ internal/engine/telemetrycontroller.go
@@ -2,7 +2,6 @@ package engine
 
 import (
 	"context"
-	"fmt"
 	"os"
 	"os/exec"
 	"time"
@@ -17,17 +16,15 @@ import (
 type TelemetryController struct {
 	clock build.Clock
 	// lock is a lock that is used to control access from the span exporter and the telemetry controller to the file
-	lock   tracer.Locker
-	dir    *dirs.WindmillDir
-	execer func(ctx context.Context, name string, arg ...string) *exec.Cmd
+	lock tracer.Locker
+	dir  *dirs.WindmillDir
 }
 
-func NewTelemetryController(lock tracer.Locker, clock build.Clock, dir *dirs.WindmillDir, execer func(ctx context.Context, name string, arg ...string) *exec.Cmd) *TelemetryController {
+func NewTelemetryController(lock tracer.Locker, clock build.Clock, dir *dirs.WindmillDir) *TelemetryController {
 	return &TelemetryController{
-		lock:   lock,
-		clock:  clock,
-		dir:    dir,
-		execer: execer,
+		lock:  lock,
+		clock: clock,
+		dir:   dir,
 	}
 }
 
@@ -38,53 +35,36 @@ func (t *TelemetryController) OnChange(ctx context.Context, st store.RStore) {
 	tc := state.TelemetryCmd
 	st.RUnlockState()
 
-	if lastTelemetryRun.Add(1*time.Hour).Before(t.clock.Now()) && !tc.Empty() {
-		t.lock.Lock()
-		defer t.lock.Unlock()
+	if tc.Empty() || !lastTelemetryRun.Add(1*time.Hour).Before(t.clock.Now()) {
+		return
+	}
 
-		// exec the telemetry command, passing in the contents of the file on stdin
-		cmd := t.execer(ctx, tc.String())
-		stdin, err := cmd.StdinPipe()
-		if err != nil {
+	t.lock.Lock()
+	defer t.lock.Unlock()
+
+	// exec the telemetry command, passing in the contents of the file on stdin
+	cmd := exec.CommandContext(ctx, tc.Argv[0], tc.Argv[1:]...)
+	file, err := t.dir.OpenFile(tracer.OutgoingFilename, os.O_RDONLY, 0644)
+	if err != nil {
+		st.Dispatch(NewErrorAction(err))
+		return
+	}
+	defer file.Close()
+
+	cmd.Stdin = file
+
+	err = cmd.Run()
+	if err != nil {
+		st.Dispatch(NewErrorAction(err))
+	}
+	cmdSucceeded := err == nil
+
+	st.Dispatch(TelemetryScriptRanAction{At: t.clock.Now()})
+
+	// truncate the file if the telemetry command succeeded
+	if cmdSucceeded {
+		if err = t.dir.WriteFile(tracer.OutgoingFilename, ""); err != nil {
 			st.Dispatch(NewErrorAction(err))
-			return
-		}
-		if stdin == nil {
-			st.Dispatch(NewErrorAction(fmt.Errorf("Unable to open stdin to telemetry command")))
-			return
-		}
-		defer stdin.Close()
-
-		file, err := t.dir.OpenFile(tracer.OutgoingFilename, os.O_RDONLY, 0644)
-		if err != nil {
-			st.Dispatch(NewErrorAction(err))
-			return
-		}
-
-		cmd.Stdin = file
-
-		err = cmd.Run()
-		if err != nil {
-			st.Dispatch(NewErrorAction(err))
-		}
-		cmdSucceeded := err == nil
-
-		st.Dispatch(TelemetryScriptRanAction{At: t.clock.Now()})
-
-		// truncate the file if the telemetry command succeeded
-		if cmdSucceeded {
-			err = t.clearAnalyticsFile()
-			if err != nil {
-				st.Dispatch(NewErrorAction(err))
-			}
 		}
 	}
-}
-
-func (t *TelemetryController) clearAnalyticsFile() error {
-	return t.dir.WriteFile(tracer.OutgoingFilename, "")
-}
-
-func ProvideExecer() func(ctx context.Context, name string, arg ...string) *exec.Cmd {
-	return exec.CommandContext
 }
