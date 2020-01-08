5bb21fbfa7b3536467d3df2467ef57c0364a0a28
Dan Miller <dmiller@windmill.engineering>
2020-01-08 15:02:56 -0500

    engine: simple local process execer

--- internal/engine/local/execer.go
+++ internal/engine/local/execer.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"os/exec"
 
 	"github.com/windmilleng/tilt/pkg/model"
 )
@@ -12,8 +13,7 @@ type Execer interface {
 	Start(ctx context.Context, cmd model.Cmd, w io.Writer, statusCh chan Status) chan struct{}
 }
 
-type FakeExecer struct {
-}
+type FakeExecer struct{}
 
 func NewFakeExecer() *FakeExecer {
 	return &FakeExecer{}
@@ -36,6 +36,46 @@ func fakeRun(ctx context.Context, cmd model.Cmd, w io.Writer, statusCh chan Stat
 
 	<-ctx.Done()
 	_, _ = fmt.Fprintf(w, "Finished cmd %v", cmd)
+
+	statusCh <- Done
+}
+
+type processExecer struct{}
+
+func NewProcessExecer() *processExecer {
+	return &processExecer{}
+}
+
+func (e *processExecer) Start(ctx context.Context, cmd model.Cmd, w io.Writer, statusCh chan Status) chan struct{} {
+	doneCh := make(chan struct{})
+
+	go processRun(ctx, cmd, w, statusCh, doneCh)
+
+	return doneCh
+}
+
+func processRun(ctx context.Context, cmd model.Cmd, w io.Writer, statusCh chan Status, doneCh chan struct{}) {
+	defer close(doneCh)
+	defer close(statusCh)
+
+	c := exec.CommandContext(ctx, cmd.Argv[0], cmd.Argv[1:]...)
+	c.Stderr = w
+	c.Stdout = w
+
+	err := c.Start()
+	if err != nil {
+		// TODO(dmiller): should this be different status than when the command fails? Unknown?
+		statusCh <- Error
+		return
+	}
+
+	statusCh <- Running
+
+	err = c.Wait()
+	if err != nil {
+		statusCh <- Error
+		return
+	}
 
 	statusCh <- Done
 }
