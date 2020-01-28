a443face9dc6b3ddf5431d40a8cde7d3d67b46c3
Dan Miller <dmiller@windmill.engineering>
2020-01-28 16:13:13 -0500

    Revert "test change"
    This reverts commit dc0d4d7314721c2346bf8c39dfb8e5dd17edab5b.

--- internal/build/tar.go
+++ internal/build/tar.go
@@ -3,7 +3,6 @@ package build
 import (
 	"archive/tar"
 	"context"
-	"fmt"
 	"io"
 	"os"
 	"path/filepath"
@@ -43,7 +42,6 @@ func (a *ArchiveBuilder) Close() error {
 func clearUIDAndGID(h *tar.Header) {
 	h.Uid = 0
 	h.Gid = 0
-	fmt.Println("test")
 }
 
 func (a *ArchiveBuilder) archiveDf(ctx context.Context, df dockerfile.Dockerfile) error {
