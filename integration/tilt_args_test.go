a780954a7909d6b21101cb6be717b5e22855c457
Nick Santos <nicholas.j.santos@gmail.com>
2020-01-28 19:23:23 -0500

    integration: fix a race condition in the tilt args integration test (#2867)

--- integration/tilt_args_test.go
+++ integration/tilt_args_test.go
@@ -3,6 +3,7 @@
 package integration
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
@@ -23,7 +24,16 @@ func TestTiltArgs(t *testing.T) {
 	f.logs.Reset()
 
 	err = f.tilt.Args([]string{"bar"}, f.LogWriter())
-	require.NoError(t, err)
+	if err != nil {
+		// Currently, Tilt starts printing logs before the webserver has bound to a port.
+		// If this happens, just sleep for a second and try again.
+		duration := 2 * time.Second
+		fmt.Printf("Error setting args. Sleeping (%s): %v\n", duration, err)
+
+		time.Sleep(duration)
+		err = f.tilt.Args([]string{"bar"}, f.LogWriter())
+		require.NoError(t, err)
+	}
 
 	err = f.logs.WaitUntilContains("bar run", time.Second)
 	require.NoError(t, err)
