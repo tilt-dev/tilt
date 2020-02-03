0fc407687025befcd1e404ea221f25511f3010a7
Han Yu <han@tilt.dev>
2020-02-03 11:32:05 -0500

    defer to maia’s sense of order

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -21,15 +21,15 @@ import (
 // messages (like Infof) append a newline to the string before passing it to
 // Write().
 type Logger interface {
-	// log information that we always want to show
-	Infof(format string, a ...interface{})
+	// log information that is likely to only be of interest to tilt developers
+	Debugf(format string, a ...interface{})
 
 	// log information that a tilt user might not want to see on every run, but that they might find
 	// useful when debugging their Tiltfile/docker/k8s configs
 	Verbosef(format string, a ...interface{})
 
-	// log information that is likely to only be of interest to tilt developers
-	Debugf(format string, a ...interface{})
+	// log information that we always want to show
+	Infof(format string, a ...interface{})
 
 	// Warnings to show in the alert pane.
 	Warnf(format string, a ...interface{})
@@ -77,9 +77,9 @@ func (l Level) AsSevereAs(log Level) bool {
 
 var (
 	NoneLvl    = Level{id: 0, severity: 0}
-	InfoLvl    = Level{id: 1, severity: 300}
-	VerboseLvl = Level{id: 2, severity: 200}
 	DebugLvl   = Level{id: 3, severity: 100}
+	VerboseLvl = Level{id: 2, severity: 200}
+	InfoLvl    = Level{id: 1, severity: 300}
 	WarnLvl    = Level{id: 4, severity: 400}
 	ErrorLvl   = Level{id: 5, severity: 500}
 )
