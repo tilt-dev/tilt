5ee01e2384380e683907f151ab5457c1ac2066f0
Dan Miller <dmiller@windmill.engineering>
2020-02-06 15:13:04 -0500

    extension: define extension storage API w/ filesystem implementation

---
+++ internal/tiltfile/extension/store.go
@@ -0,0 +1,71 @@
+package extension
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/windmilleng/tilt/internal/tiltfile"
+)
+
+const extensionDirName = "tilt_modules"
+
+type Store interface {
+	// Stat is used to check if an extension exists before fetching it
+	Stat(ctx context.Context, moduleName string) (string, error)
+	Write(ctx context.Context, contents ModuleContents) (string, error)
+}
+
+// TODO(dmiller): should this include an integrity hash?
+type ModuleContents struct {
+	Name             string
+	TiltfileContents string
+	GitCommitHash    string
+	Source           string
+	TimeFetched      time.Time
+}
+
+type LocalStore struct {
+	baseDir string
+}
+
+func NewLocalStore(baseDir string) *LocalStore {
+	return &LocalStore{
+		baseDir: filepath.Join(baseDir, extensionDirName),
+	}
+}
+
+func (s *LocalStore) Stat(ctx context.Context, moduleName string) (string, error) {
+	tiltfilePath := filepath.Join(s.baseDir, moduleName, tiltfile.FileName)
+
+	_, err := os.Stat(tiltfilePath)
+	if err == nil {
+		return tiltfilePath, nil
+	}
+
+	if os.IsNotExist(err) {
+		return "", nil
+	}
+
+	return "", err
+}
+
+func (s *LocalStore) Write(ctx context.Context, contents ModuleContents) (string, error) {
+	moduleDir := filepath.Join(s.baseDir, contents.Name)
+	if err := os.MkdirAll(moduleDir, os.FileMode(0700)); err != nil {
+		return "", fmt.Errorf("couldn't store module %s: %v", contents.Name, err)
+	}
+
+	tiltfilePath := filepath.Join(moduleDir, tiltfile.FileName)
+	// TODO(dmiller): store hash, source, time fetched
+	if err := ioutil.WriteFile(tiltfilePath, []byte(contents.TiltfileContents), os.FileMode(0700)); err != nil {
+		return "", fmt.Errorf("couldn't store module %s: %v", contents.Name, err)
+	}
+
+	return tiltfilePath, nil
+}
+
+var _ Store = (*LocalStore)(nil)
