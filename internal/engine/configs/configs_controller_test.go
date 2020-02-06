a6c9d4d87d25177f8c6e30466fc996fd2f1fe0cb
Nick Santos <nicholas.j.santos@gmail.com>
2020-02-06 15:38:51 -0500

    Revert "engine/ospath: use tempdir.Chdir in more places (#2907)"
    This reverts commit 284ed0748138e3b33d96cd32810c7e6af0ea1bab.

--- internal/engine/configs/configs_controller_test.go
+++ internal/engine/configs/configs_controller_test.go
@@ -3,6 +3,7 @@ package configs
 import (
 	"context"
 	"fmt"
+	"os"
 	"reflect"
 	"testing"
 	"time"
@@ -125,7 +126,19 @@ func (f *ccFixture) run(m model.Manifest) ConfigsReloadedAction {
 	// configs_controller uses state.RelativeTiltfilePath, which is relative to wd
 	// sometimes the original directory was invalid (e.g., it was another test's temp dir, which was deleted,
 	// but not changed out of), and if it was already invalid, then let's not worry about it.
-	f.Chdir()
+	origDir, _ := os.Getwd()
+	err := os.Chdir(f.Path())
+	if err != nil {
+		f.T().Fatalf("error changing dir: %v", err)
+	}
+	defer func() {
+		if origDir != "" {
+			err = os.Chdir(origDir)
+			if err != nil {
+				f.T().Fatalf("unable to restore original wd: '%v'", err)
+			}
+		}
+	}()
 
 	f.st.SetUpSubscribersForTesting(f.ctx)
 
