2cbf100533255c9ea32c8c527c6b9b9ffc4d237f
Dan Miller <dmiller@tilt.dev>
2020-02-07 12:09:03 -0500

    extension: define extension storage API w/ filesystem implementation (#2909)

---
+++ internal/tiltfile/extension/store.go
@@ -0,0 +1,69 @@
+package extension
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/pkg/errors"
+
+	"github.com/windmilleng/tilt/internal/tiltfile"
+)
+
+const extensionDirName = "tilt_modules"
+
+type Store interface {
+	// ModulePath is used to check if an extension exists before fetching it
+	// Returns ErrNotExist if module doesn't exist
+	ModulePath(ctx context.Context, moduleName string) (string, error)
+	Write(ctx context.Context, contents ModuleContents) (string, error)
+}
+
+// TODO(dmiller): should this include an integrity hash?
+type ModuleContents struct {
+	Name              string
+	TiltfileContents  string
+	GitCommitHash     string
+	ExtensionRegistry string
+	TimeFetched       time.Time
+}
+
+type LocalStore struct {
+	baseDir string
+}
+
+func NewLocalStore(baseDir string) *LocalStore {
+	return &LocalStore{
+		baseDir: filepath.Join(baseDir, extensionDirName),
+	}
+}
+
+func (s *LocalStore) ModulePath(ctx context.Context, moduleName string) (string, error) {
+	tiltfilePath := filepath.Join(s.baseDir, moduleName, tiltfile.FileName)
+
+	_, err := os.Stat(tiltfilePath)
+	if err != nil {
+		return "", err
+	}
+
+	return tiltfilePath, nil
+}
+
+func (s *LocalStore) Write(ctx context.Context, contents ModuleContents) (string, error) {
+	moduleDir := filepath.Join(s.baseDir, contents.Name)
+	if err := os.MkdirAll(moduleDir, os.FileMode(0700)); err != nil {
+		return "", errors.Wrapf(err, "couldn't create module directory %s at path %s", contents.Name, moduleDir)
+	}
+
+	tiltfilePath := filepath.Join(moduleDir, tiltfile.FileName)
+	// TODO(dmiller): store hash, source, time fetched
+	if err := ioutil.WriteFile(tiltfilePath, []byte(contents.TiltfileContents), os.FileMode(0600)); err != nil {
+		return "", errors.Wrapf(err, "couldn't store module %s at path %s", contents.Name, tiltfilePath)
+	}
+
+	return tiltfilePath, nil
+}
+
+var _ Store = (*LocalStore)(nil)
---
+++ internal/tiltfile/extension/store_test.go
@@ -0,0 +1,109 @@
+package extension
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/windmilleng/tilt/internal/testutils/tempdir"
+	"github.com/windmilleng/tilt/internal/tiltfile"
+)
+
+func TestWrite(t *testing.T) {
+	f := newFixture(t)
+	defer f.tearDown()
+
+	path := f.writeModule(ModuleContents{
+		Name:              "test",
+		TiltfileContents:  "print('hi')",
+		GitCommitHash:     "aaaaaa",
+		ExtensionRegistry: "https://github.com/windmill/extensions",
+	})
+
+	f.assertPath(path)
+	f.assertExtension("test", "print('hi')", "aaaaaa", "https://github.com/windmill/extensions")
+}
+
+func TestWriteAndStat(t *testing.T) {
+	f := newFixture(t)
+	defer f.tearDown()
+
+	f.writeModule(ModuleContents{
+		Name:              "test",
+		TiltfileContents:  "print('hi')",
+		GitCommitHash:     "aaaaaa",
+		ExtensionRegistry: "https://github.com/windmill/extensions",
+	})
+
+	f.assertExtension("test", "print('hi')", "aaaaaa", "https://github.com/windmill/extensions")
+
+	path := f.modulePath("test")
+	f.assertPath(path)
+}
+
+func TestStatModuleDoesntExist(t *testing.T) {
+	f := newFixture(t)
+	defer f.tearDown()
+
+	f.assertModulePathDoesntExist("test")
+}
+
+type fixture struct {
+	t       *testing.T
+	ctx     context.Context
+	tempdir *tempdir.TempDirFixture
+	store   *LocalStore
+}
+
+func newFixture(t *testing.T) *fixture {
+	ctx := context.Background()
+	temp := tempdir.NewTempDirFixture(t)
+	ls := NewLocalStore(temp.Path())
+
+	return &fixture{
+		t:       t,
+		ctx:     ctx,
+		tempdir: temp,
+		store:   ls,
+	}
+}
+
+func (f *fixture) writeModule(contents ModuleContents) string {
+	path, err := f.store.Write(f.ctx, contents)
+	require.NoError(f.t, err)
+
+	return path
+}
+
+func (f *fixture) modulePath(moduleName string) string {
+	path, err := f.store.ModulePath(f.ctx, moduleName)
+	require.NoError(f.t, err)
+
+	return path
+}
+
+func (f *fixture) assertModulePathDoesntExist(moduleName string) {
+	path, err := f.store.ModulePath(f.ctx, moduleName)
+	assert.True(f.t, os.IsNotExist(err))
+	assert.Equal(f.t, "", path)
+}
+
+func (f *fixture) assertExtension(moduleName, contents, hash, source string) {
+	tiltfileContents, err := ioutil.ReadFile(f.tempdir.JoinPath(extensionDirName, moduleName, tiltfile.FileName))
+	require.NoError(f.t, err)
+
+	assert.Equal(f.t, contents, string(tiltfileContents))
+	// TODO(dmiller): assert on hash and source
+}
+
+func (f *fixture) assertPath(path string) {
+	assert.Equal(f.t, f.tempdir.JoinPath("tilt_modules", "test", "Tiltfile"), path)
+}
+
+func (f *fixture) tearDown() {
+	f.tempdir.TearDown()
+}
