5b71fbe0793f348095b38fa6010c9a0b73018850
Matt Landis <matt@windmill.engineering>
2020-02-14 13:24:32 -0500

    tiltfile: add bool and string config settings

---
+++ internal/tiltfile/config/bool.go
@@ -0,0 +1,72 @@
+package config
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"strconv"
+
+	"go.starlark.net/starlark"
+)
+
+type boolSetting struct {
+	value bool
+	isSet bool
+}
+
+var _ configValue = &boolSetting{}
+var _ flag.Value = &boolSetting{}
+
+func (s *boolSetting) starlark() starlark.Value {
+	return starlark.Bool(s.value)
+}
+
+func (s *boolSetting) IsSet() bool {
+	return s.isSet
+}
+
+func (s *boolSetting) MarshalJSON() ([]byte, error) {
+	ret := &bytes.Buffer{}
+	err := json.NewEncoder(ret).Encode(s.value)
+	if err != nil {
+		return nil, err
+	}
+	return ret.Bytes(), nil
+}
+
+func (s *boolSetting) setFromInterface(i interface{}) error {
+	if i == nil {
+		return nil
+	}
+	v, ok := i.(bool)
+	if !ok {
+		return fmt.Errorf("expected %T, found %T", s.value, i)
+	}
+
+	s.value = v
+	s.isSet = true
+
+	return nil
+}
+
+func (s *boolSetting) Set(v string) error {
+	if s.isSet {
+		return fmt.Errorf("bool settings can only be specified once. multiple values found (last value: %s)", v)
+	}
+	b, err := strconv.ParseBool(v)
+	if err != nil {
+		return err
+	}
+	s.value = b
+	s.isSet = true
+	return nil
+}
+
+func (s *boolSetting) String() string {
+	return strconv.FormatBool(s.value)
+}
+
+func (s *boolSetting) IsBoolFlag() bool {
+	return true
+}
