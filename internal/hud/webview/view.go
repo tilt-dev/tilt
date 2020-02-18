856e1697a259aacfa862e1c1d812516d391c3958
Maia McCormick <maia@windmill.engineering>
2020-02-18 16:01:14 -0500

    webview: add buildResult.BuildType to webview proto [ch4520] (#2956)

--- internal/hud/webview/view.go
+++ internal/hud/webview/view.go
@@ -2,6 +2,7 @@ package webview
 
 // TODO(dmiller): delete these tests once StateToWebView is deleted
 import (
+	"fmt"
 	"time"
 
 	"github.com/windmilleng/tilt/internal/container"
@@ -37,6 +38,76 @@ func timeToProto(t time.Time) (*timestamp.Timestamp, error) {
 	return ts, nil
 }
 
+func buildTypesToProtoUpdateTypes(bts []model.BuildType) ([]proto_webview.UpdateType, error) {
+	result := make([]proto_webview.UpdateType, len(bts))
+	for i, bt := range bts {
+		protoType, err := buildTypeToProto(bt)
+		if err != nil {
+			return nil, err
+		}
+		result[i] = protoType
+	}
+	return result, nil
+}
+
+func buildTypeToProto(bt model.BuildType) (proto_webview.UpdateType, error) {
+	switch bt {
+	case model.BuildTypeImage:
+		return proto_webview.UpdateType_UPDATE_TYPE_IMAGE, nil
+	case model.BuildTypeLiveUpdate:
+		return proto_webview.UpdateType_UPDATE_TYPE_LIVE_UPDATE, nil
+	case model.BuildTypeDockerCompose:
+		return proto_webview.UpdateType_UPDATE_TYPE_DOCKER_COMPOSE, nil
+	case model.BuildTypeK8s:
+		return proto_webview.UpdateType_UPDATE_TYPE_K8S, nil
+	case model.BuildTypeLocal:
+		return proto_webview.UpdateType_UPDATE_TYPE_LOCAL, nil
+	default:
+		return proto_webview.UpdateType_UPDATE_TYPE_UNSPECIFIED, fmt.Errorf("unknown build type '%v'", bt)
+	}
+}
+
+func targetSpecToProto(spec model.TargetSpec) (proto_webview.TargetSpec, error) {
+	switch typ := spec.(type) {
+	case model.ImageTarget:
+		return proto_webview.TargetSpec{
+			Id:            typ.ID().String(),
+			Type:          proto_webview.TargetType_TARGET_TYPE_IMAGE,
+			HasLiveUpdate: !typ.LiveUpdateInfo().Empty(),
+		}, nil
+	case model.DockerComposeTarget:
+		return proto_webview.TargetSpec{
+			Id:   typ.ID().String(),
+			Type: proto_webview.TargetType_TARGET_TYPE_DOCKER_COMPOSE,
+		}, nil
+	case model.K8sTarget:
+		return proto_webview.TargetSpec{
+			Id:   typ.ID().String(),
+			Type: proto_webview.TargetType_TARGET_TYPE_K8S,
+		}, nil
+	case model.LocalTarget:
+		return proto_webview.TargetSpec{
+			Id:   typ.ID().String(),
+			Type: proto_webview.TargetType_TARGET_TYPE_LOCAL,
+		}, nil
+	default:
+		return proto_webview.TargetSpec{}, fmt.Errorf("unknown TargetSpec type %T for spec: '%v'", spec, spec)
+	}
+}
+
+func TargetSpecsToProto(specs []model.TargetSpec) ([]*proto_webview.TargetSpec, error) {
+	result := make([]*proto_webview.TargetSpec, len(specs))
+	for i, spec := range specs {
+		protoSpec, err := targetSpecToProto(spec)
+		if err != nil {
+			return nil, err
+		}
+		result[i] = &protoSpec
+	}
+
+	return result, nil
+}
+
 func ToProtoBuildRecord(br model.BuildRecord, logStore *logstore.LogStore) (*proto_webview.BuildRecord, error) {
 	e := ""
 	if br.Error != nil {
@@ -57,6 +128,10 @@ func ToProtoBuildRecord(br model.BuildRecord, logStore *logstore.LogStore) (*pro
 		warnings = logStore.Warnings(br.SpanID)
 	}
 
+	updateTypes, err := buildTypesToProtoUpdateTypes(br.BuildTypes)
+	if err != nil {
+		return nil, err
+	}
 	return &proto_webview.BuildRecord{
 		Edits: br.Edits,
 		Error: e,
@@ -64,6 +139,7 @@ func ToProtoBuildRecord(br model.BuildRecord, logStore *logstore.LogStore) (*pro
 		Warnings:       warnings,
 		StartTime:      start,
 		FinishTime:     finish,
+		UpdateTypes:    updateTypes,
 		IsCrashRebuild: br.Reason.IsCrashOnly(),
 		SpanId:         string(br.SpanID),
 	}, nil
