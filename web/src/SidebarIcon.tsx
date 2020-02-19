0c1985c3e2ddeeedce7fb5633753d23730887f62
Maia McCormick <maia@windmill.engineering>
2020-02-19 13:29:06 -0500

    web: last build duration in sidebar [ch4520] (#2955)
    * placeholder styling
    
    * naive second formatting
    
    * slightly better formatting??
    
    * prettier
    
    * web: new sidebar design to fit duration
    
    * web: fix snapshot tests
    
    * web: add snapshot test for sidebar, for now
    
    * web: run prettier
    
    * web: remove unused SVG
    
    * web: lazy fix for test that can’t find styled-component
    
    * web: add TODO for undone thing

--- web/src/SidebarIcon.tsx
+++ web/src/SidebarIcon.tsx
@@ -1,13 +1,21 @@
 import React, { PureComponent } from "react"
 import { ResourceStatus } from "./types"
-import { Color } from "./style-helpers"
-import { ReactComponent as DotSvg } from "./assets/svg/indicator-auto.svg"
-import { ReactComponent as DotPendingSvg } from "./assets/svg/indicator-auto-pending.svg"
-import { ReactComponent as DotBuildingSvg } from "./assets/svg/indicator-auto-building.svg"
-import "./SidebarIcon.scss"
+import {
+  AnimDuration,
+  Color,
+  ColorAlpha,
+  ColorRGBA,
+  Font,
+  FontSize,
+  SizeUnit,
+} from "./style-helpers"
+import styled, { keyframes } from "styled-components"
+import { Width } from "./style-helpers"
+import { Link } from "react-router-dom"
 
 type SidebarIconProps = {
   status: ResourceStatus
+  alertCount: number
 }
 
 // For testing
@@ -17,43 +25,97 @@ export enum IconType {
   StatusBuilding = "building",
 }
 
+let glowWhite = keyframes`
+  0% {
+    background-color: ${ColorRGBA(Color.white, ColorAlpha.translucent)};
+  }
+  50% {
+    background-color: ${ColorRGBA(Color.white, ColorAlpha.almostTransparent)};
+  }
+`
+
+let glowDark = keyframes`
+  0% {
+    background-color: ${ColorRGBA(Color.gray, ColorAlpha.translucent)};
+  }
+  50% {
+    background-color: ${ColorRGBA(Color.gray, ColorAlpha.almostTransparent)};
+  }
+`
+
+let SidebarIconRoot = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: ${Width.sidebarCollapsed * 0.7}px;
+  margin-right: ${Width.sidebarCollapsed * 0.3}px;
+  transition: background-color ${AnimDuration.default} linear,
+    opacity ${AnimDuration.default} linear;
+
+  &.isWarning {
+    background-color: ${Color.yellow};
+  }
+  &.isHealthy {
+    background-color: ${Color.green};
+  }
+  &.isUnhealthy {
+    background-color: ${Color.red};
+  }
+  &.isBuilding {
+    background-color: ${ColorRGBA(Color.white, ColorAlpha.translucent)};
+  }
+  .isSelected &.isBuilding {
+    background-color: ${ColorRGBA(Color.gray, ColorAlpha.translucent)};
+  }
+  &.isPending {
+    background-color: ${ColorRGBA(Color.white, ColorAlpha.translucent)};
+    animation: ${glowWhite} 2s linear infinite;
+  }
+  .isSelected &.isPending {
+    background-color: ${ColorRGBA(Color.gray, ColorAlpha.translucent)};
+    animation: ${glowDark} 2s linear infinite;
+  }
+  &.isNone {
+    background-color: ${ColorRGBA(Color.white, ColorAlpha.translucent)};
+  }
+  .isSelected &.isNone {
+    background-color: ${ColorRGBA(Color.gray, ColorAlpha.translucent)};
+  }
+`
+
+let AlertCount = styled.span`
+  font-family: ${Font.sansSerif};
+  font-size: ${FontSize.smallest};
+  color: ${Color.black};
+`
+
 export default class SidebarIcon extends PureComponent<SidebarIconProps> {
   render() {
-    return <div className="SidebarIcon">{this.svg()}</div>
+    return (
+      <SidebarIconRoot className={`${this.status()}`}>
+        {this.props.alertCount > 0 ? (
+          <AlertCount>{this.props.alertCount}</AlertCount>
+        ) : (
+          <span>&nbsp;</span>
+        )}
+      </SidebarIconRoot>
+    )
   }
 
-  svg() {
+  status() {
     switch (this.props.status) {
       case ResourceStatus.Building:
-        return this.building()
+        return "isBuilding"
       case ResourceStatus.Pending:
-        return this.pending()
+        return "isPending"
       case ResourceStatus.Warning:
-        return this.default(Color.yellow)
+        return "isWarning"
       case ResourceStatus.Healthy:
-        return this.default(Color.green)
+        return "isHealthy"
       case ResourceStatus.Unhealthy:
-        return this.default(Color.red)
+        return "isUnhealthy"
       case ResourceStatus.None:
-        return this.default(Color.gray)
+        return "isNone"
     }
-  }
-
-  default(fill: Color) {
-    return <DotSvg className={IconType.StatusDefault} fill={fill} />
-  }
-
-  pending() {
-    let style = {
-      animation: "glow 1s linear infinite",
-    }
-    return <DotPendingSvg className={IconType.StatusPending} style={style} />
-  }
-
-  building() {
-    let style = {
-      animation: "spin 1s linear infinite",
-    }
-    return <DotBuildingSvg className={IconType.StatusBuilding} style={style} />
   }
 }
