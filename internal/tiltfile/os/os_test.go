73a1572b438b9086ec15daac9f714675eec0f918
Nick Santos <nicholas.j.santos@gmail.com>
2020-02-26 13:54:59 -0500

    tiltfile: add os.getcwd() (#3017)

--- internal/tiltfile/os/os_test.go
+++ internal/tiltfile/os/os_test.go
@@ -1,6 +1,7 @@
 package os
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
@@ -21,6 +22,56 @@ print(os.environ['FAKE_ENV_VARIABLE'])
 	_, err := f.ExecFile("Tiltfile")
 	assert.NoError(t, err)
 	assert.Equal(t, "fakeValue\n", f.PrintOutput())
+}
+
+func TestGetCwd(t *testing.T) {
+	f := NewFixture(t)
+
+	f.File("Tiltfile", `
+print(os.getcwd())
+`)
+
+	_, err := f.ExecFile("Tiltfile")
+	assert.NoError(t, err)
+	assert.Equal(t, fmt.Sprintf("%s\n", f.Path()), f.PrintOutput())
+}
+
+func TestGetCwdLoad(t *testing.T) {
+	f := NewFixture(t)
+
+	f.File("foo/Tiltfile", `
+cwd = os.getcwd()
+`)
+	f.File("Tiltfile", `
+load('./foo/Tiltfile', 'cwd')
+print(cwd)
+`)
+
+	_, err := f.ExecFile("Tiltfile")
+	assert.NoError(t, err)
+	assert.Equal(t, fmt.Sprintf("%s/foo\n", f.Path()), f.PrintOutput())
+}
+
+func TestGetCwdLoadFunction(t *testing.T) {
+	f := NewFixture(t)
+
+	f.File("foo/Tiltfile", `
+def get_cwd_wrapper():
+  return os.getcwd()
+`)
+	f.File("Tiltfile", `
+load('./foo/Tiltfile', 'get_cwd_wrapper')
+print(get_cwd_wrapper())
+`)
+
+	_, err := f.ExecFile("Tiltfile")
+	assert.NoError(t, err)
+
+	// NOTE(nick): A bunch of us agree that this behavior is currently wrong.
+	// The working directory should be the currently executing Tiltfile,
+	// not the Tiltfile where the function was evaluated.
+	// https://app.clubhouse.io/windmill/story/4708/extensions-are-executed-with-a-working-directory-in-the-tilt-modules-dir
+	assert.Equal(t, fmt.Sprintf("%s/foo\n", f.Path()), f.PrintOutput())
 }
 
 func NewFixture(tb testing.TB) *starkit.Fixture {
