ccd61f398254f99f64bc8433b93d60f6607b6d21
Maia <maia@windmill.engineering>
2020-02-28 15:38:49 -0500

    put it all in a script

--- .circleci/integration_tests.sh
+++ .circleci/integration_tests.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Adapted from:
 # https://github.com/kubernetes-sigs/kind/commits/master/site/static/examples/kind-with-registry.sh
@@ -17,14 +17,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -o errexit
+set -oe errexit
+
+# Grab a script from the DIND kubernetes cluster that uses socat to do port-forwarding
+KUBEADM_SHA=30a2033581adf53161fe1cdc76f1550193927db4
+curl https://raw.githubusercontent.com/kubernetes-sigs/kubeadm-dind-cluster/${KUBEADM_SHA}/build/portforward.sh > /tmp/portforward.sh
+apt install -y curl ca-certificates git liblz4-tool rsync socat \
+  && chmod a+x /tmp/portforward.sh
 
 # desired cluster name; default is "kind"
-KIND_CLUSTER_NAME="${KIND_CLUSTER_NAME:-kind}"
+KIND_CLUSTER_NAME="integration"
 
-# create registry container unless it already exists
+# default registry name and port
 reg_name='kind-registry'
 reg_port='5000'
+
+echo "> initializing Kind cluster: ${KIND_CLUSTER_NAME}"
+# create registry container unless it already exists
 running="$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)"
 if [ "${running}" != 'true' ]; then
   docker run \
@@ -36,15 +45,26 @@ fi
 cat <<EOF | kind create cluster --name "${KIND_CLUSTER_NAME}" --config=-
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
-containerdConfigPatches: 
+containerdConfigPatches:
 - |-
   [plugins."io.containerd.grpc.v1.cri".registry.mirrors."registry:${reg_port}"]
     endpoint = ["http://registry:${reg_port}"]
 EOF
 
+echo "> port-forwarding k8s API server"
+APISERVER_PORT=$(kubectl config view -o jsonpath='{.clusters[].cluster.server}' | cut -d: -f 3 -)
+/tmp/portforward.sh -wait $APISERVER_PORT
+kubectl get nodes # make sure it worked
+
+echo "> port-forwarding local registry"
+/tmp/portforward.sh -wait $reg_port
+
+echo "> annotating nodes"
 # add the registry to /etc/hosts on each node
 ip_fmt='{{.NetworkSettings.IPAddress}}'
 cmd="echo $(docker inspect -f "${ip_fmt}" "${reg_name}") registry >> /etc/hosts"
+
+# and annotate each node with registry info (for Tilt to detect)
 for node in $(kind get nodes --name "${KIND_CLUSTER_NAME}"); do
   docker exec "${node}" sh -c "${cmd}"
   kubectl annotate node "${node}" \
@@ -52,3 +72,5 @@ for node in $(kind get nodes --name "${KIND_CLUSTER_NAME}"); do
           tilt.dev/registry-from-cluster=registry:${reg_port}
 done
 
+echo "> running integration tests"
+make integration
