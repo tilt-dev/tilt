66982bff6bc59a9e602ff3b722706660ec34af2e
Victor Wu <victor@tilt.dev>
2020-04-21 09:29:51 -0400

    Update up.go

--- internal/cli/up.go
+++ internal/cli/up.go
@@ -57,7 +57,7 @@ func (c *upCmd) register() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:                   "up [<tilt flags>] [-- <Tiltfile args>]",
 		DisableFlagsInUseLine: true,
-		Short:                 "Start Tilt with the given Tiltfile args",
+		Short:                 "Start Tilt with Tilt args and Tiltfile args",
 		Long: `
 Starts Tilt and runs services defined in the Tiltfile.
 
