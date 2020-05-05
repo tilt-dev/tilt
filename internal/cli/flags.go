8fb1cf062923121e831b0673f20c8d8c45edec6f
Nick Santos <nicholas.j.santos@gmail.com>
2020-05-05 19:15:17 -0400

    cli: allow 'tilt ci' to use the --web-mode flag for serving local vs prod js

--- internal/cli/flags.go
+++ internal/cli/flags.go
@@ -21,4 +21,5 @@ func addWebServerFlags(cmd *cobra.Command) {
 	addWebPortFlag(cmd)
 	cmd.Flags().StringVar(&webHost, "host", DefaultWebHost, "Host for the Tilt HTTP server and default host for any port-forwards. Set to 0.0.0.0 to listen on all interfaces.")
 	cmd.Flags().IntVar(&webDevPort, "webdev-port", DefaultWebDevPort, "Port for the Tilt Dev Webpack server. Only applies when using --web-mode=local")
+	cmd.Flags().Var(&webModeFlag, "web-mode", "Values: local, prod. Controls whether to use prod assets or a local dev server")
 }
