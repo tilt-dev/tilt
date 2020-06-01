40b6af6743c6e7f79b9c9a930bbe68810e1a79af
Dan Miller <dmiller@windmill.engineering>
2020-06-01 16:21:09 -0400

    hud/server: enable websocket compression
    May help with situations where Tilt is being run on a remote server and
    the web UI is running on a local laptop resulting in the websocket
    messages being sent over the Internet like in #3359.

--- internal/hud/server/websocket.go
+++ internal/hud/server/websocket.go
@@ -22,8 +22,9 @@ import (
 )
 
 var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
+	ReadBufferSize:    1024,
+	WriteBufferSize:   1024,
+	EnableCompression: true,
 }
 
 type WebsocketSubscriber struct {
