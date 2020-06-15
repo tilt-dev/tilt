07257692c47bd7b3218cf3cdb682fab3dc02f252
Maia McCormick <maia@windmill.engineering>
2020-06-15 11:48:40 -0400

    integration: (most of) integration tests should use restart_process extension instead of re/start.sh [ch7543] (#3457)

--- integration/live_update_after_crash_rebuild_test.go
+++ integration/live_update_after_crash_rebuild_test.go
@@ -14,10 +14,6 @@ import (
 )
 
 func TestLiveUpdateAfterCrashRebuild(t *testing.T) {
-	// TODO(maia): investigate unexplained errors in CI, reenable
-	// e.g.: https://circleci.com/gh/tilt-dev/tilt/28109
-	t.SkipNow()
-
 	f := newK8sFixture(t, "live_update_after_crash_rebuild")
 	defer f.TearDown()
 
