42cb1dbd6a8fd27766be344b628f3d38e1965afa
Nick Santos <nicholas.j.santos@gmail.com>
2020-07-01 19:41:59 -0400

    web: fix a bug in duration display. Fixes https://github.com/tilt-dev/tilt/issues/3533

---
+++ web/src/format.test.ts
@@ -0,0 +1,26 @@
+import { formatBuildDuration } from "./format"
+import moment from "moment"
+
+it("format durations correctly", () => {
+  function assertFormatted(expected: string, ms: number) {
+    expect(formatBuildDuration(moment.duration(ms))).toEqual(expected)
+  }
+
+  let second = 1000
+  assertFormatted("1.0s", second)
+  assertFormatted("20s", 20 * second)
+  assertFormatted("40s", 40 * second)
+  assertFormatted("50s", 50 * second)
+  assertFormatted("1m", 70 * second)
+  assertFormatted("2m", 150 * second)
+  assertFormatted("1h", 4000 * second)
+
+  // there used to be a bug where the UI would flip from
+  // "10.0s" to "10s", which looked weird.
+  assertFormatted("9.9s", 10 * second - 100)
+  assertFormatted("9.9s", 10 * second - 51)
+  assertFormatted("10s", 10 * second - 50)
+  assertFormatted("10s", 10 * second - 1)
+  assertFormatted("10s", 10 * second)
+  assertFormatted("10s", 10 * second + 1)
+})
