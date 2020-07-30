268577ff93c53b875c48b8e71dea2cff0c3d5e61
Maia McCormick <maia@windmill.engineering>
2020-07-30 16:30:58 -0400

    deprecate update_mode=syncet flag (#3642)

--- internal/engine/buildcontrol/update_mode.go
+++ internal/engine/buildcontrol/update_mode.go
@@ -58,11 +58,8 @@ func ProvideUpdateMode(flag UpdateModeFlag, env k8s.Env, runtime container.Runti
 	}
 
 	if mode == UpdateModeSynclet {
-		if runtime != container.RuntimeDocker {
-			return "", fmt.Errorf("update mode %q is only valid with Docker container runtime (and will NOT work with"+
-				"containerd, cri-o, etc.)", flag)
-
-		}
+		return "", fmt.Errorf("update mode %q has been removed; please use update mode %q instead. "+
+			"If this breaks your workflow, contact us: https://tilt.dev/contact", flag, UpdateModeKubectlExec)
 	}
 
 	return mode, nil
