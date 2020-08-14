f9dd2dac3c0ba4386c52d6bf3f41365fc7fd0d1d
Nick Santos <nicholas.j.santos@gmail.com>
2020-08-14 14:38:53 -0400

    release: Use a Docker container to do cross-compilation of the Tilt binaries

---
+++ scripts/release.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Do a complete release.
+# Upload assets, run goreleaser, and notify Tilt Cloud of the new release binaries.
+
+set -ex
+
+if [[ "$GITHUB_API_TOKEN" == "" ]]; then
+    echo "Missing GITHUB_API_TOKEN"
+    exit 1
+fi
+
+DIR=$(dirname "$0")
+cd "$DIR/.."
+
+./scripts/upload-assets.py latest
+./scripts/goreleaser.sh
+./scripts/record-release.sh "$$(git describe --abbrev=0 --tags)"
