780867f94e20b35c48eb7ded5d83dfec0891e9c9
Nick Santos <nicholas.j.santos@gmail.com>
2020-09-01 11:36:11 -0400

    k8s: apply resources individually. Fixes https://github.com/tilt-dev/tilt/issues/3719 (#3739)

---
+++ integration/configmap_test.go
@@ -0,0 +1,45 @@
+//+build integration
+
+package integration
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestConfigMap(t *testing.T) {
+	f := newK8sFixture(t, "configmap")
+	defer f.TearDown()
+
+	f.TiltWatch()
+
+	ctx, cancel := context.WithTimeout(f.ctx, time.Minute)
+	defer cancel()
+
+	f.WaitUntil(ctx, "Waiting for small configmap to show up", func() (string, error) {
+		out, _ := f.runCommand("kubectl", "get", "configmap", "small-configmap", namespaceFlag, "-o=go-template", "--template='{{.data}}'")
+		return out.String(), nil
+	}, "hello world")
+
+	firstCreationTime, err := f.runCommand("kubectl", "get", "configmap", "small-configmap", namespaceFlag, "-o=go-template", "--template='{{.metadata.creationTimestamp}}'")
+	require.NoError(t, err)
+	require.NotEqual(t, "", firstCreationTime.String())
+
+	f.ReplaceContents("small.txt", "hello world", "goodbye world")
+
+	f.WaitUntil(ctx, "Waiting for small configmap to get replaced", func() (string, error) {
+		out, _ := f.runCommand("kubectl", "get", "configmap", "small-configmap", namespaceFlag, "-o=go-template", "--template='{{.data}}'")
+		return out.String(), nil
+	}, "goodbye world")
+
+	secondCreationTime, err := f.runCommand("kubectl", "get", "configmap", "small-configmap", namespaceFlag, "-o=go-template", "--template='{{.metadata.creationTimestamp}}'")
+	require.NoError(t, err)
+	require.NotEqual(t, "", secondCreationTime.String())
+
+	// Make sure we applied the configmap instead of recreating it
+	assert.Equal(t, firstCreationTime, secondCreationTime)
+}
