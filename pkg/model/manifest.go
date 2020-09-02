beee31b6287cf948674226d95c64a4ebafb5bf00
Nick Santos <nicholas.j.santos@gmail.com>
2020-09-02 16:44:17 -0400

    model: remove some dead code

--- pkg/model/manifest.go
+++ pkg/model/manifest.go
@@ -227,11 +227,6 @@ func (m Manifest) Validate() error {
 	return nil
 }
 
-func (m1 Manifest) Equal(m2 Manifest) bool {
-	primitivesEq, dockerEq, k8sEq, dcEq, localEq, depsEq := m1.fieldGroupsEqual(m2)
-	return primitivesEq && dockerEq && k8sEq && dcEq && localEq && depsEq
-}
-
 // ChangesInvalidateBuild checks whether the changes from old => new manifest
 // invalidate our build of the old one; i.e. if we're replacing `old` with `new`,
 // should we perform a full rebuild?
@@ -267,10 +262,6 @@ func (m1 Manifest) fieldGroupsEqual(m2 Manifest) (primitivesEq, dockerEq, k8sEq,
 
 func (m Manifest) ManifestName() ManifestName {
 	return m.Name
-}
-
-func (m Manifest) Empty() bool {
-	return m.Equal(Manifest{})
 }
 
 func LocalRefSelectorsForManifests(manifests []Manifest) []container.RefSelector {
