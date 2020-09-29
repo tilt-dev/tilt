eba20c6cf2980be3f381f43ff96eaab11db9a6a2
Maia <maia@windmill.engineering>
2020-09-29 16:20:16 -0400

    implement --force arg via deleting the specified bucket first

--- scripts/upload-assets.py
+++ scripts/upload-assets.py
@@ -14,35 +14,38 @@ import subprocess
 import sys
 
 parser = argparse.ArgumentParser(description='Upload JS assets')
-parser.add_argument('--no_check', action='store_true',
-                    help='Don\'t check if files already exist before attempting to upload them')
+parser.add_argument('--force', action='store_true',
+                    help='Re-upload files even if they already exist')
 parser.add_argument('version', help='A version string like "v1.2.3".')
 args = parser.parse_args()
-
 version = args.version
 if version == "latest":
   version = str(subprocess.check_output([
     "git", "describe", "--tags", "--abbrev=0"
   ])).strip()
 
+if args.force:
+  # blow away existing bucket (if any) before uploading assets
+  # (If bucket DNE, this will error but that's expected; suppress STDERR)
+  subprocess.call(["gsutil", "-m", "rm", "-r", "gs://tilt-static-assets/%s" % version],
+                  stderr=open(os.devnull, 'wb'))
+
 dir_url = ("https://storage.googleapis.com/tilt-static-assets/%s/" % version)
 url = dir_url + "index.html"
 print("Uploading to %s" % dir_url)
-
-if not args.no_check:
-    status = subprocess.call([
-      "gsutil", "stat", "gs://tilt-static-assets/%s/index.html" % version
-    ])
-    if status == 0:
-      print("Error: Files already exist: {}".format(url))
-      print("You can manually delete the file(s) at:")
-      print("\thttps://console.cloud.google.com/storage/browser/tilt-static-assets"+
-            "?forceOnBucketsSortingFiltering=false&project=windmill-prod&prefix={}".format(version))
-      sys.exit(1)
+status = subprocess.call([
+  "gsutil", "stat", "gs://tilt-static-assets/%s/index.html" % version
+])
+if status == 0:
+  print("Error: bucket already exists at: %s" % url)
+  print("Rerun this script with --force or manually delete the bucket at:")
+  print("\thttps://console.cloud.google.com/storage/browser/tilt-static-assets"+
+        "?forceOnBucketsSortingFiltering=false&project=windmill-prod&prefix=%s" % version)
+  sys.exit(1)
 
 os.chdir("web")
 subprocess.check_call(["yarn", "install"])
 e = os.environ.copy()
 e["CI"] = "false"
 subprocess.check_call(["yarn", "run", "build"], env=e)
-subprocess.check_call(["gsutil", "cp", "-r", "build", "gs://tilt-static-assets/%s" % version])
+subprocess.check_call(["gsutil", "-m", "cp", "-r", "build", "gs://tilt-static-assets/%s" % version])
