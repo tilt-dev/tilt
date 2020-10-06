866fc3e48827db7a9596d2787400d4f646e6610e
Maia McCormick <maia@windmill.engineering>
2020-10-06 17:05:22 -0400

    no-check option for asset uploading, default for release (#3304)

--- scripts/upload-assets.py
+++ scripts/upload-assets.py
@@ -10,13 +10,13 @@
 
 import argparse
 import os
-import re
 import subprocess
 import sys
 
+
 parser = argparse.ArgumentParser(description='Upload JS assets')
-parser.add_argument('--force', dest='feature', action='store_true',
-                    help='Re-upload files even if they already exist')
+parser.add_argument('--clean', action='store_true',
+                    help='Instead of uploading assets, delete assets of the given version (if they exist)')
 parser.add_argument('version', help='A version string like "v1.2.3".')
 args = parser.parse_args()
 version = args.version
@@ -24,15 +24,28 @@ if version == "latest":
   version = str(subprocess.check_output([
     "git", "describe", "--tags", "--abbrev=0"
   ])).strip()
-
 dir_url = ("https://storage.googleapis.com/tilt-static-assets/%s/" % version)
+
+if args.clean:
+    # Just remove the bucket and exit w/o uploading anything
+    print("Deleting bucket at %s (if exists)" % dir_url)
+    subprocess.call(["gsutil", "-m", "rm", "-r", "gs://tilt-static-assets/%s" % version],
+                    # (If bucket DNE, this will error but that's expected; suppress STDERR)
+                    stderr=open(os.devnull, 'wb'))
+    sys.exit(0)
+
 url = dir_url + "index.html"
 print("Uploading to %s" % dir_url)
 status = subprocess.call([
   "gsutil", "stat", "gs://tilt-static-assets/%s/index.html" % version
 ])
 if status == 0:
-  print("Error: Files already exist: %s" % url)
+  print("Error: bucket already exists at: %s" % url)
+  print("Remove the bucket by running this script with --clean,")
+  print("or manually delete the bucket at:")
+  print("\thttps://console.cloud.google.com/storage/browser/tilt-static-assets"+
+        "?forceOnBucketsSortingFiltering=false&project=windmill-prod&prefix=%s" % version)
+  print("Then try uploading assets again.")
   sys.exit(1)
 
 os.chdir("web")
@@ -40,4 +53,4 @@ subprocess.check_call(["yarn", "install"])
 e = os.environ.copy()
 e["CI"] = "false"
 subprocess.check_call(["yarn", "run", "build"], env=e)
-subprocess.check_call(["gsutil", "cp", "-r", "build", "gs://tilt-static-assets/%s" % version])
+subprocess.check_call(["gsutil", "-m", "cp", "-r", "build", "gs://tilt-static-assets/%s" % version])
