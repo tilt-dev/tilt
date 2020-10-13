89e5e54a93dcd10988aac6e395a7596f7e354b84
Nick Santos <nicholas.j.santos@gmail.com>
2020-10-13 09:49:10 -0400

    scripts: use an empty directory for the goreleaser go-build cache. this should make releases much faster (#3855)

--- scripts/build-tilt-releaser.sh
+++ scripts/build-tilt-releaser.sh
@@ -8,19 +8,5 @@ set -ex
 DIR=$(dirname "$0")
 cd "$DIR/.."
 
-docker build -t tilt-releaser-base -f scripts/release.Dockerfile scripts
-docker container rm tilt-releaser-go-cache || true
-
-# We deliberately don't give this container any API keys so it can't
-# accidentally publish.
-docker run --name tilt-releaser-go-cache --privileged \
-       -w /src/tilt \
-       -v "$PWD:/src/tilt:delegated" \
-       -v /var/run/docker.sock:/var/run/docker.sock \
-       tilt-releaser-base \
-       --rm-dist --skip-validate --snapshot --skip-publish
-
-# Commit all the Go cache artifacts we generated
-docker commit tilt-releaser-go-cache gcr.io/windmill-public-containers/tilt-releaser
+docker build -t gcr.io/windmill-public-containers/tilt-releaser -f scripts/release.Dockerfile scripts
 docker push gcr.io/windmill-public-containers/tilt-releaser
-docker container rm tilt-releaser-go-cache
