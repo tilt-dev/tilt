7a271178c8d28a5d76b492aa3d95e7875bc6cfbd
Maia McCormick <maia@windmill.engineering>
2020-10-29 15:54:55 -0400

    web: show triggr button for tiltfile [ch9864] (#3877)

--- web/src/SidebarTriggerButton.test.tsx
+++ web/src/SidebarTriggerButton.test.tsx
@@ -340,7 +340,7 @@ describe("SidebarTriggerButton", () => {
     expectWithTooltip(button, TriggerButtonTooltip.ClickToForce)
   })
 
-  it("disables trigger button for Tiltfile", () => {
+  it("shows trigger button for Tiltfile", () => {
     let res = oneResource()
     res.name = "(Tiltfile)"
     res.isTiltfile = true
@@ -364,9 +364,8 @@ describe("SidebarTriggerButton", () => {
     let button = root.find("button.SidebarTriggerButton")
     expect(button).toHaveLength(1)
 
-    expectClickable(button, false)
+    expectClickable(button, true)
     expectClickMe(button, false)
     expectIsQueued(button, false)
-    expectWithTooltip(button, TriggerButtonTooltip.CannotTriggerTiltfile)
   })
 })
