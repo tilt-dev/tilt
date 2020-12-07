94c6f5dcaa5de135c0220446d2674c687d6a6019
Nick Santos <nicholas.j.santos@gmail.com>
2020-12-07 11:33:19 -0500

    web: refactor the sidebar item view storybook a bit (#3967)

--- web/src/SidebarItemView.stories.tsx
+++ web/src/SidebarItemView.stories.tsx
@@ -5,25 +5,54 @@ import PathBuilder from "./PathBuilder"
 import SidebarItem from "./SidebarItem"
 import { SidebarItemView } from "./SidebarResources"
 import { oneResourceNoAlerts } from "./testdata"
-import { ResourceStatus, ResourceView } from "./types"
+import { ResourceStatus, ResourceView, TriggerMode } from "./types"
 
 type Resource = Proto.webviewResource
 let pathBuilder = PathBuilder.forTesting("localhost", "/")
 
-function oneItemWithStatus(status: ResourceStatus) {
-  let item = new SidebarItem(oneResourceNoAlerts())
-  item.status = status
-  if (status === ResourceStatus.Building) {
-    item.currentBuildStartTime = new Date(Date.now() - 1).toISOString()
-  }
-  if (
-    status === ResourceStatus.Unhealthy ||
-    status === ResourceStatus.Warning
-  ) {
-    item.alertCount = 1
-  }
+function ItemWrapper(props: { children: React.ReactNode }) {
   return (
     <MemoryRouter initialEntries={["/"]}>
+      <div style={{ width: "336px" }}>{props.children}</div>
+    </MemoryRouter>
+  )
+}
+
+type optionFn = (item: SidebarItem) => void
+
+function withName(n: string): optionFn {
+  return (item: SidebarItem) => {
+    item.name = n
+  }
+}
+
+function withStatus(status: ResourceStatus): optionFn {
+  return (item: SidebarItem) => {
+    item.status = status
+    if (status === ResourceStatus.Building) {
+      item.currentBuildStartTime = new Date(Date.now() - 1).toISOString()
+    }
+    if (
+      status === ResourceStatus.Unhealthy ||
+      status === ResourceStatus.Warning
+    ) {
+      item.alertCount = 1
+    }
+  }
+}
+
+function withManualTrigger(): optionFn {
+  return (item: SidebarItem) => {
+    item.triggerMode = TriggerMode.TriggerModeManualIncludingInitial
+    item.hasPendingChanges = true
+  }
+}
+
+function itemView(...options: optionFn[]) {
+  let item = new SidebarItem(oneResourceNoAlerts())
+  options.forEach((option) => option(item))
+  return (
+    <ItemWrapper>
       <SidebarItemView
         item={item}
         selected={false}
@@ -31,20 +60,22 @@ function oneItemWithStatus(status: ResourceStatus) {
         resourceView={ResourceView.Log}
         pathBuilder={pathBuilder}
       />
-    </MemoryRouter>
+    </ItemWrapper>
   )
 }
 
 storiesOf("SidebarItemView", module)
-  .add(
-    "one-item-building",
-    oneItemWithStatus.bind(null, ResourceStatus.Building)
+  .add("one-item-building", () => itemView(withStatus(ResourceStatus.Building)))
+  .add("one-item-pending", () => itemView(withStatus(ResourceStatus.Pending)))
+  .add("one-item-healthy", () => itemView(withStatus(ResourceStatus.Healthy)))
+  .add("one-item-unhealthy", () =>
+    itemView(withStatus(ResourceStatus.Unhealthy))
   )
-  .add("one-item-pending", oneItemWithStatus.bind(null, ResourceStatus.Pending))
-  .add("one-item-healthy", oneItemWithStatus.bind(null, ResourceStatus.Healthy))
-  .add(
-    "one-item-unhealthy",
-    oneItemWithStatus.bind(null, ResourceStatus.Unhealthy)
+  .add("one-item-warning", () => itemView(withStatus(ResourceStatus.Warning)))
+  .add("one-item-none", () => itemView(withStatus(ResourceStatus.None)))
+  .add("one-item-trigger", () =>
+    itemView(withStatus(ResourceStatus.Pending), withManualTrigger())
   )
-  .add("one-item-warning", oneItemWithStatus.bind(null, ResourceStatus.Warning))
-  .add("one-item-none", oneItemWithStatus.bind(null, ResourceStatus.None))
+  .add("one-item-long-name", () =>
+    itemView(withName("longnamelongnameverylongname"))
+  )
