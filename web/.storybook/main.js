154d1eb97d81e064fd7808f697d7de4f014ad570
Nick Santos <nicholas.j.santos@gmail.com>
2020-12-23 19:38:22 -0500

    test: add storybook controls (#4034)

--- web/.storybook/main.js
+++ web/.storybook/main.js
@@ -4,6 +4,7 @@ module.exports = {
   stories: ['../src/**/*.stories.tsx'],
   addons: [
     '@storybook/addon-actions',
+    '@storybook/addon-controls',
     '@storybook/addon-links',
     '@storybook/preset-create-react-app'
   ],
