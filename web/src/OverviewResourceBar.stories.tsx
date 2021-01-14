27051cf1dff935ac3e218e13b9d701e50694d805
Nick Santos <nicholas.j.santos@gmail.com>
2021-01-14 11:44:20 -0500

    web: update the overview resource bar to display errors and warnings (#4071)

--- web/src/OverviewResourceBar.stories.tsx
+++ web/src/OverviewResourceBar.stories.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { MemoryRouter } from "react-router"
 import OverviewResourceBar from "./OverviewResourceBar"
 import { nResourceView, tenResourceView, twoResourceView } from "./testdata"
+import { UpdateStatus } from "./types"
 
 type Resource = Proto.webviewResource
 
@@ -25,6 +26,14 @@ export const TwoResources = () => (
 export const TenResources = () => (
   <OverviewResourceBar view={tenResourceView()} />
 )
+
+export const TenResourcesErrorsAndWarnings = () => {
+  let view = tenResourceView() as any
+  view.resources[0].updateStatus = UpdateStatus.Error
+  view.resources[1].buildHistory[0].warnings = ["warning time"]
+  view.resources[5].updateStatus = UpdateStatus.Error
+  return <OverviewResourceBar view={view} />
+}
 
 export const OneHundredResources = () => (
   <OverviewResourceBar view={nResourceView(100)} />
