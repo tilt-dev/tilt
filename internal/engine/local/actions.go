tilt-dev/tilt

commit 8a13e3876f4bcb7d02ff3d03463a83df0913f760

engine: refactor process state representation (#4082)
In preparation for `local_resource` readiness checks, represent
the process state via a specific type (modeled after the K8s
`ContainerState`).

This disambiguates it from the higher-level `RuntimeStatus` which
will be the synthesis of the process state + readiness probe.

internal/engine/local/actions.go
@@ -6,7 +6,7 @@ import (
 
 type LocalServeStatusAction struct {
 	ManifestName model.ManifestName
-	Status       model.RuntimeStatus
+	State        model.ProcessState
 	PID          int // 0 if there's no process running
 	SpanID       model.LogSpanID
 }

internal/engine/local/controller.go
@@ -163,12 +163,12 @@ func processStatuses(
 		if !stillHasSameProcNum() {
 			continue
 		}
-		runtimeStatus := sm.status.ToRuntime()
-		if runtimeStatus != "" {
+		procState := sm.status.ToProcessState()
+		if procState != "" {
 			// TODO(matt) when we get an error, the dot is red in the web ui, but green in the TUI
 			st.Dispatch(LocalServeStatusAction{
 				ManifestName: manifestName,
-				Status:       runtimeStatus,
+				State:        procState,
 				PID:          sm.pid,
 				SpanID:       sm.spanID,
 			})
@@ -247,12 +247,14 @@ const (
 	Error   status = iota
 )
 
-func (s status) ToRuntime() model.RuntimeStatus {
+func (s status) ToProcessState() model.ProcessState {
 	switch s {
 	case Running:
-		return model.RuntimeStatusOK
+		return model.ProcessStateRunning
+	case Done:
+		return model.ProcessStateTerminated
 	case Error:
-		return model.RuntimeStatusError
+		return model.ProcessStateTerminated
 	default:
 		return ""
 	}

internal/engine/upper.go
@@ -461,7 +461,6 @@ func handleBuildCompleted(ctx context.Context, engineState *store.EngineState, c
 
 	if mt.Manifest.IsLocal() {
 		lrs := ms.LocalRuntimeState()
-		lrs.Status = model.RuntimeStatusNotApplicable
 		if err == nil {
 			lrs.HasSucceededAtLeastOnce = true
 		}
@@ -771,7 +770,7 @@ func handleLocalServeStatusAction(ctx context.Context, state *store.EngineState,
 	}
 
 	lrs := ms.LocalRuntimeState()
-	lrs.Status = action.Status
+	lrs.State = action.State
 	lrs.PID = action.PID
 	lrs.SpanID = action.SpanID
 	ms.RuntimeState = lrs

internal/store/runtime_state.go
@@ -35,7 +35,7 @@ type RuntimeState interface {
 }
 
 type LocalRuntimeState struct {
-	Status                  model.RuntimeStatus
+	State                   model.ProcessState
 	HasSucceededAtLeastOnce bool
 	PID                     int
 	SpanID                  model.LogSpanID
@@ -43,8 +43,17 @@ type LocalRuntimeState struct {
 
 func (LocalRuntimeState) RuntimeState() {}
 
+var _ RuntimeState = LocalRuntimeState{}
+
 func (l LocalRuntimeState) RuntimeStatus() model.RuntimeStatus {
-	return l.Status
+	switch l.State {
+	case model.ProcessStateRunning:
+		return model.RuntimeStatusOK
+	case model.ProcessStateTerminated:
+		return model.RuntimeStatusError
+	default:
+		return model.RuntimeStatusUnknown
+	}
 }
 
 func (l LocalRuntimeState) RuntimeStatusError() error {
@@ -58,8 +67,6 @@ func (l LocalRuntimeState) RuntimeStatusError() error {
 func (l LocalRuntimeState) HasEverBeenReadyOrSucceeded() bool {
 	return l.HasSucceededAtLeastOnce
 }
-
-var _ RuntimeState = LocalRuntimeState{}
 
 type K8sRuntimeState struct {
 	// The ancestor that we match pods against to associate them with this manifest.

pkg/model/process_state.go
@@ -1,0 +1,12 @@
+package model
+
+// ProcessState represents the current execution state of a local process.
+type ProcessState string
+
+const (
+	// ProcessStateRunning indicates that a process is alive and executing.
+	ProcessStateRunning ProcessState = "running"
+	// ProcessStateTerminated indicates that a process is no longer executing
+	// either because it exited or was terminated.
+	ProcessStateTerminated ProcessState = "terminated"
+)

internal/engine/local/controller_test.go
@@ -30,18 +30,18 @@ func TestUpdate(t *testing.T) {
 	t1 := time.Unix(1, 0)
 	f.resource("foo", "true", ".", t1)
 	f.step()
-	f.assertStatus("foo", model.RuntimeStatusOK, 1)
+	f.assertStatus("foo", model.ProcessStateRunning, 1)
 
 	t2 := time.Unix(2, 0)
 	f.resource("foo", "false", ".", t2)
 	f.step()
-	f.assertStatus("foo", model.RuntimeStatusOK, 2)
+	f.assertStatus("foo", model.ProcessStateRunning, 2)
 	f.assertNoAction("error for cancel", func(action store.Action) bool {
 		a, ok := action.(LocalServeStatusAction)
 		if !ok {
 			return false
 		}
-		return a.ManifestName == "foo" && a.Status == model.RuntimeStatusError
+		return a.ManifestName == "foo" && a.State == model.ProcessStateTerminated
 	})
 	f.fe.RequireNoKnownProcess(t, "true")
 	f.assertLogMessage("foo", "Starting cmd false")
@@ -55,7 +55,7 @@ func TestServe(t *testing.T) {
 	t1 := time.Unix(1, 0)
 	f.resource("foo", "sleep 60", "testdir", t1)
 	f.step()
-	f.assertStatus("foo", model.RuntimeStatusOK, 1)
+	f.assertStatus("foo", model.ProcessStateRunning, 1)
 
 	require.Equal(t, "testdir", f.fe.processes["sleep 60"].workdir)
 
@@ -69,13 +69,13 @@ func TestFailure(t *testing.T) {
 	t1 := time.Unix(1, 0)
 	f.resource("foo", "true", ".", t1)
 	f.step()
-	f.assertStatus("foo", model.RuntimeStatusOK, 1)
+	f.assertStatus("foo", model.ProcessStateRunning, 1)
 	f.assertLogMessage("foo", "Starting cmd true")
 
 	err := f.fe.stop("true", 5)
 	require.NoError(t, err)
 
-	f.assertStatus("foo", model.RuntimeStatusError, 1)
+	f.assertStatus("foo", model.ProcessStateTerminated, 1)
 	f.assertLogMessage("foo", "cmd true exited with code 5")
 }
 
@@ -197,13 +197,13 @@ func (f *fixture) assertNoAction(msg string, pred func(action store.Action) bool
 	}
 }
 
-func (f *fixture) assertStatus(name string, status model.RuntimeStatus, sequenceNum int) {
+func (f *fixture) assertStatus(name string, status model.ProcessState, sequenceNum int) {
 	msg := fmt.Sprintf("didn't find name %s, status %v, sequence %d", name, status, sequenceNum)
 	pred := func(action store.Action) bool {
 		stAction, ok := action.(LocalServeStatusAction)
 		if !ok ||
 			stAction.ManifestName != model.ManifestName(name) ||
-			stAction.Status != status {
+			stAction.State != status {
 			return false
 		}
 		return true

internal/hud/webview/convert_test.go
@@ -278,14 +278,14 @@ func TestLocalResource(t *testing.T) {
 
 	state := newState([]model.Manifest{m})
 	state.ManifestTargets[m.Name].State.RuntimeState = store.LocalRuntimeState{
-		Status: model.RuntimeStatusNotApplicable,
+		State: model.ProcessStateRunning,
 	}
 	v := stateToProtoView(t, *state)
 
 	assert.Equal(t, 2, len(v.Resources))
 	r := v.Resources[1]
 	assert.Equal(t, "test", r.Name)
-	assert.Equal(t, model.RuntimeStatusNotApplicable, model.RuntimeStatus(r.RuntimeStatus))
+	assert.Equal(t, model.RuntimeStatusOK, model.RuntimeStatus(r.RuntimeStatus))
 	require.Len(t, r.Specs, 1)
 	spec := r.Specs[0]
 	require.Equal(t, proto_webview.TargetType_TARGET_TYPE_LOCAL, spec.Type)

commit 8ec8d73001ac100557e7029cb9bf9234f53d57f2

engine: fix local runtime state for jobs
This was regressed by #4082 - this commit reverts the changes made
there and refactors readiness probe logic to work with it.

Closes #4133.

internal/engine/local/actions.go
@@ -6,7 +6,7 @@ import (
 
 type LocalServeStatusAction struct {
 	ManifestName model.ManifestName
-	State        model.ProcessState
+	Status       model.RuntimeStatus
 	PID          int // 0 if there's no process running
 	SpanID       model.LogSpanID
 }

internal/engine/local/controller.go
@@ -161,11 +161,6 @@ func (c *Controller) start(ctx context.Context, spec ServeSpec, st store.RStore)
 
 	statusCh := make(chan statusAndMetadata)
 
-	go processStatuses(statusCh, st, spec.ManifestName, proc.stillHasSameProcNum())
-
-	spanID := SpanIDForServeLog(proc.procNum)
-	proc.doneCh = c.execer.Start(ctx, spec.ServeCmd, logger.Get(ctx).Writer(logger.InfoLvl), statusCh, spanID)
-
 	if spec.ReadinessProbe != nil {
 		statusChangeFunc := processReadinessProbeUpdate(ctx, st, spec.ManifestName, proc.stillHasSameProcNum())
 		probeWorker, err := probeWorkerFromSpec(
@@ -179,6 +174,11 @@ func (c *Controller) start(ctx context.Context, spec ServeSpec, st store.RStore)
 			go proc.probeWorker.Run(ctx)
 		}
 	}
+
+	go processStatuses(statusCh, st, spec.ManifestName, proc)
+
+	spanID := SpanIDForServeLog(proc.procNum)
+	proc.doneCh = c.execer.Start(ctx, spec.ServeCmd, logger.Get(ctx).Writer(logger.InfoLvl), statusCh, spanID)
 }
 
 func processReadinessProbeUpdate(
@@ -215,17 +215,31 @@ func processStatuses(
 	statusCh chan statusAndMetadata,
 	st store.RStore,
 	manifestName model.ManifestName,
-	stillHasSameProcNum func() bool) {
+	proc *currentProcess) {
+
+	stillHasSameProcNum := proc.stillHasSameProcNum()
+
 	for sm := range statusCh {
-		if !stillHasSameProcNum() {
+		if !stillHasSameProcNum() || sm.status == Unknown {
 			continue
 		}
-		procState := sm.status.ToProcessState()
-		if procState != "" {
+
+		var runtimeStatus model.RuntimeStatus
+		if sm.status == Done || sm.status == Error {
+			runtimeStatus = model.RuntimeStatusError
+		} else if sm.status == Running {
+			if proc.probeWorker != nil {
+				runtimeStatus = model.RuntimeStatusPending
+			} else {
+				runtimeStatus = model.RuntimeStatusOK
+			}
+		}
+
+		if runtimeStatus != "" {
 			// TODO(matt) when we get an error, the dot is red in the web ui, but green in the TUI
 			st.Dispatch(LocalServeStatusAction{
 				ManifestName: manifestName,
-				State:        procState,
+				Status:       runtimeStatus,
 				PID:          sm.pid,
 				SpanID:       sm.spanID,
 			})
@@ -305,16 +319,3 @@ const (
 	Done    status = iota
 	Error   status = iota
 )
-
-func (s status) ToProcessState() model.ProcessState {
-	switch s {
-	case Running:
-		return model.ProcessStateRunning
-	case Done:
-		return model.ProcessStateTerminated
-	case Error:
-		return model.ProcessStateTerminated
-	default:
-		return ""
-	}
-}

internal/engine/upper.go
@@ -463,9 +463,17 @@ func handleBuildCompleted(ctx context.Context, engineState *store.EngineState, c
 
 	if mt.Manifest.IsLocal() {
 		lrs := ms.LocalRuntimeState()
-		if err == nil && mt.Manifest.LocalTarget().ReadinessProbe == nil {
-			// only update the succeeded time if there's no readiness probe
-			lrs.LastReadyOrSucceededTime = time.Now()
+		if err == nil {
+			lt := mt.Manifest.LocalTarget()
+			if lt.ReadinessProbe == nil {
+				// only update the succeeded time if there's no readiness probe
+				lrs.LastReadyOrSucceededTime = time.Now()
+			}
+			if lt.ServeCmd.Empty() {
+				// local resources without a serve command are jobs that run and
+				// terminate; so there's no real runtime status
+				lrs.Status = model.RuntimeStatusNotApplicable
+			}
 		}
 		ms.RuntimeState = lrs
 	}
@@ -767,13 +775,19 @@ func handleSetTiltfileArgsAction(state *store.EngineState, action server.SetTilt
 }
 
 func handleLocalServeStatusAction(ctx context.Context, state *store.EngineState, action local.LocalServeStatusAction) {
-	ms, ok := state.ManifestState(action.ManifestName)
+	mt, ok := state.ManifestTargets[action.ManifestName]
 	if !ok {
 		logger.Get(ctx).Infof("got runtime status information for unknown local resource %s", action.ManifestName)
 	}
+	ms := mt.State
 
 	lrs := ms.LocalRuntimeState()
-	lrs.State = action.State
+
+	if action.Status == model.RuntimeStatusError {
+		lrs.Ready = false
+	}
+
+	lrs.Status = action.Status
 	lrs.PID = action.PID
 	lrs.SpanID = action.SpanID
 	ms.RuntimeState = lrs
@@ -786,7 +800,16 @@ func handleLocalServeReadinessProbeAction(ctx context.Context, state *store.Engi
 	}
 
 	lrs := ms.LocalRuntimeState()
-	lrs.SetReadinessProbeState(action.Ready)
+	lrs.Ready = action.Ready
+	lrs.LastReadyOrSucceededTime = time.Now()
+
+	if lrs.Ready && (lrs.Status == "" || lrs.Status == model.RuntimeStatusPending) {
+		// only transition to OK if currently pending AND ready
+		lrs.Status = model.RuntimeStatusOK
+	} else if !lrs.Ready && (lrs.Status == "" || lrs.Status == model.RuntimeStatusOK) {
+		// only transition to pending if currently OK and NOT ready
+		lrs.Status = model.RuntimeStatusPending
+	}
 	ms.RuntimeState = lrs
 }
 

internal/store/engine_state.go
@@ -491,7 +491,7 @@ func newManifestState(m model.Manifest) *ManifestState {
 	if m.IsK8s() {
 		ms.RuntimeState = NewK8sRuntimeState(m)
 	} else if m.IsLocal() {
-		ms.RuntimeState = NewLocalRuntimeState(m)
+		ms.RuntimeState = LocalRuntimeState{}
 	}
 
 	// For historical reasons, DC state is initialized differently.

internal/store/runtime_state.go
@@ -37,43 +37,19 @@ type RuntimeState interface {
 type ProbeState int
 
 type LocalRuntimeState struct {
-	State                    model.ProcessState
+	Status                   model.RuntimeStatus
 	PID                      int
 	SpanID                   model.LogSpanID
 	LastReadyOrSucceededTime time.Time
-
-	readinessProbeSuccessful bool
+	Ready                    bool
 }
-
-func (LocalRuntimeState) RuntimeState() {}
 
 var _ RuntimeState = LocalRuntimeState{}
 
-func NewLocalRuntimeState(m model.Manifest) LocalRuntimeState {
-	var state LocalRuntimeState
-	if m.LocalTarget().ReadinessProbe == nil {
-		// targets with a readiness probe are failing by default (until probe passes)
-		// while targets without a readiness probe are initialized to success to
-		// prevent the process from never appearing as ready
-		state.readinessProbeSuccessful = true
-	}
-	return state
-}
+func (LocalRuntimeState) RuntimeState() {}
 
 func (l LocalRuntimeState) RuntimeStatus() model.RuntimeStatus {
-	if l.Ready() {
-		return model.RuntimeStatusOK
-	}
-
-	switch l.State {
-	case model.ProcessStateRunning:
-		// process is running but not ready
-		return model.RuntimeStatusPending
-	case model.ProcessStateTerminated:
-		return model.RuntimeStatusError
-	default:
-		return model.RuntimeStatusUnknown
-	}
+	return l.Status
 }
 
 func (l LocalRuntimeState) RuntimeStatusError() error {
@@ -86,20 +62,6 @@ func (l LocalRuntimeState) RuntimeStatusError() error {
 
 func (l LocalRuntimeState) HasEverBeenReadyOrSucceeded() bool {
 	return !l.LastReadyOrSucceededTime.IsZero()
-}
-
-func (l LocalRuntimeState) Ready() bool {
-	if l.State == model.ProcessStateRunning {
-		return l.readinessProbeSuccessful
-	}
-	return false
-}
-
-func (l *LocalRuntimeState) SetReadinessProbeState(success bool) {
-	if success {
-		l.LastReadyOrSucceededTime = time.Now()
-	}
-	l.readinessProbeSuccessful = success
 }
 
 type K8sRuntimeState struct {

pkg/model/process_state.go
@@ -1,12 +1,0 @@
-package model
-
-// ProcessState represents the current execution state of a local process.
-type ProcessState string
-
-const (
-	// ProcessStateRunning indicates that a process is alive and executing.
-	ProcessStateRunning ProcessState = "running"
-	// ProcessStateTerminated indicates that a process is no longer executing
-	// either because it exited or was terminated.
-	ProcessStateTerminated ProcessState = "terminated"
-)

internal/engine/local/controller_test.go
@@ -32,18 +32,18 @@ func TestUpdate(t *testing.T) {
 	t1 := time.Unix(1, 0)
 	f.resource("foo", "true", ".", t1)
 	f.step()
-	f.assertStatus("foo", model.ProcessStateRunning, 1)
+	f.assertStatus("foo", model.RuntimeStatusOK, 1)
 
 	t2 := time.Unix(2, 0)
 	f.resource("foo", "false", ".", t2)
 	f.step()
-	f.assertStatus("foo", model.ProcessStateRunning, 2)
+	f.assertStatus("foo", model.RuntimeStatusOK, 2)
 	f.assertNoAction("error for cancel", func(action store.Action) bool {
 		a, ok := action.(LocalServeStatusAction)
 		if !ok {
 			return false
 		}
-		return a.ManifestName == "foo" && a.State == model.ProcessStateTerminated
+		return a.ManifestName == "foo" && a.Status == model.RuntimeStatusError
 	})
 	f.fe.RequireNoKnownProcess(t, "true")
 	f.assertLogMessage("foo", "Starting cmd false")
@@ -57,7 +57,7 @@ func TestServe(t *testing.T) {
 	t1 := time.Unix(1, 0)
 	f.resource("foo", "sleep 60", "testdir", t1)
 	f.step()
-	f.assertStatus("foo", model.ProcessStateRunning, 1)
+	f.assertStatus("foo", model.RuntimeStatusOK, 1)
 
 	require.Equal(t, "testdir", f.fe.processes["sleep 60"].workdir)
 
@@ -112,7 +112,7 @@ func TestServeReadinessProbeInvalidSpec(t *testing.T) {
 	f.resourceFromTarget("foo", localTarget, t1)
 	f.step()
 
-	f.assertStatus("foo", model.ProcessStateRunning, 1)
+	f.assertStatus("foo", model.RuntimeStatusOK, 1)
 
 	f.assertLogMessage("foo", "Invalid readiness probe: unsupported probe type")
 	assert.Equal(t, 0, f.fpm.ProbeCount())
@@ -125,13 +125,13 @@ func TestFailure(t *testing.T) {
 	t1 := time.Unix(1, 0)
 	f.resource("foo", "true", ".", t1)
 	f.step()
-	f.assertStatus("foo", model.ProcessStateRunning, 1)
+	f.assertStatus("foo", model.RuntimeStatusOK, 1)
 	f.assertLogMessage("foo", "Starting cmd true")
 
 	err := f.fe.stop("true", 5)
 	require.NoError(t, err)
 
-	f.assertStatus("foo", model.ProcessStateTerminated, 1)
+	f.assertStatus("foo", model.RuntimeStatusError, 1)
 	f.assertLogMessage("foo", "cmd true exited with code 5")
 }
 
@@ -260,13 +260,13 @@ func (f *fixture) assertNoAction(msg string, pred func(action store.Action) bool
 	}
 }
 
-func (f *fixture) assertStatus(name string, status model.ProcessState, sequenceNum int) {
+func (f *fixture) assertStatus(name string, status model.RuntimeStatus, sequenceNum int) {
 	msg := fmt.Sprintf("didn't find name %s, status %v, sequence %d", name, status, sequenceNum)
 	pred := func(action store.Action) bool {
 		stAction, ok := action.(LocalServeStatusAction)
 		if !ok ||
 			stAction.ManifestName != model.ManifestName(name) ||
-			stAction.State != status {
+			stAction.Status != status {
 			return false
 		}
 		return true

internal/hud/webview/convert_test.go
@@ -277,15 +277,14 @@ func TestLocalResource(t *testing.T) {
 	}.WithDeployTarget(lt)
 
 	state := newState([]model.Manifest{m})
-	lrs := store.NewLocalRuntimeState(m)
-	lrs.State = model.ProcessStateRunning
+	lrs := store.LocalRuntimeState{Status: model.RuntimeStatusNotApplicable}
 	state.ManifestTargets[m.Name].State.RuntimeState = lrs
 	v := stateToProtoView(t, *state)
 
 	assert.Equal(t, 2, len(v.Resources))
 	r := v.Resources[1]
 	assert.Equal(t, "test", r.Name)
-	assert.Equal(t, model.RuntimeStatusOK, model.RuntimeStatus(r.RuntimeStatus))
+	assert.Equal(t, model.RuntimeStatusNotApplicable, model.RuntimeStatus(r.RuntimeStatus))
 	require.Len(t, r.Specs, 1)
 	spec := r.Specs[0]
 	require.Equal(t, proto_webview.TargetType_TARGET_TYPE_LOCAL, spec.Type)