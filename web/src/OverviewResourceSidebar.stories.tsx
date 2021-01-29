04941bd1ea3121cfbbe44178264227242043b1d6
Maia <maia@windmill.engineering>
2021-01-29 13:57:39 -0500

    storybook entry for tests with errors

--- web/src/OverviewResourceSidebar.stories.tsx
+++ web/src/OverviewResourceSidebar.stories.tsx
@@ -11,6 +11,7 @@ import {
   tiltfileResource,
   twoResourceView,
 } from "./testdata"
+import {UpdateStatus} from "./types";
 
 type Resource = Proto.webviewResource
 let pathBuilder = PathBuilder.forTesting("localhost", "/")
@@ -50,6 +51,26 @@ export function TwoResourcesTwoTests() {
   ]
   all[2].name = "snack"
   all[3].name = "beep"
+  let view = { resources: all, tiltfileKey: "test" }
+  return <OverviewResourceSidebar name={""} view={view} />
+}
+
+export function TestsWithErrors() {
+  let all: Resource[] = [
+    tiltfileResource(),
+  ]
+  for (let i = 0; i < 8; i++) {
+    let test = oneResourceTest()
+    test.name = "test_" + i
+    if (i % 2 === 0) {
+    if (!test.buildHistory) {
+      throw "unexpectedly empty buildHistory on test resource"
+    }
+    test.buildHistory[0].error = "egads!"
+      test.updateStatus = UpdateStatus.Error
+    }
+    all.push(test)
+  }
   let view = { resources: all, tiltfileKey: "test" }
   return <OverviewResourceSidebar name={""} view={view} />
 }
