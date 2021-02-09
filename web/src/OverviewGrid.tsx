861a007e076804959abd2082dab8c45714b1b1fd
Victor Wu <victor@tilt.dev>
2021-02-09 12:01:05 -0500

    web: Add divider lines between sections in Overview (#4181)

--- web/src/OverviewGrid.tsx
+++ web/src/OverviewGrid.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import styled from "styled-components"
 import OverviewItemView, { OverviewItem } from "./OverviewItemView"
-import { SizeUnit } from "./style-helpers"
+import { Color, SizeUnit } from "./style-helpers"
 
 type OverviewGridProps = {
   items: OverviewItem[]
@@ -16,6 +16,10 @@ let OverviewGridRoot = styled.ul`
   padding-left: ${SizeUnit(0.25)};
   position: relative;
   box-sizing: border-box;
+  border-bottom: 1px dotted ${Color.grayLight};
+  &:last-child {
+    border-bottom-width: 0;
+  }
 `
 
 export default function OverviewGrid(props: OverviewGridProps) {
