ef2adfac9e9be7dec095ddb1fb9a000af136c265
Nick Santos <nicholas.j.santos@gmail.com>
2021-03-19 18:44:54 -0400

    scripts: set permissions on codegen files (#4349)

--- scripts/update-codegen.sh
+++ scripts/update-codegen.sh
@@ -5,6 +5,6 @@ set -e
 DIR=$(dirname "$0")
 cd "$DIR/.."
 
-exec docker run -v "$(pwd)":/go/src/github.com/tilt-dev/tilt --workdir /go/src/github.com/tilt-dev/tilt \
+exec docker run -e "CODEGEN_USER=$USER" -v "$(pwd)":/go/src/github.com/tilt-dev/tilt --workdir /go/src/github.com/tilt-dev/tilt \
    --entrypoint ./scripts/update-codegen-helper.sh \
    golang:1.15
