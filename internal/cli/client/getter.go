1fa9b000e60264919173789fec2cc170f2864d45
Nick Santos <nicholas.j.santos@gmail.com>
2021-04-02 13:09:31 -0400

    cli: add 'tilt alpha get' for fetching from the apiserver
    This is a fork of kubectl get, simplified a bit and adapted to Tilt's
    Command conventions.
    
    I expect this will eventually graduate to `tilt get` (replacing `tilt dump engine`),
    and we'll do a similar thing with kubectl create/delete/apply/api-resources

---
+++ internal/cli/client/getter.go
@@ -0,0 +1,132 @@
+/*
+Adapted from
+https://github.com/kubernetes/cli-runtime/tree/master/pkg/genericclioptions
+*/
+
+/*
+Copyright 2014 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package client
+
+import (
+	"fmt"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"time"
+
+	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+	"k8s.io/client-go/discovery"
+	diskcached "k8s.io/client-go/discovery/cached/disk"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/restmapper"
+	"k8s.io/client-go/tools/clientcmd"
+	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
+	"k8s.io/client-go/util/homedir"
+)
+
+var (
+	defaultCacheDir = filepath.Join(homedir.HomeDir(), ".tilt-dev", "cache")
+)
+
+// Composes the set of values necessary
+// for obtaining a REST client config
+type Getter struct {
+	Host string
+	Port int
+}
+
+func NewGetter(host string, port int) *Getter {
+	return &Getter{Host: host, Port: port}
+}
+
+var _ genericclioptions.RESTClientGetter = &Getter{}
+
+// ToRESTConfig implements RESTClientGetter.
+// Returns a REST client configuration based on a provided path
+// to a .kubeconfig file, loading rules, and config flag overrides.
+// Expects the AddFlags method to have been called.
+func (f *Getter) ToRESTConfig() (*rest.Config, error) {
+	return f.ToRawKubeConfigLoader().ClientConfig()
+}
+
+// ToRawKubeConfigLoader binds config flag values to config overrides
+// Returns an interactive clientConfig if the password flag is enabled,
+// or a non-interactive clientConfig otherwise.
+func (f *Getter) ToRawKubeConfigLoader() clientcmd.ClientConfig {
+	const apiserver = "tilt-apiserver"
+	config := clientcmdapi.NewConfig()
+	config.Clusters[apiserver] = &clientcmdapi.Cluster{
+		Server:                fmt.Sprintf("http://%s:%d", f.Host, f.Port),
+		InsecureSkipTLSVerify: true,
+	}
+	config.AuthInfos[apiserver] = &clientcmdapi.AuthInfo{
+		Username: "corgi",
+		Password: "charge!!!",
+	}
+	config.Contexts[apiserver] = &clientcmdapi.Context{
+		Cluster:  apiserver,
+		AuthInfo: apiserver,
+	}
+	config.CurrentContext = apiserver
+
+	return clientcmd.NewNonInteractiveClientConfig(*config, apiserver, &clientcmd.ConfigOverrides{}, nil)
+}
+
+// ToDiscoveryClient implements RESTClientGetter.
+// Expects the AddFlags method to have been called.
+// Returns a CachedDiscoveryInterface using a computed RESTConfig.
+func (f *Getter) ToDiscoveryClient() (discovery.CachedDiscoveryInterface, error) {
+	config, err := f.ToRESTConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	// The more groups you have, the more discovery requests you need to make.
+	// given 25 groups (our groups + a few custom resources) with one-ish version each, discovery needs to make 50 requests
+	// double it just so we don't end up here again for a while.  This config is only used for discovery.
+	config.Burst = 100
+
+	cacheDir := defaultCacheDir
+	httpCacheDir := filepath.Join(cacheDir, "http")
+	discoveryCacheDir := computeDiscoverCacheDir(filepath.Join(cacheDir, "discovery"), config.Host)
+
+	return diskcached.NewCachedDiscoveryClientForConfig(config, discoveryCacheDir, httpCacheDir, time.Duration(10*time.Minute))
+}
+
+func (f *Getter) ToRESTMapper() (meta.RESTMapper, error) {
+	discoveryClient, err := f.ToDiscoveryClient()
+	if err != nil {
+		return nil, err
+	}
+
+	mapper := restmapper.NewDeferredDiscoveryRESTMapper(discoveryClient)
+	expander := restmapper.NewShortcutExpander(mapper, discoveryClient)
+	return expander, nil
+}
+
+// overlyCautiousIllegalFileCharacters matches characters that *might* not be supported.  Windows is really restrictive, so this is really restrictive
+var overlyCautiousIllegalFileCharacters = regexp.MustCompile(`[^(\w/\.)]`)
+
+// computeDiscoverCacheDir takes the parentDir and the host and comes up with a "usually non-colliding" name.
+func computeDiscoverCacheDir(parentDir, host string) string {
+	// strip the optional scheme from host if its there:
+	schemelessHost := strings.Replace(strings.Replace(host, "https://", "", 1), "http://", "", 1)
+	// now do a simple collapse of non-AZ09 characters.  Collisions are possible but unlikely.  Even if we do collide the problem is short lived
+	safeHost := overlyCautiousIllegalFileCharacters.ReplaceAllString(schemelessHost, "_")
+	return filepath.Join(parentDir, safeHost)
+}
