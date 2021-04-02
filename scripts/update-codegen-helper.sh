cb997425cc136e23296b397df3ab0d49c6960f73
Nick Santos <nicholas.j.santos@gmail.com>
2021-04-02 17:08:34 -0400

    clientset: remove dead code
    Kubernetes in general has moved away from needing generated clients, and
    instead now prefers the more dynamic approach. So we can remove all this.

--- scripts/update-codegen-helper.sh
+++ scripts/update-codegen-helper.sh
@@ -27,31 +27,14 @@ export GOPATH
 
 SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
-export CLIENTSET_NAME_VERSIONED="tiltapi"
 
 if [[ "$(pwd)" != "${GOPATH}"* ]]; then
     echo "ERROR: update-codegen.sh does not work correctly outside of GOPATH: $GOPATH $(pwd)"
     exit 1
 fi
 
-# generate the code with:
-# --output-base    because this script should also be able to run inside the vendor dir of
-#                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
-#                  instead of the $GOPATH directly. For normal projects this can be dropped.
-bash "${CODEGEN_PKG}/generate-groups.sh" all \
-  github.com/tilt-dev/tilt/pkg github.com/tilt-dev/tilt/pkg/apis \
-  "core:v1alpha1" \
-  --output-base "$(dirname "${BASH_SOURCE[0]}")/../../../.." \
-  --go-header-file "${SCRIPT_ROOT}"/hack/boilerplate.go.txt
-
 bash "${CODEGEN_PKG}/generate-internal-groups.sh" "deepcopy,defaulter,openapi" \
   github.com/tilt-dev/tilt/pkg github.com/tilt-dev/tilt/pkg/apis github.com/tilt-dev/tilt/pkg/apis \
   "core:v1alpha1" \
   --output-base "$(dirname "${BASH_SOURCE[0]}")/../../../.." \
   --go-header-file "${SCRIPT_ROOT}/hack/boilerplate.go.txt"
-
-if [[ -n "${CODEGEN_USER-}" ]]; then
-    chown "$CODEGEN_USER" -R pkg/clientset
-    chown "$CODEGEN_USER" -R pkg/informers
-    chown "$CODEGEN_USER" -R pkg/listers
-fi
