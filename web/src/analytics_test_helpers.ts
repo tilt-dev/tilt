c44aaaa98a69f4ee22ea5790750cb28535cf741c
Matt Landis <matt@windmill.engineering>
2021-04-08 17:12:38 -0400

    prettier

--- web/src/analytics_test_helpers.ts
+++ web/src/analytics_test_helpers.ts
@@ -28,14 +28,18 @@ export function expectIncr(fetchMockIndex: number, name: string, tags: Tags) {
 }
 
 export function expectIncrs(...incrs: { name: string; tags: Tags }[]) {
-  const expectedRequestBodies = incrs.map((i) => [{
+  const expectedRequestBodies = incrs.map((i) => [
+    {
       verb: "incr",
       name: i.name,
       tags: i.tags,
-    }])
+    },
+  ])
   const incrCalls = fetchMock.mock.calls.filter((e) =>
     e[0]?.toString().endsWith("/api/analytics")
   )
-  const actualRequestBodies = incrCalls.map((e) => JSON.parse(e[1]?.body?.toString() ?? ""))
+  const actualRequestBodies = incrCalls.map((e) =>
+    JSON.parse(e[1]?.body?.toString() ?? "")
+  )
   expect(actualRequestBodies).toEqual(expectedRequestBodies)
 }
