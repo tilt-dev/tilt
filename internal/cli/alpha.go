85ae858def2bdaa6a7a8b28953c784ceb68b15ef
Nick Santos <nicholas.j.santos@gmail.com>
2021-04-14 17:03:25 -0400

    cli: add tilt alpha apply (#4432)

--- internal/cli/alpha.go
+++ internal/cli/alpha.go
@@ -18,6 +18,7 @@ The APIs of these commands may change frequently.
 	addCommand(result, newEditCmd())
 	addCommand(result, newApiresourcesCmd())
 	addCommand(result, newDeleteCmd())
+	addCommand(result, newApplyCmd())
 
 	return result
 }
