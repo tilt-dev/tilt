6b3d8f446afcd4a0b6ebe01e979d33b7469f2b54
Maia <maia@windmill.engineering>
2021-05-03 11:55:57 -0400

    add spanID annotation

--- internal/engine/portforward/subscriber.go
+++ internal/engine/portforward/subscriber.go
@@ -73,6 +73,7 @@ func (s *Subscriber) diff(ctx context.Context, st store.RStore) (toStart []portF
 					// Name of the manifest that this Port Forward corresponds to
 					// (we need this to route the logs correctly)
 					v1alpha1.AnnotationManifest: manifest.Name.String(),
+					v1alpha1.AnnotationSpanID:   string(k8sconv.SpanIDForPod(podID)),
 				},
 			},
 			Spec: PortForwardSpec{
