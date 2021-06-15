c09cac38feb9b27b5f49c9389c76d799e34746a2
Nick Santos <nicholas.j.santos@gmail.com>
2021-06-15 09:35:23 -0400

    apis: ImageMap fields (#4646)

--- pkg/apis/core/v1alpha1/imagemap_types.go
+++ pkg/apis/core/v1alpha1/imagemap_types.go
@@ -46,6 +46,11 @@ import (
 // For example, a builder that builds to a local image registry might create
 // a map from: 'my-apiserver:dev' to 'localhost:5000/my-apiserver:content-based-label'.
 //
+// ImageMap doesn't follow the usual Kubernetes-style API semantics
+// (where the Status is the result of running the Spec). It's closer to a
+// ConfigMap. Though the Status does represent a real runtime result
+// (an image in a registry).
+//
 // +k8s:openapi-gen=true
 type ImageMap struct {
 	metav1.TypeMeta   `json:",inline"`
@@ -66,6 +71,59 @@ type ImageMapList struct {
 
 // ImageMapSpec defines the desired state of ImageMap
 type ImageMapSpec struct {
+	// A named image reference.
+	//
+	// Deployment tools expect this image reference to match an image
+	// in the YAML being deployed, and will replace that image reference.
+	//
+	// By default, this selector will match an image if the names match
+	// (tags on both the selector and the matched reference are ignored).
+	Selector string `json:"selector" protobuf:"bytes,1,opt,name=selector"`
+
+	// If specified, then tags on both the selector and the matched
+	// reference are used for matching. The selector will only
+	// match the reference if the tags match exactly.
+	//
+	// +optional
+	MatchExact bool `json:"matchExact,omitempty" protobuf:"varint,2,opt,name=matchExact"`
+
+	// If specified, then the selector will also match any strings
+	// in container env variables.
+	//
+	// +optional
+	MatchInEnvVars bool `json:"matchInEnvVars,omitempty" protobuf:"varint,3,opt,name=matchInEnvVars"`
+
+	// If specified, the injector will replace the 'command'
+	// field in the container when it replaces the image.
+	//
+	// +optional
+	OverrideCommand *ImageMapOverrideCommand `json:"overrideCommand,omitempty" protobuf:"bytes,4,opt,name=overrideCommand"`
+
+	// If specified, the injector will replace the 'args'
+	// field in the container when it replaces the image.
+	//
+	// +optional
+	OverrideArgs *ImageMapOverrideArgs `json:"overrideArgs,omitempty" protobuf:"bytes,5,opt,name=overrideArgs"`
+}
+
+// ImageMapCommandOverride defines a command to inject when the image
+// is injected. Only applies to types that embed a v1.Container
+// with a Command field.
+//
+// https://pkg.go.dev/k8s.io/api/core/v1#Container
+type ImageMapOverrideCommand struct {
+	// A list of command strings.
+	Command []string `json:"command" protobuf:"bytes,1,rep,name=command"`
+}
+
+// ImageMapArgsOverride defines args to inject when the image
+// is injected. Only applies to types that embed a v1.Container
+// with a Command field.
+//
+// https://pkg.go.dev/k8s.io/api/core/v1#Container
+type ImageMapOverrideArgs struct {
+	// A list of args strings.
+	Args []string `json:"args" protobuf:"bytes,1,rep,name=args"`
 }
 
 var _ resource.Object = &ImageMap{}
@@ -112,6 +170,24 @@ func (in *ImageMapList) GetListMeta() *metav1.ListMeta {
 
 // ImageMapStatus defines the observed state of ImageMap
 type ImageMapStatus struct {
+	// A fully-qualified image reference, including a name and an immutable tag.
+	//
+	// The image will not necessarily have the same repo URL as the selector. Many
+	// Kubernetes clusters let you push to a local registry for local development.
+	Image string `json:"image" protobuf:"bytes,1,opt,name=image"`
+
+	// TODO(nick): I'm not totally sure how we should model registries in this system.
+	//
+	// We need to be able to support an image existing at multiple URLs in
+	// multiple registries.  Even more subtly, a registry might have multiple URLs
+	// (the local registry spec has 3 - from the POV of a user outside the cluster,
+	// from the POV of the cluster's container runtime, and from the POV of the
+	// cluster network).
+	//
+	// This might mean we have multiple image references, or it might mean
+	// we have one image reference that gets rebased to multiple registries.
+	//
+	// It might make sense for a Registry to be its own API object.
 }
 
 // ImageMap implements ObjectWithStatusSubResource interface.
