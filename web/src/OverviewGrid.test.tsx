6b7420d1cad51a3b8986094e1ec7a3c90896f8ad
Han Yu <han@sent.com>
2021-07-02 14:36:30 -0400

    web: replace Grid with Table Overview

--- web/src/OverviewGrid.test.tsx
+++
@@ -1,8 +0,0 @@
-import { mount } from "enzyme"
-import React from "react"
-import { MemoryRouter } from "react-router"
-import { TenResources } from "./OverviewGrid.stories"
-
-it("renders 10 item grid", () => {
-  mount(<MemoryRouter initialEntries={["/"]}>{TenResources()}</MemoryRouter>)
-})
