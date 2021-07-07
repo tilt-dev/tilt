a497d5c8bffacf26a5a7874bc0e3ce6f8dcca532
Matt Landis <matt@windmill.engineering>
2021-07-07 11:53:30 -0400

    tiltfile: add `module`

--- internal/tiltfile/starlarkstruct/struct_test.go
+++ internal/tiltfile/starlarkstruct/struct_test.go
@@ -21,6 +21,21 @@ print("b",x.b)
 	require.Contains(t, f.PrintOutput(), "b 2")
 }
 
+func TestModule(t *testing.T) {
+	f := NewFixture(t)
+	f.File("Tiltfile", `
+x = module("test_module", a = "foo", b = 2)
+print("a",x.a)
+print("b",x.b)
+print("x",x)
+`)
+	_, err := f.ExecFile("Tiltfile")
+	require.NoError(t, err)
+	require.Contains(t, f.PrintOutput(), "a foo")
+	require.Contains(t, f.PrintOutput(), "b 2")
+	require.Contains(t, f.PrintOutput(), "x <module \"test_module\">")
+}
+
 func NewFixture(tb testing.TB) *starkit.Fixture {
 	return starkit.NewFixture(tb, NewExtension())
 }
