d137297a5d8bca6f67847bee4a9e8d2ad3c995a3
Matt Landis <landism@users.noreply.github.com>
2021-08-31 10:51:48 -0400

    web: add InstrumentedTextField (#4906)

--- web/src/instrumentedComponents.tsx
+++ web/src/instrumentedComponents.tsx
@@ -1,23 +1,35 @@
-import { Button, ButtonProps } from "@material-ui/core"
-import React from "react"
+import {
+  Button,
+  ButtonProps,
+  debounce,
+  TextField,
+  TextFieldProps,
+} from "@material-ui/core"
+import React, { useMemo } from "react"
 import { AnalyticsAction, incr, Tags } from "./analytics"
 
-export type InstrumentedButtonProps = ButtonProps & {
+// Shared components that implement analytics
+// 1. Saves callers from having to implement/test analytics for every interactive
+//    component.
+// 2. Allows wrappers to cheaply require analytics params.
+
+type InstrumentationProps = {
   analyticsName: string
   analyticsTags?: Tags
 }
 
-export function InstrumentedButton(props: InstrumentedButtonProps) {
+export function InstrumentedButton(props: ButtonProps & InstrumentationProps) {
   const { analyticsName, analyticsTags, onClick, ...buttonProps } = { ...props }
-  const tags = { action: AnalyticsAction.Click, ...(analyticsTags ?? {}) }
-  const instrumentedOnClick = (
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
-    incr(analyticsName, tags)
+  const instrumentedOnClick: typeof onClick = (e) => {
+    incr(analyticsName, {
+      action: AnalyticsAction.Click,
+      ...(analyticsTags ?? {}),
+    })
     if (onClick) {
       onClick(e)
     }
   }
+
   return (
     <Button
       variant="outlined"
@@ -26,4 +38,40 @@ export function InstrumentedButton(props: InstrumentedButtonProps) {
       {...buttonProps}
     />
   )
+}
+
+// How long to debounce TextField edit events. i.e., only send one edit
+// event per this duration. These don't need to be submitted super
+// urgently, and we want to be closer to sending one per user intent than
+// one per keystroke.
+const textFieldEditDebounceMilliseconds = 5000
+
+export function InstrumentedTextField(
+  props: TextFieldProps & InstrumentationProps
+) {
+  const { analyticsName, analyticsTags, onChange, ...textFieldProps } = {
+    ...props,
+  }
+
+  // we have to memoize the debounced function so that incrs reuse the same debounce timer
+  const debouncedIncr = useMemo(
+    () =>
+      // debounce so we don't send analytics for every single keypress
+      debounce(() => {
+        incr(analyticsName, {
+          action: AnalyticsAction.Edit,
+          ...(analyticsTags ?? {}),
+        })
+      }, textFieldEditDebounceMilliseconds),
+    [analyticsName, analyticsTags]
+  )
+
+  const instrumentedOnChange: typeof onChange = (e) => {
+    debouncedIncr()
+    if (onChange) {
+      onChange(e)
+    }
+  }
+
+  return <TextField onChange={instrumentedOnChange} {...textFieldProps} />
 }
