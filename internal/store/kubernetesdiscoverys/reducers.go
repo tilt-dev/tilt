ec08ded76e5ab8131fcf668b19ab1584fcec655a
Nick Santos <nick@tilt.dev>
2021-10-01 18:49:01 -0400

    hack around a test

--- internal/store/kubernetesdiscoverys/reducers.go
+++ internal/store/kubernetesdiscoverys/reducers.go
@@ -1,6 +1,7 @@
 package kubernetesdiscoverys
 
 import (
+	"github.com/tilt-dev/tilt/internal/controllers/apicmp"
 	"github.com/tilt-dev/tilt/internal/store"
 	"github.com/tilt-dev/tilt/internal/store/k8sconv"
 	"github.com/tilt-dev/tilt/internal/store/liveupdates"
@@ -9,15 +10,34 @@ import (
 
 func HandleKubernetesDiscoveryUpsertAction(state *store.EngineState, action KubernetesDiscoveryUpsertAction) {
 	n := action.KubernetesDiscovery.Name
+	oldState := state.KubernetesDiscoverys[n]
 	state.KubernetesDiscoverys[n] = action.KubernetesDiscovery
-	RefreshKubernetesResource(state, n)
-	liveupdates.CheckForContainerCrash(state, n)
+
+	// We only refresh when the K8sDiscovery is changed.
+	//
+	// This is really only needed for tests - we have tests that wait until we've
+	// reached a steady state, then change some fields on EngineState.
+	//
+	// K8s controllers assume everything is idempotent, and will wipe out our changes
+	// later with duplicate events.
+	isChanged := oldState == nil ||
+		!apicmp.DeepEqual(oldState.Status, action.KubernetesDiscovery.Status) ||
+		!apicmp.DeepEqual(oldState.Spec, action.KubernetesDiscovery.Spec)
+	if isChanged {
+		RefreshKubernetesResource(state, n)
+		liveupdates.CheckForContainerCrash(state, n)
+	}
 }
 
 func HandleKubernetesDiscoveryDeleteAction(state *store.EngineState, action KubernetesDiscoveryDeleteAction) {
+	oldState := state.KubernetesDiscoverys[action.Name]
 	delete(state.KubernetesDiscoverys, action.Name)
-	RefreshKubernetesResource(state, action.Name)
-	liveupdates.CheckForContainerCrash(state, action.Name)
+
+	isChanged := oldState != nil
+	if isChanged {
+		RefreshKubernetesResource(state, action.Name)
+		liveupdates.CheckForContainerCrash(state, action.Name)
+	}
 }
 
 func RefreshKubernetesResource(state *store.EngineState, name string) {
