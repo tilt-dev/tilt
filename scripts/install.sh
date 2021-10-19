7927442b774756b4d659e87e61645f52c358d28c
Jerome Petazzoni <jerome.petazzoni@gmail.com>
2021-10-19 20:57:53 +0200

    script: make install.sh arch-aware
    The installation scripts hard-code the architecture to x86_64.
    I wish I could tell you "hey, it doesn't work on my shiny new
    M1 super duper max", but I am, in fact, typing this from a
    Raspberry Pi. 😅
    
    (Changes tested on Linux on Pi, Intel 64 bits machines, and
    ARM 64 bits cloud VMs.)

--- scripts/install.sh
+++ scripts/install.sh
@@ -61,16 +61,34 @@ function install_tilt() {
           # under $HOME/.local/bin isn't overriding the homebrew one.
           rm -f "$HOME/.local/bin/tilt" || true
       else
+          # On Linux, "uname -m" reports "aarch64" on ARM 64 bits machines,
+          # and armv7l on ARM 32 bits machines like the Raspberry Pi.
+          # However, as of October 2021, the Tilt releases for linux/arm are
+          # named "arm64" and "arm", and tagged as _ALPHA releases.
+          # This is a small workaround so that the install script works on ARM.
+          case $(uname -m) in
+              aarch64) ARCH=arm64_ALPHA;;
+              armv7l)  ARCH=arm_ALPHA;;
+              *)       ARCH=$(uname -m);;
+          esac
           set -x
-          curl -fsSL https://github.com/tilt-dev/tilt/releases/download/v$VERSION/tilt.$VERSION.linux.x86_64.tar.gz | tar -xzv tilt
+          curl -fsSL https://github.com/tilt-dev/tilt/releases/download/v$VERSION/tilt.$VERSION.linux.$ARCH.tar.gz | tar -xzv tilt
           copy_binary
       fi
   elif [[ "$OSTYPE" == "darwin"* ]]; then
       if [[ "$BREW" != "" ]]; then
           brew_install_or_upgrade
       else
+          # On macOS, "uname -m" reports "arm64" on ARM 64 bits machines
+          # (like the M1). However, as of October 2021, the Tilt releases
+          # for darwin/arm are tagged as _ALPHA releases.
+          # This is a small workaround so that the install script works on the M1.
+          case $(uname -m) in
+              arm64) ARCH=arm64_ALPHA;;
+              *)     ARCH=$(uname -m);;
+          esac
           set -x
-          curl -fsSL https://github.com/tilt-dev/tilt/releases/download/v$VERSION/tilt.$VERSION.mac.x86_64.tar.gz | tar -xzv tilt
+          curl -fsSL https://github.com/tilt-dev/tilt/releases/download/v$VERSION/tilt.$VERSION.mac.$(uname -m).tar.gz | tar -xzv tilt
           copy_binary
       fi
   else
