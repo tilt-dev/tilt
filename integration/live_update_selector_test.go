ca418d1a4865365b1140b39a6cf6197fd0058619
Nick Santos <nick@tilt.dev>
2021-10-25 14:12:22 -0400

    store: fix a regression in how live-update interacts with selector-based pod discovery (#5073)

---
+++ integration/live_update_selector_test.go
@@ -0,0 +1,61 @@
+//+build integration
+
+package integration
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLiveUpdateSelector(t *testing.T) {
+	f := newK8sFixture(t, "live_update_selector")
+	defer f.TearDown()
+	f.SetRestrictedCredentials()
+
+	f.TiltUp()
+
+	ctx, cancel := context.WithTimeout(f.ctx, time.Minute)
+	defer cancel()
+	oneUpPods := f.WaitForAllPodsReady(ctx, "app=live-update-selector")
+
+	ctx, cancel = context.WithTimeout(f.ctx, time.Minute)
+	defer cancel()
+	f.CurlUntil(ctx, "http://localhost:31234", "🍄 One-Up! 🍄")
+
+	// Introduce a syntax error
+	f.ReplaceContents("compile.sh", "One-Up", "One-Up\"")
+
+	f.WaitUntil(ctx, "live_update syntax error", func() (string, error) {
+		return f.logs.String(), nil
+	}, "Failed to update container")
+
+	f.ReplaceContents("compile.sh", "One-Up\"", "Two-Up")
+
+	ctx, cancel = context.WithTimeout(f.ctx, time.Minute)
+	defer cancel()
+	f.CurlUntil(ctx, "http://localhost:31234", "🍄 Two-Up! 🍄")
+
+	twoUpPods := f.WaitForAllPodsReady(ctx, "app=live-update-selector")
+
+	// Assert that the pods were changed in-place, and not that we
+	// created new pods.
+	assert.Equal(t, oneUpPods, twoUpPods)
+
+	if len(twoUpPods) != 1 {
+		t.Fatalf("Expected one pod, actual: %v", twoUpPods)
+	}
+
+	// Delete the pod and make sure it got replaced with something that prints the
+	// same thing (crash rebuild).
+	f.runCommandSilently("kubectl", "delete", "pod", twoUpPods[0], namespaceFlag)
+
+	ctx, cancel = context.WithTimeout(f.ctx, time.Minute)
+	defer cancel()
+	f.CurlUntil(ctx, "http://localhost:31234", "🍄 Two-Up! 🍄")
+
+	newTwoUpPods := f.WaitForAllPodsReady(ctx, "app=live-update-selector")
+	assert.NotEqual(t, twoUpPods, newTwoUpPods)
+}
