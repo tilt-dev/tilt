26478ffecc5bcc46ccf38bb24c9010f71fb8e537
Milas Bowman <milas@tilt.dev>
2021-11-01 13:45:37 -0400

    localexec: provide a facade for local command exec (#5097)
    This is a Tilt-centric local process executor meant for commands
    that run to completion. The `Cmd` controller, which manages
    long-running processes for `local_resource`'s `serve_cmd`, has
    its own unexported implementation that's focused more on async
    signaling of process status for a daemon-style process that runs
    perpetually.
    
    There is overlap between these two, specifically in that they
    take care to use process groups and terminate the entire group to
    avoid leaking resources.
    
    The Tiltfile loader has been updated to use this for running
    `local()` commands, which should be invisible to consumers, but
    does mean that it's now possible to inject a fake execer for
    Tiltfile loading and return pre-canned responses for `local()`
    calls, which could be useful.

---
+++ internal/localexec/execer_test.go
@@ -0,0 +1,70 @@
+package localexec
+
+import (
+	"context"
+	"os"
+	"runtime"
+	"strconv"
+	"strings"
+	"syscall"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/tilt-dev/tilt/internal/testutils"
+	"github.com/tilt-dev/tilt/pkg/model"
+)
+
+func TestProcessExecer_Run(t *testing.T) {
+	ctx, _, _ := testutils.CtxAndAnalyticsForTest()
+	ctx, cancel := context.WithTimeout(ctx, 500*time.Millisecond)
+	defer cancel()
+
+	// this works across both cmd.exe + sh
+	script := `echo hello from stdout && echo hello from stderr 1>&2`
+
+	execer := NewProcessExecer(EmptyEnv())
+
+	r, err := OneShot(ctx, execer, model.ToHostCmd(script))
+
+	require.NoError(t, err)
+	assert.Equal(t, 0, r.ExitCode)
+	// trim space to not deal with line-ending/whitespace differences between cmd.exe/sh
+	assert.Equal(t, "hello from stdout", strings.TrimSpace(string(r.Stdout)))
+	assert.Equal(t, "hello from stderr", strings.TrimSpace(string(r.Stderr)))
+}
+
+func TestProcessExecer_Run_ProcessGroup(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping test in short mode")
+	}
+	if runtime.GOOS == "windows" {
+		t.Skip("test not supported on Windows")
+	}
+
+	ctx, _, _ := testutils.CtxAndAnalyticsForTest()
+	ctx, cancel := context.WithTimeout(ctx, 500*time.Millisecond)
+	defer cancel()
+
+	script := `sleep 60 & echo $!`
+
+	execer := NewProcessExecer(EmptyEnv())
+	r, err := OneShot(ctx, execer, model.ToUnixCmd(script))
+
+	require.NoError(t, err)
+	assert.Equal(t, 137, r.ExitCode)
+
+	output := strings.TrimSpace(string(r.Stdout))
+	if assert.NotEmpty(t, output) {
+		childPid, err := strconv.Atoi(output)
+		require.NoError(t, err, "Couldn't get child PID from stdout/stderr: %s", output)
+		// os.FindProcess is a no-op on Unix-like systems and always succeeds; need to send signal 0 to probe it
+		proc, _ := os.FindProcess(childPid)
+		err = proc.Signal(syscall.Signal(0))
+		if !assert.Equal(t, os.ErrProcessDone, err, "Child process was still running") {
+			_ = proc.Kill()
+		}
+	}
+}
