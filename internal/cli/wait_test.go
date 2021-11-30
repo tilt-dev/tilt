3393803952243553ffcd4544c0350ea38bf2978b
Nick Santos <nick@tilt.dev>
2021-11-30 16:04:29 -0500

    cli: add 'tilt wait' to wait on resources to become ready

---
+++ internal/cli/wait_test.go
@@ -0,0 +1,45 @@
+package cli
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/tilt-dev/tilt/pkg/apis"
+	"github.com/tilt-dev/tilt/pkg/apis/core/v1alpha1"
+)
+
+func TestWait(t *testing.T) {
+	f := newServerFixture(t)
+	defer f.TearDown()
+
+	err := f.client.Create(f.ctx, &v1alpha1.UIResource{
+		ObjectMeta: metav1.ObjectMeta{Name: "my-sleep"},
+		Status: v1alpha1.UIResourceStatus{
+			Conditions: []v1alpha1.UIResourceCondition{
+				{
+					Type:               v1alpha1.UIResourceReady,
+					Status:             metav1.ConditionTrue,
+					LastTransitionTime: apis.NowMicro(),
+				},
+			},
+		},
+	})
+	require.NoError(t, err)
+
+	out := bytes.NewBuffer(nil)
+	wait := newWaitCmd()
+	cmd := wait.register()
+	wait.flags.IOStreams.Out = out
+
+	err = cmd.Flags().Parse([]string{"--for=condition=Ready"})
+	require.NoError(t, err)
+
+	err = wait.run(f.ctx, []string{"uiresource/my-sleep"})
+	require.NoError(t, err)
+
+	assert.Contains(t, out.String(), `uiresource.tilt.dev/my-sleep condition met`)
+}
