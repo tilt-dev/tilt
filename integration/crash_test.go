a051018a1de35fe38f43665de0e4df618b091e8f
Nick Sieger <nick@nicksieger.com>
2021-12-01 10:08:07 -0600

    Fix TestCrash to specify port for both Tilts

--- integration/crash_test.go
+++ integration/crash_test.go
@@ -11,18 +11,16 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// Make sure that Tilt crashes if there are two tilts running on the same port.
+// Make sure that Tilt crashes if there are two tilts running on the same requested port.
 func TestCrash(t *testing.T) {
 	f := newK8sFixture(t, "oneup")
 	defer f.TearDown()
 
-	f.TiltUp()
+	f.TiltUp("--port=9975")
 	time.Sleep(500 * time.Millisecond)
 
 	out := bytes.NewBuffer(nil)
-	// fixture assigns a random unused port when created, which is used for all Tilt commands, so this
-	// will collide with the previous invocation
-	res, err := f.tilt.Up(f.ctx, UpCommandUp, out)
+	res, err := f.tilt.Up(f.ctx, UpCommandUp, out, "--port=9975")
 	assert.NoError(t, err)
 	<-res.Done()
 	assert.Contains(t, out.String(), "Tilt cannot start")
