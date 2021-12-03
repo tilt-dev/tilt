3f593401ae60ba2188545fc622692a9349827f37
Nick Sieger <nick@nicksieger.com>
2021-12-03 09:52:33 -0600

    errors: clean up error handling

--- internal/k8s/portforward.go
+++ internal/k8s/portforward.go
@@ -156,18 +156,17 @@ func (c portForwardClient) CreatePortForwarder(ctx context.Context, namespace Na
 
 func getListenableAddresses(host string) ([]string, error) {
 	// handle IPv6 literals like `[::1]`
-	url, hostErr := url.Parse(fmt.Sprintf("http://%s/", host))
-	if hostErr != nil {
-		return nil, errors.Wrap(hostErr, fmt.Sprintf("invalid host %s", host))
+	url, err := url.Parse(fmt.Sprintf("http://%s/", host))
+	if err != nil {
+		return nil, errors.Wrap(err, fmt.Sprintf("invalid host %s", host))
 	}
-	addresses, hostErr := net.LookupHost(url.Hostname())
-	if hostErr != nil {
-		return nil, errors.Wrap(hostErr, fmt.Sprintf("failed to look up address for %s", host))
+	addresses, err := net.LookupHost(url.Hostname())
+	if err != nil {
+		return nil, errors.Wrap(err, fmt.Sprintf("failed to look up address for %s", host))
 	}
 	listenable := make([]string, 0)
 	for _, addr := range addresses {
 		var l net.Listener
-		var err error
 		if ipv6 := strings.Contains(addr, ":"); ipv6 {
 			l, err = net.Listen("tcp6", fmt.Sprintf("[%s]:0", addr))
 		} else {
@@ -179,7 +178,7 @@ func getListenableAddresses(host string) ([]string, error) {
 		}
 	}
 	if len(listenable) == 0 {
-		return nil, errors.New(fmt.Sprintf("host %s: cannot listen on any resolved addresses: %v", host, addresses))
+		return nil, errors.Errorf("host %s: cannot listen on any resolved addresses: %v", host, addresses)
 	}
 	return listenable, nil
 }
