a5eceb791d1edbb964e8ef6de08b67d1e460bd15
Matt Landis <matt@windmill.engineering>
2021-12-14 17:16:52 -0500

    engine: clean out build history/state of disabled resources

--- internal/store/uiresources/reducers.go
+++ internal/store/uiresources/reducers.go
@@ -29,6 +29,16 @@ func HandleUIResourceUpsertAction(state *store.EngineState, action UIResourceUps
 			a := store.NewLogAction(model.ManifestName(n), logstore.SpanID(fmt.Sprintf("disabletoggle-%s", n)), logger.InfoLvl, nil, []byte(message))
 			state.LogStore.Append(a, state.Secrets)
 		}
+
+		ms, ok := state.ManifestState(model.ManifestName(n))
+		if uir.Status.DisableStatus.DisabledCount > 0 && ok {
+			// since file watches are disabled while a resource is disabled, we can't
+			// have confidence in any previous build state
+			ms.BuildHistory = nil
+			if len(ms.BuildStatuses) > 0 {
+				ms.BuildStatuses = make(map[model.TargetID]*store.BuildStatus)
+			}
+		}
 	}
 
 	state.UIResources[n] = uir
