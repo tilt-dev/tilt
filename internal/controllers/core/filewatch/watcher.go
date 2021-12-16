18263b5e17f47a85cacf253f8404e84c10e51618
Milas Bowman <milas@tilt.dev>
2021-12-16 15:19:32 -0500

    api: use patch for filewatch status updates

--- internal/controllers/core/filewatch/watcher.go
+++ internal/controllers/core/filewatch/watcher.go
@@ -10,6 +10,7 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	ctrlclient "sigs.k8s.io/controller-runtime/pkg/client"
 
+	"github.com/tilt-dev/tilt/internal/controllers/apicmp"
 	"github.com/tilt-dev/tilt/internal/store"
 	"github.com/tilt-dev/tilt/internal/watch"
 	filewatches "github.com/tilt-dev/tilt/pkg/apis/core/v1alpha1"
@@ -60,20 +61,41 @@ func (w *watcher) recordEvent(ctx context.Context, client ctrlclient.Client, st 
 			w.status.FileEvents = w.status.FileEvents[len(w.status.FileEvents)-MaxFileEventHistory:]
 		}
 
+		// TODO(milas): we should only update the internal status and then trigger
+		// 	reconciliation and let that handle updating status
+		//
+		// historically, this method did a PUT/replace, which meant file events
+		// could be indefinitely delayed on optimistic concurrency failures due
+		// to lack of retry
+		//
+		// now, we PATCH assuming the spec matches at the time of fetch, but this
+		// means there is technically still a race if the spec changes between
+		// our fetch and server processing the update, where we'll post "stale"
+		// events, but this is less harmful than effectively missing files events
 		var fw filewatches.FileWatch
 		err := client.Get(ctx, w.name, &fw)
 		if err != nil {
-			// status is updated internally so will become eventually consistent, but if there's no file
-			// changes for a while after this, the updates aren't going to appear; retry logic is probably
-			// warranted here
+			// status is updated internally so will become eventually consistent,
+			// but the file event won't be seen until the _next_ event!
+			//
+			// see the note above on how we should really resolve this
+			return nil
+		}
+		if !apicmp.DeepEqual(w.spec, fw.Spec) {
+			// spec changed - these events are outdated, so don't write them to the status
 			return nil
 		}
 
-		w.status.DeepCopyInto(&fw.Status)
-		err = client.Status().Update(ctx, &fw)
+		// only modify the specific fields on status that we care about so we can do a PATCH
+		patchBase := ctrlclient.MergeFrom(fw.DeepCopy())
+		updateStatus := w.status.DeepCopy()
+		fw.Status.LastEventTime = updateStatus.LastEventTime
+		fw.Status.FileEvents = updateStatus.FileEvents
+
+		err = client.Status().Patch(ctx, &fw, patchBase)
 		if err == nil {
 			st.Dispatch(NewFileWatchUpdateStatusAction(&fw))
-		} else if !apierrors.IsNotFound(err) && !apierrors.IsConflict(err) {
+		} else if !apierrors.IsNotFound(err) {
 			// can safely ignore not found/conflict errors - because this work loop is the only updater of
 			// status, any conflict errors means the spec was changed since fetching it, and as a result,
 			// these events are no longer useful anyway
