a3f17ca2ec7c860be6da9908c6252cf8dce5cd24
Milas Bowman <milas@tilt.dev>
2022-03-04 11:40:33 -0500

    k8s: improve error messages during Pod exec failures (#5557)
    When running a command in a Pod, `client-go` attempts to handle
    the failure case of a non-zero exit code by inspecting a
    `metav1.Status` object.
    
    For other failure cases, it returns `fmt.Errorf(status.Message)`,
    and `Message` might be blank, resulting in cryptic error messages
    from Tilt.
    
    There's not a lot we can do without upstream `client-go` changes
    on the Tilt side, so some extra context is added around where the
    exec failed, and for the stream error case, a generic fallback
    error message is used if we get an empty error from `client-go`.

--- internal/k8s/exec.go
+++ internal/k8s/exec.go
@@ -2,6 +2,8 @@ package k8s
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"io"
 
 	corev1 "k8s.io/api/core/v1"
@@ -11,7 +13,7 @@ import (
 	"github.com/tilt-dev/tilt/internal/container"
 )
 
-func (k *K8sClient) Exec(ctx context.Context, podID PodID, cName container.Name, n Namespace, cmd []string, stdin io.Reader, stdout io.Writer, stderr io.Writer) error {
+func (k *K8sClient) Exec(_ context.Context, podID PodID, cName container.Name, n Namespace, cmd []string, stdin io.Reader, stdout io.Writer, stderr io.Writer) error {
 	req := k.core.RESTClient().Post().
 		Resource("pods").
 		Namespace(n.String()).
@@ -28,12 +30,25 @@ func (k *K8sClient) Exec(ctx context.Context, podID PodID, cName container.Name,
 
 	exec, err := remotecommand.NewSPDYExecutor(k.restConfig, "POST", req.URL())
 	if err != nil {
-		return err
+		return fmt.Errorf("establishing connection: %w", err)
 	}
 
-	return exec.Stream(remotecommand.StreamOptions{
+	err = exec.Stream(remotecommand.StreamOptions{
 		Stdin:  stdin,
 		Stdout: stdout,
 		Stderr: stderr,
 	})
+	if err != nil {
+		if err.Error() == "" {
+			// Executor::Stream() attempts to decode metav1.Status errors to
+			// handle non-zero exit codes from commands; unfortunately, for
+			// all _other_ failure cases, the error returned is the `Message`
+			// field, which might be empty and there's no way for us to further
+			// introspect at this point, so a generic message is the best we
+			// can do here
+			err = errors.New("unknown server error")
+		}
+		return fmt.Errorf("executing command: %w", err)
+	}
+	return nil
 }
