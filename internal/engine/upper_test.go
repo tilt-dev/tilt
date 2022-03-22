1174a1e69ff160ece7cc85db1f3245207dcbd2f2
Nick Santos <nick@tilt.dev>
2022-03-22 17:52:55 -0400

    dockercomposeservice: publish the container name to the status API (#5620)
    This makes it a bit easier to print human-readable output.

--- internal/engine/upper_test.go
+++ internal/engine/upper_test.go
@@ -369,7 +369,7 @@ func (b *fakeBuildAndDeployer) BuildAndDeploy(ctx context.Context, st store.RSto
 
 		dcContainerState := b.nextDockerComposeContainerState
 		result[call.dc().ID()] = store.NewDockerComposeDeployResult(
-			call.dc().ID(), dockercompose.ToServiceStatus(dcContainerID, dcContainerState, nil))
+			call.dc().ID(), dockercompose.ToServiceStatus(dcContainerID, string(dcContainerID), dcContainerState, nil))
 	}
 
 	if kTarg := call.k8s(); !kTarg.Empty() {
