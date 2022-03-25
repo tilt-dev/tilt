a16d60c58f66732ec867cd9c9c57028c52ce8af2
Lizz Thabet <lizzthabet@gmail.com>
2022-03-25 15:44:18 -0400

    cluster: add registry field to Cluster API object (#5631)

--- pkg/openapi/zz_generated.openapi.go
+++ pkg/openapi/zz_generated.openapi.go
@@ -149,6 +149,7 @@ func GetOpenAPIDefinitions(ref common.ReferenceCallback) map[string]common.OpenA
 		"github.com/tilt-dev/tilt/pkg/apis/core/v1alpha1.PortForwardStatus":               schema_pkg_apis_core_v1alpha1_PortForwardStatus(ref),
 		"github.com/tilt-dev/tilt/pkg/apis/core/v1alpha1.PortForwardTemplateSpec":         schema_pkg_apis_core_v1alpha1_PortForwardTemplateSpec(ref),
 		"github.com/tilt-dev/tilt/pkg/apis/core/v1alpha1.Probe":                           schema_pkg_apis_core_v1alpha1_Probe(ref),
+		"github.com/tilt-dev/tilt/pkg/apis/core/v1alpha1.RegistryHosting":                 schema_pkg_apis_core_v1alpha1_RegistryHosting(ref),
 		"github.com/tilt-dev/tilt/pkg/apis/core/v1alpha1.RestartOnSpec":                   schema_pkg_apis_core_v1alpha1_RestartOnSpec(ref),
 		"github.com/tilt-dev/tilt/pkg/apis/core/v1alpha1.Session":                         schema_pkg_apis_core_v1alpha1_Session(ref),
 		"github.com/tilt-dev/tilt/pkg/apis/core/v1alpha1.SessionList":                     schema_pkg_apis_core_v1alpha1_SessionList(ref),
@@ -436,11 +437,17 @@ func schema_pkg_apis_core_v1alpha1_ClusterStatus(ref common.ReferenceCallback) c
 							Ref:         ref("k8s.io/apimachinery/pkg/apis/meta/v1.MicroTime"),
 						},
 					},
+					"registry": {
+						SchemaProps: spec.SchemaProps{
+							Description: "Registry describes a local registry that developer tools can connect to. A local registry allows clients to load images into the local cluster by pushing to this registry.",
+							Ref:         ref("github.com/tilt-dev/tilt/pkg/apis/core/v1alpha1.RegistryHosting"),
+						},
+					},
 				},
 			},
 		},
 		Dependencies: []string{
-			"k8s.io/apimachinery/pkg/apis/meta/v1.MicroTime"},
+			"github.com/tilt-dev/tilt/pkg/apis/core/v1alpha1.RegistryHosting", "k8s.io/apimachinery/pkg/apis/meta/v1.MicroTime"},
 	}
 }
 
@@ -5647,6 +5654,46 @@ func schema_pkg_apis_core_v1alpha1_Probe(ref common.ReferenceCallback) common.Op
 		},
 		Dependencies: []string{
 			"github.com/tilt-dev/tilt/pkg/apis/core/v1alpha1.ExecAction", "github.com/tilt-dev/tilt/pkg/apis/core/v1alpha1.HTTPGetAction", "github.com/tilt-dev/tilt/pkg/apis/core/v1alpha1.TCPSocketAction"},
+	}
+}
+
+func schema_pkg_apis_core_v1alpha1_RegistryHosting(ref common.ReferenceCallback) common.OpenAPIDefinition {
+	return common.OpenAPIDefinition{
+		Schema: spec.Schema{
+			SchemaProps: spec.SchemaProps{
+				Type: []string{"object"},
+				Properties: map[string]spec.Schema{
+					"host": {
+						SchemaProps: spec.SchemaProps{
+							Description: "Host documents the host (hostname and port) of the registry, as seen from outside the cluster.\n\nThis is the registry host that tools outside the cluster should push images to.",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"hostFromClusterNetwork": {
+						SchemaProps: spec.SchemaProps{
+							Description: "HostFromClusterNetwork documents the host (hostname and port) of the registry, as seen from networking inside the container pods.\n\nThis is the registry host that tools running on pods inside the cluster should push images to. If not set, then tools inside the cluster should assume the local registry is not available to them.",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"hostFromContainerRuntime": {
+						SchemaProps: spec.SchemaProps{
+							Description: "HostFromContainerRuntime documents the host (hostname and port) of the registry, as seen from the cluster's container runtime.\n\nWhen tools apply Kubernetes objects to the cluster, this host should be used for image name fields. If not set, users of this field should use the value of Host instead.\n\nNote that it doesn't make sense semantically to define this field, but not define Host or HostFromClusterNetwork. That would imply a way to pull images without a way to push images.",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"help": {
+						SchemaProps: spec.SchemaProps{
+							Description: "Help contains a URL pointing to documentation for users on how to set up and configure a local registry.\n\nTools can use this to nudge users to enable the registry. When possible, the writer should use as permanent a URL as possible to prevent drift (e.g., a version control SHA).\n\nWhen image pushes to a registry host specified in one of the other fields fail, the tool should display this help URL to the user. The help URL should contain instructions on how to diagnose broken or misconfigured registries.",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+				},
+			},
+		},
 	}
 }
 
