dfdbdcd92a7c6fd1d5dd0d0664ebe78e9a1c1cd2
Milas Bowman <milas@tilt.dev>
2022-04-06 10:35:17 -0400

    dockerimage: fix import

--- internal/controllers/core/dockerimage/reconciler.go
+++ internal/controllers/core/dockerimage/reconciler.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/source"
