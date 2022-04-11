496db885bae030f23423a8ca058cdbf73ffa63ba
Milas Bowman <milas@tilt.dev>
2022-04-11 17:35:01 -0400

    portforward: add indexing for Cluster
    This is very standard except that I added some extra logic to
    the fake client to apply defaults as they are in the real API
    server.

--- internal/controllers/fake/client.go
+++ internal/controllers/fake/client.go
@@ -6,6 +6,8 @@ import (
 
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	ctrlclient "sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/tilt-dev/tilt-apiserver/pkg/server/builder/resource/resourcestrategy"
 )
 
 // Wrap the fake client in other assertions we want to make.
@@ -13,10 +15,19 @@ type fakeTiltClient struct {
 	ctrlclient.Client
 }
 
-// NOTE(nick): We've had a lot of bugs due to the way the apiserver
-// modifies and truncates objects on update.
-//
-// We simulate this behavior to catch this class of bug.
+func (f fakeTiltClient) Create(ctx context.Context, obj client.Object, opts ...client.CreateOption) error {
+	forCreate := obj.DeepCopyObject().(client.Object)
+	if defaulter, ok := forCreate.(resourcestrategy.Defaulter); ok {
+		defaulter.Default()
+	}
+
+	if err := f.Client.Create(ctx, forCreate, opts...); err != nil {
+		return err
+	}
+
+	return simulateMarshalRoundtrip(forCreate, obj)
+}
+
 func (f fakeTiltClient) Update(ctx context.Context, obj client.Object, opts ...client.UpdateOption) error {
 	if ctxErr := ctx.Err(); ctxErr != nil {
 		// controller-runtime fake ignores context; check it here to allow controllers to test
@@ -24,16 +35,17 @@ func (f fakeTiltClient) Update(ctx context.Context, obj client.Object, opts ...c
 		return ctxErr
 	}
 
-	err := f.Client.Update(ctx, obj, opts...)
+	forUpdate := obj.DeepCopyObject().(client.Object)
+	if defaulter, ok := forUpdate.(resourcestrategy.Defaulter); ok {
+		defaulter.Default()
+	}
+
+	err := f.Client.Update(ctx, forUpdate, opts...)
 	if err != nil {
 		return err
 	}
 
-	content, err := json.Marshal(obj)
-	if err != nil {
-		return err
-	}
-	return json.Unmarshal(content, obj)
+	return simulateMarshalRoundtrip(forUpdate, obj)
 }
 
 func (f fakeTiltClient) Status() client.StatusWriter {
@@ -80,4 +92,25 @@ func (f fakeStatusWriter) Patch(ctx context.Context, obj ctrlclient.Object, patc
 		return err
 	}
 	return json.Unmarshal(content, obj)
+}
+
+// simulateMarshalRoundtrip makes API types behave closer to reality for the
+// fake client.
+//
+// NOTE(nick): We've had a lot of bugs due to the way the apiserver
+// modifies and truncates objects on update. We simulate this behavior
+// to catch this class of bug.
+//
+// The `apiResp` argument is marshaled and then unmarshalled into `dest` arg.
+// (This allows the fake client to mutate *copy* of the user-provided obj, and
+// only mutate the *original* obj here on success.)
+func simulateMarshalRoundtrip(apiResp, dest ctrlclient.Object) error {
+	content, err := json.Marshal(apiResp)
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(content, dest); err != nil {
+		return err
+	}
+	return nil
 }
