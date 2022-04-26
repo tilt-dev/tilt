ddffce39f5200ea3fe7ffb5b3c9c453a30d00cc4
Lizz Thabet <lizzthabet@gmail.com>
2022-04-26 15:16:30 -0400

    (build) Simplify the Jest testing setup by removing unnecessary config

--- web/jest.config.ts
+++
@@ -1,7 +0,0 @@
-import { Config } from "@jest/types"
-
-const config: Config.InitialOptions = {
-  setupFilesAfterEnv: ["<rootDir>/setupTests.ts"]
-}
-
-export default config
--- web/src/setupTests.ts
+++ web/src/setupTests.ts
@@ -1,1 +1,3 @@
+// This file is detected automatically and run before tests
+
 import "@testing-library/jest-dom"
