db7dce0ac4d72c23e4a00911312908134ceb7f09
Milas Bowman <milas@tilt.dev>
2022-04-27 17:20:48 -0400

    cli: remove deprecated --hud flag in favor of --legacy (#5736)
    The `--hud` flag has been deprecated for a bit and thrown a warning
    on start (with a forced multi-second wait).
    
    Users who wish to continue using the TUI may do so for now by running
    `tilt up --legacy`.

--- internal/cli/up_test.go
+++ internal/cli/up_test.go
@@ -15,9 +15,9 @@ func TestHudEnabled(t *testing.T) {
 		args     string
 		expected store.TerminalMode
 	}{
-		{"old behavior: no --hud", "", store.TerminalModePrompt},
-		{"old behavior: --legacy", "--legacy", store.TerminalModeHUD},
-		{"old behavior: --stream=true", "--stream=true", store.TerminalModeStream},
+		{"no flags (default)", "", store.TerminalModePrompt},
+		{"legacy flag", "--legacy", store.TerminalModeHUD},
+		{"stream flag", "--stream=true", store.TerminalModeStream},
 	} {
 		t.Run(test.name, func(t *testing.T) {
 			cmd := upCmd{}
@@ -27,9 +27,6 @@ func TestHudEnabled(t *testing.T) {
 			c := cmd.register()
 			err := c.Flags().Parse(args)
 			require.NoError(t, err)
-
-			c.PreRun(c, args)
-
 			require.Equal(t, test.expected, cmd.initialTermMode(true), test.args)
 		})
 	}
