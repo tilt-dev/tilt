e6b6c0eb03211941dc4a25e301768f86f225376c
Nick Santos <nick.santos@docker.com>
2023-04-17 14:05:38 -0400

    web: display 0.0.0.0 as the current window hostname (#6103)
    fixes https://github.com/tilt-dev/tilt/issues/6080
    
    Signed-off-by: Nick Santos <nick.santos@docker.com>

--- web/src/links.ts
+++ web/src/links.ts
@@ -1,10 +1,26 @@
 // Helper functions for displaying links
 
-import { UILink } from "./types"
+// If a URL contains the hostname 0.0.0.0, resolve that to the current
+// window.location.hostname. This ensures that tilt sends users to the
+// right place when it's being accessed from a remote machine.
+export function resolveURL(input: string): string {
+  if (!input) {
+    return input
+  }
+  try {
+    let url = new URL(input)
+    if (url.hostname === "0.0.0.0") {
+      url.hostname = window.location.hostname
+      return url.toString()
+    }
+  } catch (err) {} // fall through
+  return input
+}
 
-export function displayURL(li: UILink): string {
-  let url = li.url?.replace(/^(http:\/\/)/, "")
-  url = url?.replace(/^(https:\/\/)/, "")
-  url = url?.replace(/^(www\.)/, "")
+export function displayURL(url: string): string {
+  url = url.replace(/^http:\/\//, "")
+  url = url.replace(/^https:\/\//, "")
+  url = url.replace(/^www\./, "")
+  url = url.replace(/^([^\/]+)\/$/, "$1")
   return url || ""
 }
