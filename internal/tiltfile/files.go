24fdc0c193c34e3d7e14a6e4715bc21c19bc3c81
Daniel Nephin <dnephin@gmail.com>
2023-12-11 13:12:22 -0500

    Improve output of multi-line stderr from local command (#6282)
    * Better format for multi-line stderr output in localcmd
    
    Signed-off-by: Daniel Nephin <dnephin@gmail.com>
    
    * Update internal/tiltfile/files.go
    
    Co-authored-by: Daniel Nephin <dnephin@gmail.com>
    
    ---------
    
    Signed-off-by: Daniel Nephin <dnephin@gmail.com>
    Co-authored-by: Nick Santos <nick.santos@docker.com>

--- internal/tiltfile/files.go
+++ internal/tiltfile/files.go
@@ -120,8 +120,9 @@ func (s *tiltfileState) execLocalCmd(t *starlark.Thread, cmd model.Cmd, options 
 		if !options.logOutput {
 			// if we already logged the output, don't include it in the error message to prevent it from
 			// getting output 2x
-			errMessage.WriteString(fmt.Sprintf("\nstdout: %q\nstderr: %q",
-				stdoutBuf.String(), stderrBuf.String()))
+
+			stdout, stderr := stdoutBuf.String(), stderrBuf.String()
+			fmt.Fprintf(&errMessage, "\nstdout:\n%v\nstderr:\n%v\n", stdout, stderr)
 		}
 
 		return "", errors.New(errMessage.String())
