c1e0786af9d8da2fc002cc202ec86b5d5085fb77
Nick Santos <nick.santos@docker.com>
2024-05-31 08:47:26 -0400

    vendor: update fsevents (#6385)
    contains updates for MacOS 13.0 APIs
    
    Signed-off-by: Nick Santos <nick.santos@docker.com>

--- vendor/github.com/fsnotify/fsevents/wrap.go
+++ vendor/github.com/fsnotify/fsevents/wrap.go
@@ -1,4 +1,4 @@
-// +build darwin
+//go:build darwin
 
 package fsevents
 
@@ -22,6 +22,10 @@ static FSEventStreamRef EventStreamCreate(FSEventStreamContext * context, uintpt
 	context->info = (void*) info;
 	return FSEventStreamCreate(NULL, (FSEventStreamCallback) fsevtCallback, context, paths, since, latency, flags);
 }
+
+static void DispatchQueueRelease(dispatch_queue_t queue) {
+	dispatch_release(queue);
+}
 */
 import "C"
 import (
@@ -33,6 +37,215 @@ import (
 	"time"
 	"unsafe"
 )
+
+// CreateFlags specifies what events will be seen in an event stream.
+type CreateFlags uint32
+
+const (
+	// NoDefer sends events on the leading edge (for interactive applications).
+	// By default events are delivered after latency seconds (for background tasks).
+	//
+	// Affects the meaning of the EventStream.Latency parameter. If you specify
+	// this flag and more than latency seconds have elapsed since
+	// the last event, your app will receive the event immediately.
+	// The delivery of the event resets the latency timer and any
+	// further events will be delivered after latency seconds have
+	// elapsed. This flag is useful for apps that are interactive
+	// and want to react immediately to changes but avoid getting
+	// swamped by notifications when changes are occurring in rapid
+	// succession. If you do not specify this flag, then when an
+	// event occurs after a period of no events, the latency timer
+	// is started. Any events that occur during the next latency
+	// seconds will be delivered as one group (including that first
+	// event). The delivery of the group of events resets the
+	// latency timer and any further events will be delivered after
+	// latency seconds. This is the default behavior and is more
+	// appropriate for background, daemon or batch processing apps.
+	NoDefer = CreateFlags(C.kFSEventStreamCreateFlagNoDefer)
+
+	// WatchRoot requests notifications of changes along the path to
+	// the path(s) you're watching. For example, with this flag, if
+	// you watch "/foo/bar" and it is renamed to "/foo/bar.old", you
+	// would receive a RootChanged event. The same is true if the
+	// directory "/foo" were renamed. The event you receive is a
+	// special event: the path for the event is the original path
+	// you specified, the flag RootChanged is set and event ID is
+	// zero. RootChanged events are useful to indicate that you
+	// should rescan a particular hierarchy because it changed
+	// completely (as opposed to the things inside of it changing).
+	// If you want to track the current location of a directory, it
+	// is best to open the directory before creating the stream so
+	// that you have a file descriptor for it and can issue an
+	// F_GETPATH fcntl() to find the current path.
+	WatchRoot = CreateFlags(C.kFSEventStreamCreateFlagWatchRoot)
+
+	// IgnoreSelf doesn't send events triggered by the current process (macOS 10.6+).
+	//
+	// Don't send events that were triggered by the current process.
+	// This is useful for reducing the volume of events that are
+	// sent. It is only useful if your process might modify the file
+	// system hierarchy beneath the path(s) being monitored. Note:
+	// this has no effect on historical events, i.e., those
+	// delivered before the HistoryDone sentinel event.
+	IgnoreSelf = CreateFlags(C.kFSEventStreamCreateFlagIgnoreSelf)
+
+	// FileEvents sends events about individual files, generating significantly
+	// more events (macOS 10.7+) than directory level notifications.
+	FileEvents = CreateFlags(C.kFSEventStreamCreateFlagFileEvents)
+)
+
+// EventFlags passed to the FSEventStreamCallback function.
+// These correspond directly to the flags as described here:
+// https://developer.apple.com/documentation/coreservices/1455361-fseventstreameventflags
+type EventFlags uint32
+
+const (
+	// MustScanSubDirs indicates that events were coalesced hierarchically.
+	//
+	// Your application must rescan not just the directory given in
+	// the event, but all its children, recursively. This can happen
+	// if there was a problem whereby events were coalesced
+	// hierarchically. For example, an event in /Users/jsmith/Music
+	// and an event in /Users/jsmith/Pictures might be coalesced
+	// into an event with this flag set and path=/Users/jsmith. If
+	// this flag is set you may be able to get an idea of whether
+	// the bottleneck happened in the kernel (less likely) or in
+	// your client (more likely) by checking for the presence of the
+	// informational flags UserDropped or KernelDropped.
+	MustScanSubDirs EventFlags = EventFlags(C.kFSEventStreamEventFlagMustScanSubDirs)
+
+	// KernelDropped or UserDropped may be set in addition
+	// to the MustScanSubDirs flag to indicate that a problem
+	// occurred in buffering the events (the particular flag set
+	// indicates where the problem occurred) and that the client
+	// must do a full scan of any directories (and their
+	// subdirectories, recursively) being monitored by this stream.
+	// If you asked to monitor multiple paths with this stream then
+	// you will be notified about all of them. Your code need only
+	// check for the MustScanSubDirs flag; these flags (if present)
+	// only provide information to help you diagnose the problem.
+	KernelDropped = EventFlags(C.kFSEventStreamEventFlagKernelDropped)
+
+	// UserDropped is related to UserDropped above.
+	UserDropped = EventFlags(C.kFSEventStreamEventFlagUserDropped)
+
+	// EventIDsWrapped indicates the 64-bit event ID counter wrapped around.
+	//
+	// If EventIdsWrapped is set, it means
+	// the 64-bit event ID counter wrapped around. As a result,
+	// previously-issued event ID's are no longer valid
+	// for the EventID field when using EventStream.Resume.
+	EventIDsWrapped = EventFlags(C.kFSEventStreamEventFlagEventIdsWrapped)
+
+	// HistoryDone is a sentinel event when retrieving events with EventStream.Resume.
+	//
+	// Denotes a sentinel event sent to mark the end of the
+	// "historical" events sent as a result of specifying
+	// EventStream.Resume.
+	//
+	// After sending all the "historical" events that occurred before now,
+	// an event will be sent with the HistoryDone flag set. The client
+	// should ignore the path supplied in that event.
+	HistoryDone = EventFlags(C.kFSEventStreamEventFlagHistoryDone)
+
+	// RootChanged indicates a change to a directory along the path being watched.
+	//
+	// Denotes a special event sent when there is a change to one of
+	// the directories along the path to one of the directories you
+	// asked to watch. When this flag is set, the event ID is zero
+	// and the path corresponds to one of the paths you asked to
+	// watch (specifically, the one that changed). The path may no
+	// longer exist because it or one of its parents was deleted or
+	// renamed. Events with this flag set will only be sent if you
+	// passed the flag WatchRoot when you created the stream.
+	RootChanged = EventFlags(C.kFSEventStreamEventFlagRootChanged)
+
+	// Mount for a volume mounted underneath the path being monitored.
+	//
+	// Denotes a special event sent when a volume is mounted
+	// underneath one of the paths being monitored. The path in the
+	// event is the path to the newly-mounted volume. You will
+	// receive one of these notifications for every volume mount
+	// event inside the kernel (independent of DiskArbitration).
+	// Beware that a newly-mounted volume could contain an
+	// arbitrarily large directory hierarchy. Avoid pitfalls like
+	// triggering a recursive scan of a non-local filesystem, which
+	// you can detect by checking for the absence of the MNT_LOCAL
+	// flag in the f_flags returned by statfs(). Also be aware of
+	// the MNT_DONTBROWSE flag that is set for volumes which should
+	// not be displayed by user interface elements.
+	Mount = EventFlags(C.kFSEventStreamEventFlagMount)
+
+	// Unmount event occurs after a volume is unmounted.
+	//
+	// Denotes a special event sent when a volume is unmounted
+	// underneath one of the paths being monitored. The path in the
+	// event is the path to the directory from which the volume was
+	// unmounted. You will receive one of these notifications for
+	// every volume unmount event inside the kernel. This is not a
+	// substitute for the notifications provided by the
+	// DiskArbitration framework; you only get notified after the
+	// unmount has occurred. Beware that unmounting a volume could
+	// uncover an arbitrarily large directory hierarchy, although
+	// macOS never does that.
+	Unmount = EventFlags(C.kFSEventStreamEventFlagUnmount)
+
+	// The following flags are only set when using FileEvents.
+
+	// ItemCreated indicates that a file or directory has been created.
+	ItemCreated = EventFlags(C.kFSEventStreamEventFlagItemCreated)
+
+	// ItemRemoved indicates that a file or directory has been removed.
+	ItemRemoved = EventFlags(C.kFSEventStreamEventFlagItemRemoved)
+
+	// ItemInodeMetaMod indicates that a file or directory's metadata has has been modified.
+	ItemInodeMetaMod = EventFlags(C.kFSEventStreamEventFlagItemInodeMetaMod)
+
+	// ItemRenamed indicates that a file or directory has been renamed.
+	// TODO is there any indication what it might have been renamed to?
+	ItemRenamed = EventFlags(C.kFSEventStreamEventFlagItemRenamed)
+
+	// ItemModified indicates that a file has been modified.
+	ItemModified = EventFlags(C.kFSEventStreamEventFlagItemModified)
+
+	// ItemFinderInfoMod indicates the the item's Finder information has been
+	// modified.
+	// TODO the above is just a guess.
+	ItemFinderInfoMod = EventFlags(C.kFSEventStreamEventFlagItemFinderInfoMod)
+
+	// ItemChangeOwner indicates that the file has changed ownership.
+	ItemChangeOwner = EventFlags(C.kFSEventStreamEventFlagItemChangeOwner)
+
+	// ItemXattrMod indicates that the files extended attributes have changed.
+	ItemXattrMod = EventFlags(C.kFSEventStreamEventFlagItemXattrMod)
+
+	// ItemIsFile indicates that the item is a file.
+	ItemIsFile = EventFlags(C.kFSEventStreamEventFlagItemIsFile)
+
+	// ItemIsDir indicates that the item is a directory.
+	ItemIsDir = EventFlags(C.kFSEventStreamEventFlagItemIsDir)
+
+	// ItemIsSymlink indicates that the item is a symbolic link.
+	ItemIsSymlink = EventFlags(C.kFSEventStreamEventFlagItemIsSymlink)
+)
+
+const (
+	nullCFStringRef = C.CFStringRef(0)
+	nullCFUUIDRef   = C.CFUUIDRef(0)
+
+	// eventIDSinceNow is a sentinel to begin watching events "since now".
+	eventIDSinceNow = uint64(C.kFSEventStreamEventIdSinceNow)
+)
+
+// GetDeviceUUID retrieves the UUID required to identify an EventID
+// in the FSEvents database
+func GetDeviceUUID(deviceID int32) string {
+	uuid := C.FSEventsCopyUUIDForDevice(C.dev_t(deviceID))
+	if uuid == nullCFUUIDRef {
+		return ""
+	}
+	return cfStringToGoString(C.CFUUIDCreateString(C.kCFAllocatorDefault, uuid))
+}
 
 // LatestEventID returns the most recently generated event ID, system-wide.
 func LatestEventID() uint64 {
@@ -69,28 +282,30 @@ func fsevtCallback(stream C.FSEventStreamRef, info uintptr, numEvents C.size_t, 
 	es.Events <- events
 }
 
-// FSEventStreamRef wraps C.FSEventStreamRef
-type FSEventStreamRef C.FSEventStreamRef
+type fsDispatchQueueRef C.dispatch_queue_t
 
-// GetStreamRefEventID retrieves the last EventID from the ref
-func GetStreamRefEventID(f FSEventStreamRef) uint64 {
+// fsEventStreamRef wraps C.FSEventStreamRef
+type fsEventStreamRef C.FSEventStreamRef
+
+// getStreamRefEventID retrieves the last EventID from the ref
+func getStreamRefEventID(f fsEventStreamRef) uint64 {
 	return uint64(C.FSEventStreamGetLatestEventId(f))
 }
 
-// GetStreamRefDeviceID retrieves the device ID the stream is watching
-func GetStreamRefDeviceID(f FSEventStreamRef) int32 {
+// getStreamRefDeviceID retrieves the device ID the stream is watching
+func getStreamRefDeviceID(f fsEventStreamRef) int32 {
 	return int32(C.FSEventStreamGetDeviceBeingWatched(f))
 }
 
-// GetStreamRefDescription retrieves debugging description information
+// getStreamRefDescription retrieves debugging description information
 // about the StreamRef
-func GetStreamRefDescription(f FSEventStreamRef) string {
+func getStreamRefDescription(f fsEventStreamRef) string {
 	return cfStringToGoString(C.FSEventStreamCopyDescription(f))
 }
 
-// GetStreamRefPaths returns a copy of the paths being watched by
+// getStreamRefPaths returns a copy of the paths being watched by
 // this stream
-func GetStreamRefPaths(f FSEventStreamRef) []string {
+func getStreamRefPaths(f fsEventStreamRef) []string {
 	arr := C.FSEventStreamCopyPathsBeingWatched(f)
 	l := cfArrayLen(arr)
 
@@ -140,9 +355,6 @@ func copyCFString(cfs C.CFStringRef) C.CFStringRef {
 	return C.CFStringCreateCopy(C.kCFAllocatorDefault, cfs)
 }
 
-// CFRunLoopRef wraps C.CFRunLoopRef
-type CFRunLoopRef C.CFRunLoopRef
-
 // EventIDForDeviceBeforeTime returns an event ID before a given time.
 func EventIDForDeviceBeforeTime(dev int32, before time.Time) uint64 {
 	tm := C.CFAbsoluteTime(before.Unix())
@@ -186,7 +398,7 @@ func cfArrayLen(ref C.CFArrayRef) int {
 	return int(C.CFArrayGetCount(ref))
 }
 
-func setupStream(paths []string, flags CreateFlags, callbackInfo uintptr, eventID uint64, latency time.Duration, deviceID int32) FSEventStreamRef {
+func setupStream(paths []string, flags CreateFlags, callbackInfo uintptr, eventID uint64, latency time.Duration, deviceID int32) fsEventStreamRef {
 	cPaths, err := createPaths(paths)
 	if err != nil {
 		log.Printf("Error creating paths: %s", err)
@@ -208,10 +420,10 @@ func setupStream(paths []string, flags CreateFlags, callbackInfo uintptr, eventI
 			cfinv, C.FSEventStreamCreateFlags(flags))
 	}
 
-	return FSEventStreamRef(ref)
+	return fsEventStreamRef(ref)
 }
 
-func (es *EventStream) start(paths []string, callbackInfo uintptr) {
+func (es *EventStream) start(paths []string, callbackInfo uintptr) error {
 
 	since := eventIDSinceNow
 	if es.Resume {
@@ -220,17 +432,20 @@ func (es *EventStream) start(paths []string, callbackInfo uintptr) {
 
 	es.stream = setupStream(paths, es.Flags, callbackInfo, since, es.Latency, es.Device)
 
-	started := make(chan struct{})
+	es.qref = fsDispatchQueueRef(C.dispatch_queue_create(nil, nil))
+	C.FSEventStreamSetDispatchQueue(es.stream, es.qref)
 
-	go func() {
-		runtime.LockOSThread()
-		es.rlref = CFRunLoopRef(C.CFRunLoopGetCurrent())
-		C.CFRetain(C.CFTypeRef(es.rlref))
-		C.FSEventStreamScheduleWithRunLoop(es.stream, C.CFRunLoopRef(es.rlref), C.kCFRunLoopDefaultMode)
-		C.FSEventStreamStart(es.stream)
-		close(started)
-		C.CFRunLoopRun()
-	}()
+	if C.FSEventStreamStart(es.stream) == 0 {
+		// cleanup stream
+		C.FSEventStreamInvalidate(es.stream)
+		C.FSEventStreamRelease(es.stream)
+		es.stream = nil
+
+		C.DispatchQueueRelease(es.qref)
+		es.qref = nil
+
+		return fmt.Errorf("failed to start eventstream")
+	}
 
 	if !es.hasFinalizer {
 		// TODO: There is no guarantee this run before program exit
@@ -239,7 +454,7 @@ func (es *EventStream) start(paths []string, callbackInfo uintptr) {
 		es.hasFinalizer = true
 	}
 
-	<-started
+	return nil
 }
 
 func finalizer(es *EventStream) {
@@ -249,7 +464,7 @@ func finalizer(es *EventStream) {
 }
 
 // flush drains the event stream of undelivered events
-func flush(stream FSEventStreamRef, sync bool) {
+func flush(stream fsEventStreamRef, sync bool) {
 	if sync {
 		C.FSEventStreamFlushSync(stream)
 	} else {
@@ -258,10 +473,9 @@ func flush(stream FSEventStreamRef, sync bool) {
 }
 
 // stop requests fsevents stops streaming events
-func stop(stream FSEventStreamRef, rlref CFRunLoopRef) {
+func stop(stream fsEventStreamRef, qref fsDispatchQueueRef) {
 	C.FSEventStreamStop(stream)
 	C.FSEventStreamInvalidate(stream)
 	C.FSEventStreamRelease(stream)
-	C.CFRunLoopStop(C.CFRunLoopRef(rlref))
-	C.CFRelease(C.CFTypeRef(rlref))
+	C.DispatchQueueRelease(qref)
 }
