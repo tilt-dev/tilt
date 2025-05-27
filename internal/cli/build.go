06acc129e34fe7fdfb137f0ccdb5d8613e353ea0
Tilt Dev <it@tilt.dev>
2025-05-27 19:50:04 +0000

    Update version numbers: 0.34.4

--- internal/cli/build.go
+++ internal/cli/build.go
@@ -17,7 +17,7 @@ import (
 // For distributed binaries, version is automatically baked
 // into the binary with goreleaser. If this doesn't get updated
 // on every release, it's often not that big a deal.
-const devVersion = "0.34.3"
+const devVersion = "0.34.4"
 
 var commitSHA string
 var globalTiltInfo model.TiltBuild
