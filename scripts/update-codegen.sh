09c5604925cb57ed55c96be22bc8378f20584c9c
Nick Santos <nick.santos@docker.com>
2025-09-30 12:24:16 -0400

    chore: use buildkit instead of toast for ts protogen (#6614)
    Signed-off-by: Nick Santos <nick.santos@docker.com>

--- scripts/update-codegen.sh
+++ scripts/update-codegen.sh
@@ -22,6 +22,7 @@ docker run --rm -v "$(pwd)":/go/src/github.com/tilt-dev/tilt \
    --entrypoint /go/src/github.com/tilt-dev/tilt/scripts/update-protobuf-helper.sh \
    tilt-protobuf-helper
 
+
 docker run --rm -e "CODEGEN_UID=$(id -u)" -e "CODEGEN_GID=$(id -g)" -v "$(pwd)":/go/src/github.com/tilt-dev/tilt \
    --workdir /go/src/github.com/tilt-dev/tilt \
    --entrypoint ./scripts/update-codegen-helper.sh \
