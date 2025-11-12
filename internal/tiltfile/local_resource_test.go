39bd1fb3a7427e820ffae8365395056f710cd2ba
Nick Santos <nick.santos@docker.com>
2025-11-12 09:29:14 -0500

    tiltfile: validation on local_resource dir, serve_dir
    fixes https://github.com/tilt-dev/tilt/issues/6625
    
    Signed-off-by: Nick Santos <nick.santos@docker.com>

--- internal/tiltfile/local_resource_test.go
+++ internal/tiltfile/local_resource_test.go
@@ -10,3 +10,48 @@ test("test", "echo hi")
 `)
 	f.loadAssertWarnings(testDeprecationMsg)
 }
+
+func TestLocalResourceDirWithoutCmd(t *testing.T) {
+	f := newFixture(t)
+
+	f.file("Tiltfile", `
+local_resource("test", serve_cmd="python server.py", dir="./foo")
+`)
+	f.loadErrString("'dir' only affects 'cmd', not 'serve_cmd'. Did you mean to use 'serve_dir' instead?")
+}
+
+func TestLocalResourceDirWithoutCmdNoServe(t *testing.T) {
+	f := newFixture(t)
+
+	f.file("Tiltfile", `
+local_resource("test", dir="./foo")
+`)
+	f.loadErrString("'dir' specified but 'cmd' is empty")
+}
+
+func TestLocalResourceServeDirWithoutServeCmd(t *testing.T) {
+	f := newFixture(t)
+
+	f.file("Tiltfile", `
+local_resource("test", cmd="echo hi", serve_dir="./foo")
+`)
+	f.loadErrString("'serve_dir' specified but 'serve_cmd' is empty")
+}
+
+func TestLocalResourceDirWithCmdWorks(t *testing.T) {
+	f := newFixture(t)
+
+	f.file("Tiltfile", `
+local_resource("test", cmd="echo hi", dir="./foo")
+`)
+	f.load()
+}
+
+func TestLocalResourceServeDirWithServeCmdWorks(t *testing.T) {
+	f := newFixture(t)
+
+	f.file("Tiltfile", `
+local_resource("test", serve_cmd="python server.py", serve_dir="./foo")
+`)
+	f.load()
+}
