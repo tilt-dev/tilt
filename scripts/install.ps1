90e15ac610edaab9a9cc2475244329d875461506
asrichesson <51305664+asrichesson@users.noreply.github.com>
2026-01-26 16:59:54 -0500

    Improve error handling during Tilt PowerShell installation (#6670)
    Signed-off-by: Andrew Richesson <asrichesson@gmail.com>
    
    Co-authored-by: Andrew Richesson <ARichesson@tql.com>

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -3,6 +3,9 @@
 # Usage:
 #   iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/tilt-dev/tilt/master/scripts/install.ps1')
 
+& { #enclose in scope to avoid leaking variables when ran through iex
+$ErrorActionPreference = 'Stop'
+try {        
 $version = "0.36.1"
 $url = "https://github.com/tilt-dev/tilt/releases/download/v" + $version + "/tilt." + $version + ".windows.x86_64.zip"
 $zip = "tilt-" + $version + ".zip"
@@ -40,10 +43,13 @@ Expand-Archive $zip -DestinationPath $extractDir
 
 Write-Output "Installing Tilt as $dest"
 New-Item -ItemType Directory -Force -Path $binDir >$null
-Move-Item -Force -Path "$extractDir\tilt.exe" -Destination "$dest"
-
-Remove-Item -Force -Path "$zip"
-Remove-Item -Force -Recurse -Path "$extractDir"
+try {
+    Move-Item -Force -Path "$extractDir\tilt.exe" -Destination "$dest"    
+}
+catch {
+    Write-Host "Unable to install tilt.exe. Is tilt in use?" -ForegroundColor Red
+    throw
+}
 
 iex "$dest version"
 iex "$dest verify-install"
@@ -51,4 +57,13 @@ iex "$dest verify-install"
 Write-Output "Tilt installed!"
 Write-Output "Run '$dest up' to start."
 Write-Output "Or add $binDir to your PATH"
-
+}
+catch {
+    Write-Output $_
+    Write-Host "Tilt installation failed" -ForegroundColor Red
+}
+finally {
+    Remove-Item -Force -Path "$zip" -ErrorAction Ignore
+    Remove-Item -Force -Recurse -Path "$extractDir" -ErrorAction Ignore
+}
+}
